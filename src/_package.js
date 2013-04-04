5bb1b21356c2d8780edd8ecc6723201729f505d2
Jeffrey Heer <jeffrey.heer@gmail.com>
2013-04-03 18:41:42 -0700

    Remove tab characters.

--- src/_package.js
+++ src/_package.js
@@ -124,39 +124,39 @@ vg.unique = function(data, f) {
 // Colors
 
 vg.category10 = [
-	"#1f77b4",
-	"#ff7f0e",
-	"#2ca02c",
-	"#d62728",
-	"#9467bd",
-	"#8c564b",
-	"#e377c2",
-	"#7f7f7f",
-	"#bcbd22",
-	"#17becf"
+  "#1f77b4",
+  "#ff7f0e",
+  "#2ca02c",
+  "#d62728",
+  "#9467bd",
+  "#8c564b",
+  "#e377c2",
+  "#7f7f7f",
+  "#bcbd22",
+  "#17becf"
 ];
 
 vg.category20 = [
-	"#1f77b4",
-	"#aec7e8",
-	"#ff7f0e",
-	"#ffbb78",
-	"#2ca02c",
-	"#98df8a",
-	"#d62728",
-	"#ff9896",
-	"#9467bd",
-	"#c5b0d5",
-	"#8c564b",
-	"#c49c94",
-	"#e377c2",
-	"#f7b6d2",
-	"#7f7f7f",
-	"#c7c7c7",
-	"#bcbd22",
-	"#dbdb8d",
-	"#17becf",
-	"#9edae5"
+  "#1f77b4",
+  "#aec7e8",
+  "#ff7f0e",
+  "#ffbb78",
+  "#2ca02c",
+  "#98df8a",
+  "#d62728",
+  "#ff9896",
+  "#9467bd",
+  "#c5b0d5",
+  "#8c564b",
+  "#c49c94",
+  "#e377c2",
+  "#f7b6d2",
+  "#7f7f7f",
+  "#c7c7c7",
+  "#bcbd22",
+  "#dbdb8d",
+  "#17becf",
+  "#9edae5"
 ];
 
 vg.shapes = [
