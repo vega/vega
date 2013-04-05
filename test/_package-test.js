afb89709e2b3b6a196613e988147702332a7b4b6
Lars Grammel <lars.grammel@gmail.com>
2013-04-05 16:28:10 -0700

    added unit tests for vq.unique

--- test/_package-test.js
+++ test/_package-test.js
@@ -26,7 +26,33 @@ suite.addBatch({
       'array(one non-array argument) should return an array containing the argument': function (vg) {
         assert.deepEqual(vg.array(1), [1]);
       }
-    }   }
+    },
+    'unique': {
+      'without transformation': {
+        'should return all values of an array that contains only unique values in the same order': function (vg) {
+          assert.deepEqual(vg.unique([1, 2, 3]), [1, 2, 3]);
+        },
+        'should filter out repeated occurrences of values': function (vg) {
+          assert.deepEqual(vg.unique([1, 1, 2, 1, 2, 3, 1, 2, 3, 3, 3]), [1, 2, 3]);
+        },
+        'should treat undefined as a value and remove duplicates': function (vg) {
+          assert.deepEqual(vg.unique([1, undefined, 2, undefined]), [1, undefined, 2]);
+        }
+      },
+      'with transformation': {
+        'should apply transformation to array elements': function (vg) {
+          assert.deepEqual(vg.unique([1, 2, 3], function (d) {
+            return -2 * d
+          }), [-2, -4, -6]);
+        },
+        'should filter out repeated occurrences of transformed values': function (vg) {
+          assert.deepEqual(vg.unique([1, 1, 2, 3], function (d) {
+            return d < 3 ? 1 : 3;
+          }), [1, 3]);
+        }
+      }
+    }
+  }
 });
 
 suite.export(module);
