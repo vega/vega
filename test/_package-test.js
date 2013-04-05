790c1e4619fd7833463c65b481f8cbe94d56d17a
Lars Grammel <lars.grammel@gmail.com>
2013-04-05 15:58:05 -0700

    simplified test cases

--- test/_package-test.js
+++ test/_package-test.js
@@ -8,50 +8,25 @@ suite.addBatch({
   '_package': {
     topic: setup("_package").expression("vg"),
     'isNumber': {
-      '0 is a number': {
-        topic: function (vg) {
-          return vg.isNumber(0);
-        },
-        'should be true': function (topic) {
-          assert.equal(topic, true);
-        }
+      'isNumber(0) should be true': function (vg) {
+        assert.equal(vg.isNumber(0), true);
       }
     },
     'array': {
-      'array called with null': {
-        topic: function (vg) {
-          return vg.array(null);
-        },
-        'should return an empty array': function (topic) {
-          assert.deepEqual(topic, []);
-        }
+      'array(null) should return an empty array': function (vg) {
+        assert.deepEqual(vg.array(null), []);
       },
-      'array called without parameters)': {
-        topic: function (vg) {
-          return vg.array();
-        },
-        'should return an empty array': function (topic) {
-          assert.deepEqual(topic, []);
-        }
+      'array(undefined) should return an empty array': function (vg) {
+        assert.deepEqual(vg.array(), []);
       },
-      'array called with an array': {
-        topic: function (vg) {
-            return vg.array([1, 2, 3]);
-        },
-        'should return the same array': function (topic) {
-          assert.deepEqual(topic, [1, 2, 3]);
-        }
+      'array(some array) should return the same array': function (vg) {
+        var value = [1, 2, 3];
+        assert.strictEqual(vg.array(value), value);
       },
-      'array called with one non-array argument': {
-        topic: function (vg) {
-            return vg.array(1);
-        },
-        'should return an array containing the argument': function (topic) {
-          assert.deepEqual(topic, [1]);
-        }
+      'array(one non-array argument) should return an array containing the argument': function (vg) {
+        assert.deepEqual(vg.array(1), [1]);
       }
-    }
-  }
+    }   }
 });
 
 suite.export(module);
