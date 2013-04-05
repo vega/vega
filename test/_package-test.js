1435f506795366509c0bbd262fcefdded84cbb3e
Lars Grammel <lars.grammel@gmail.com>
2013-04-05 15:47:53 -0700

    added unit tests for vg.array

--- test/_package-test.js
+++ test/_package-test.js
@@ -16,6 +16,40 @@ suite.addBatch({
           assert.equal(topic, true);
         }
       }
+    },
+    'array': {
+      'array called with null': {
+        topic: function (vg) {
+          return vg.array(null);
+        },
+        'should return an empty array': function (topic) {
+          assert.deepEqual(topic, []);
+        }
+      },
+      'array called without parameters)': {
+        topic: function (vg) {
+          return vg.array();
+        },
+        'should return an empty array': function (topic) {
+          assert.deepEqual(topic, []);
+        }
+      },
+      'array called with an array': {
+        topic: function (vg) {
+            return vg.array([1, 2, 3]);
+        },
+        'should return the same array': function (topic) {
+          assert.deepEqual(topic, [1, 2, 3]);
+        }
+      },
+      'array called with one non-array argument': {
+        topic: function (vg) {
+            return vg.array(1);
+        },
+        'should return an array containing the argument': function (topic) {
+          assert.deepEqual(topic, [1]);
+        }
+      }
     }
   }
 });
