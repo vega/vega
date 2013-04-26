70167da47dec1d19eb2e90d612b36a5b4aeecb5d
Jeffrey Heer <jeffrey.heer@gmail.com>
2013-04-25 17:47:05 -0700

    Update stack transform for missing values.

--- vega.js
+++ vega.js
@@ -2490,7 +2490,7 @@ function vg_load_http(url, callback) {
       // if no keys, skip collation step
       vals.push(obj = {
         key: "", keys: [], index: 0,
-        values: sort ? data : data.slice()
+        values: sort ? data.slice() : data
       });
       if (sort) obj.values.sort(sort);
       return result;
@@ -3029,10 +3029,9 @@ function vg_load_http(url, callback) {
 
   return sort;
 };vg.data.stack = function() {
-  var layout = d3.layout.stack()
-                 .values(function(d) { return d.values; }),
-      point = null,
-      height = null,
+  var layout = d3.layout.stack(),
+      point = vg.accessor("index"),
+      height = vg.accessor("data"),
       params = ["offset", "order"],
       output = {
         "y0": "y2",
@@ -3043,14 +3042,47 @@ function vg_load_http(url, callback) {
     var out_y0 = output["y0"],
         out_y1 = output["y1"];
     
-    return layout
-      .x(point)
-      .y(height)
-      .out(function(d, y0, y) {
-        d[out_y0] = y0;
-        d[out_y1] = y + y0;
-      })
-      (data.values);
+    layout.out(function(d, y0, y) {
+      if (d.datum) {
+        d.datum[out_y0] = y0;
+        d.datum[out_y1] = y + y0;
+      }
+    })(stacks(data));
+    
+    return data;
+  }
+  
+  function stacks(data) {
+    var values = vg.values(data),
+        points = [], series = [],
+        a, i, n, j, m, k, p, v, x;
+
+    // collect and sort data points
+    for (i=0, n=values.length; i<n; ++i) {
+      a = vg.values(values[i]);
+      for (j=0, m=a.length; j<m; ++j) {
+        points.push({x:point(a[j]), y:height(a[j]), z:i, datum:a[j]});
+      }
+      series.push([]);
+    }
+    points.sort(function(a,b) {
+      return a.x<b.x ? -1 : a.x>b.x ? 1 : (a.z<b.z ? -1 : a.z>b.z ? 1 : 0);
+    });
+
+    // emit data series for stack layout
+    for (x=points[0].x, i=0, j=0, k=0, n=points.length; k<n; ++k) {
+      p = points[k];    
+      if (p.x !== x) {
+        while (i < series.length) series[i++].push({x:j, y:0});
+        x = p.x; i = 0; j += 1;
+      }
+      while (p.z > i) series[i++].push({x:j, y:0});
+      p.x = j;
+      series[i++].push(p);
+    }
+    while (i < series.length) series[i++].push({x:j, y:0});
+
+    return series;
   }
        
   stack.point = function(field) {
