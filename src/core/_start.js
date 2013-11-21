86aa583da7e2f4427b08ee7dda8f2740ad7c69c5
Jeffrey Heer <jeffrey.heer@gmail.com>
2013-11-21 10:30:56 -0800

    Create v1.3.4 branch.

--- src/core/_start.js
+++ src/core/_start.js
@@ -1,6 +1,6 @@
 vg = (function(d3, topojson) { // take d3 & topojson as imports
   var vg = {
-    version:  "1.3.3", // semantic versioning
+    version:  "1.3.4", // semantic versioning
     d3:       d3,      // stash d3 for use in property functions
     topojson: topojson // stash topojson similarly
   };
