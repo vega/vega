336b46696f90ee618236c0ab5732ce4e2be485e1
Jeffrey Heer <jeffrey.heer@gmail.com>
2014-05-01 10:35:09 -0700

    Add CSS classes to SVG output (#182).

--- src/parse/mark.js
+++ src/parse/mark.js
@@ -1,16 +1,17 @@
 vg.parse.mark = function(mark) {
   var props = mark.properties,
       group = mark.marks;
-  
+
   // parse mark property definitions
   vg.keys(props).forEach(function(k) {
     props[k] = vg.parse.properties(mark.type, props[k]);
   });
+
   // parse delay function
   if (mark.delay) {
     mark.delay = vg.parse.properties(mark.type, {delay: mark.delay});
   }
-      
+
   // parse mark data definition
   if (mark.from) {
     var name = mark.from.data,
@@ -20,11 +21,11 @@ vg.parse.mark = function(mark) {
       return tx(data, db, group);
     };
   }
-  
+
   // recurse if group type
   if (group) {
     mark.marks = group.map(vg.parse.mark);
   }
-      
+    
   return mark;
 };
