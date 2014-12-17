a4225f3909328dd5f6d3c6198858e2cef1b7b745
Jeffrey Heer <jeffrey.heer@gmail.com>
2014-12-17 15:12:23 -0800

    Add aggregate and bin transforms.

--- vega.js
+++ vega.js
@@ -2952,7 +2952,101 @@ function vg_load_http(url, callback) {
   read.formats = formats;
   read.parse = parseValues;
   return read;
-})();vg.data.array = function() {
+})();vg.data.aggregate = function() {
+  var groupby = [],
+      fields = [],
+      gaccess,
+      faccess;
+
+  var OPS = {
+    "count": function() {},
+		"sum": function(c, s, x) { return s + x; },
+		"avg": function(c, s, x) { return s + (x-s)/c.count; },
+		"min": function(c, s, x) { return x < s ? x : s; },
+		"max": function(c, s, x) { return x > s ? x : s; }
+	};
+	OPS.min.init = function() { return +Infinity; }
+	OPS.max.init = function() { return -Infinity; }
+
+	function fkey(x) {
+		return x.op + "_" + x.field;
+	}
+
+	var cells = {};
+
+  function cell(x) {
+    // consider other key constructions...
+    var k = gaccess.reduce(function(v,f) {
+      return (v.push(f(x)), v);
+    }, []).join("|");
+    return cells[k] || (cells[k] = new_cell(x));
+  }
+
+  function new_cell(x) {
+    var o = {};
+    // dimensions
+    for (var i=0, f; i<groupby.length; ++i) {
+      o[groupby[i]] = gaccess[i](x);
+    }
+    // measures
+    o.count = 0;
+		for (i=0; i<fields.length; ++i) {
+		  if (fields[i].op === "count") continue;
+		  var op = OPS[fields[i].op];
+			o[fkey(fields[i])] = op.init ? op.init() : 0;
+		}
+    return o;
+  }
+
+  function aggregate(input) {
+    var output = [], k;
+		var keys = fields.map(fkey);
+		var ops = fields.map(function(x) { return OPS[x.op]; });
+
+    // compute aggregates
+    input.forEach(function(x) {
+      var c = cell(x);
+
+			// compute aggregates...
+      c.count += 1;
+			for (var i=0; i<fields.length; ++i) {
+				c[keys[i]] = ops[i](c, c[keys[i]], faccess[i](x));
+			}
+    });
+    // collect output tuples
+    var index = 0;
+    for (k in cells) {
+      output.push({index:index++, data:cells[k]});
+    }
+    cells = {}; // clear internal state
+    return output;
+  };
+
+  aggregate.fields = function(f) {
+    fields = vg.array(f);
+    faccess = fields.map(function(x,i) {
+      var xf = x.field;
+      if (xf.indexOf("data.") === 0) {
+        fields[i] = {op:x.op, field:xf.slice(5)};
+      }
+      return vg.accessor(xf);
+    });
+    return aggregate;
+  };
+
+  aggregate.groupby = function(f) {
+    groupby = vg.array(f);
+    gaccess = groupby.map(function(x,i) {
+      if (x.indexOf("data.") === 0) {
+        groupby[i] = x.slice(5);
+      }
+      return vg.accessor(x);
+    });
+    return aggregate;
+  };
+
+  return aggregate;
+};vg.data.array = function() {
   var fields = [];
    
   function array(data) {
@@ -2971,6 +3065,146 @@ function vg_load_http(url, callback) {
   };
   
   return array;
+};vg.data.bin = function() {
+
+  var field,
+      accessor,
+      setter,
+      min = undefined,
+      max = undefined,
+      step = undefined,
+      maxbins = 20,
+      output = "bin";
+
+  function compare(a, b) {
+    return a < b ? -1 : a > b ? 1 : a >= b ? 0 : NaN;
+  }
+
+  function bisectLeft(a, x, lo, hi) {
+    if (arguments.length < 3) { lo = 0; }
+    if (arguments.length < 4) { hi = a.length; }
+    while (lo < hi) {
+      var mid = lo + hi >>> 1;
+      if (compare(a[mid], x) < 0) { lo = mid + 1; }
+      else { hi = mid; }
+    }
+    return lo;
+  }
+
+  function bins(opt) {
+    opt = opt || {};
+
+    // determine range
+    var maxb = opt.maxbins || 1024,
+        base = opt.base || 10,
+        div = opt.div || [5, 2],
+        mins = opt.minstep || 0,
+        logb = Math.log(base),
+        level = Math.ceil(Math.log(maxb) / logb),
+        min = opt.min,
+        max = opt.max,
+        span = max - min,
+        step = Math.max(mins, Math.pow(base, Math.round(Math.log(span) / logb) - level)),
+        nbins = Math.ceil(span / step),
+        precision, v, i, eps;
+
+    if (opt.step != null) {
+      step = opt.step;
+    } else if (opt.steps) {
+      // if provided, limit choice to acceptable step sizes
+      step = opt.steps[Math.min(
+          opt.steps.length - 1,
+          bisectLeft(opt.steps, span / maxb)
+      )];
+    } else {
+      // increase step size if too many bins
+      do {
+        step *= base;
+        nbins = Math.ceil(span / step);
+      } while (nbins > maxb);
+
+      // decrease step size if allowed
+      for (i = 0; i < div.length; ++i) {
+        v = step / div[i];
+        if (v >= mins && span / v <= maxb) {
+          step = v;
+          nbins = Math.ceil(span / step);
+        }
+      }
+    }
+
+    // update precision, min and max
+    v = Math.log(step);
+    precision = v >= 0 ? 0 : ~~(-v / logb) + 1;
+    eps = Math.pow(base, -precision - 1);
+
+    // outer Math.min to remove some rounding errors:
+    min = Math.min(min, Math.floor(min / step + eps) * step);
+    max = Math.ceil(max / step) * step;
+
+    return {
+      start: min,
+      stop: max,
+      step: step,
+      unit: precision
+    };
+  }
+
+  function bin(input) {
+    var opt = {
+      min: min != null ? min : +Infinity,
+      max: max != null ? max : -Infinity,
+      step: step != null ? step : null,
+      maxbins: maxbins
+    };
+    if (min == null || max == null) {
+      input.forEach(function(d) {
+        var v = accessor(d);
+        if (min == null && v > opt.max) opt.max = v;
+        if (max == null && v < opt.min) opt.min = v;
+      });
+    }
+    var b = bins(opt);
+    input.forEach(function(d) {
+      var v = accessor(d);
+      setter(d, b.start + b.step * ~~((v - b.start) / b.step));
+    });
+    return input;
+  }
+
+  bin.min = function(x) {
+    min = x;
+    return bin;
+  };
+
+  bin.max = function(x) {
+    max = x;
+    return bin;
+  };
+
+  bin.step = function(x) {
+    step = x;
+    return bin;
+  };
+
+  bin.maxbins = function(x) {
+    maxbins = x;
+    return bin;
+  };
+
+  bin.field = function(f) {
+    field = f;
+    accessor = vg.accessor(f);
+    return bin;
+  };
+
+  bin.output = function(f) {
+    output = f;
+    setter = vg.mutator(f);
+    return bin;
+  };
+
+  return bin;
 };vg.data.copy = function() {
   var from = vg.accessor("data"),
       fields = [],
