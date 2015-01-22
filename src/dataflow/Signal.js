c51d06bb3c7b7d8c023b6145b38d3286b2f08f2e
Arvind Satyanarayan <arvindsatya1@gmail.com>
2015-01-22 09:14:34 -0800

    Refactored Signal. Signal is a Node now.

--- src/dataflow/Signal.js
+++ src/dataflow/Signal.js
@@ -1,44 +1,30 @@
 define(function(require, exports, module) {
-  var changeset = require('./changeset'),
+  var Node = require('./Node'),
+      changeset = require('./changeset'),
       util = require('../util/index');
 
-  return function(model) {
-    function Signal(name, init) {
-      this._name  = name;
-      this._value = init;
-      this._node  = new model.Node();
-    };
+  function Signal(graph, name, init) {
+    Node.prototype.call(this, graph);
 
-    Signal.prototype.name = function() { return this._name; };
-
-    Signal.prototype.node = function(node) {
-      if(!arguments.length) return this._node;
-      this._node = node;
-      return this;
-    };
-
-    Signal.prototype.value = function(val) {
-      if(!arguments.length) return this._value;
-      this._value = val;
-      return this;
-    };
-
-    Signal.prototype.fire = function() {
-      var c = changeset.create({}, true);
-      c.signals[this._name] = 1;
-      model.graph.propagate(c, this._node);
-    };
-
-    Signal.prototype.addListener = function(l) { 
-      if(l instanceof Signal) l = l.node();
-      this._node.addListener(l); 
-    };
-
-    Signal.prototype.removeListener = function(l) { 
-      if(l instanceof Signal) l = l.node();
-      this._node.removeListener(l); 
-    };
-
-    return Signal;
+    this._name  = name;
+    this._value = init;
   };
+
+  var proto = (Bin.prototype = new Node());
+
+  proto.name = function() { return this._name; };
+
+  proto.value = function(val) {
+    if(!arguments.length) return this._value;
+    this._value = val;
+    return this;
+  };
+
+  proto.fire = function(cs) {
+    if(!cs) cs = changeset.create({}, true);
+    cs.signals[this._name] = 1;
+    this._graph.propagate(cs, this);
+  };
+
+  return Signal;
 })
