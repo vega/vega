f6196dbfbf2448803dcbf3202f6e1acef9b1d7f1
Arvind Satyanarayan <arvindsatya1@gmail.com>
2015-01-22 09:28:45 -0800

    Cleaned up changeset. Renamed touch to reflow.

--- src/dataflow/changeset.js
+++ src/dataflow/changeset.js
@@ -1,10 +1,15 @@
-define(function() {
-  function create(cs, touch) {
+define(function(require, module, exports) {
+  var C = require('../util/constants');
+
+  function create(cs, reflow) {
     var out = {};
     copy(cs, out);
 
-    out.add = [], out.mod = [], out.rem = [];
-    out.touch = touch;
+    out.add = [];
+    out.mod = [];
+    out.rem = [];
+
+    out.reflow = reflow;
 
     return out;
   }
@@ -14,12 +19,11 @@ define(function() {
   }
 
   function copy(a, b) {
-    if(!a) a = {};
-    b.stamp = a.stamp||0;
-    if(a.sort)  b.sort  = a.sort;
-    if(a.facet) b.facet = a.facet;
-    if(a.trans) b.trans = a.trans;
-    ['signals', 'fields', 'data', 'scales'].forEach(function(k) { b[k] = a[k]||{} });
+    b.stamp = a ? a.stamp : 0;
+    b.sort  = a ? a.sort  : null;
+    b.facet = a ? a.facet : null;
+    b.trans = a ? a.trans : null;
+    C.DEPS.forEach(function(d) { b[d] = a ? a[d] : {} });
   }
 
   return {
