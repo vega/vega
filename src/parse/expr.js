3df8fcfeb3e5f1b4a88d0422ccb3982465ab2b7b
Dominik Moritz <domoritz@gmail.com>
2015-02-21 18:15:30 -0800

    expr.js: tabs -> spaces

--- src/parse/expr.js
+++ src/parse/expr.js
@@ -1,36 +1,36 @@
 vg.parse.expr = (function() {
 
   var CONSTANT = {
-  	"E":       "Math.E",
-  	"LN2":     "Math.LN2",
-  	"LN10":    "Math.LN10",
-  	"LOG2E":   "Math.LOG2E",
-  	"LOG10E":  "Math.LOG10E",
-  	"PI":      "Math.PI",
-  	"SQRT1_2": "Math.SQRT1_2",
-  	"SQRT2":   "Math.SQRT2"
+    "E":       "Math.E",
+    "LN2":     "Math.LN2",
+    "LN10":    "Math.LN10",
+    "LOG2E":   "Math.LOG2E",
+    "LOG10E":  "Math.LOG10E",
+    "PI":      "Math.PI",
+    "SQRT1_2": "Math.SQRT1_2",
+    "SQRT2":   "Math.SQRT2"
   };
 
   var FUNCTION = {
-  	"abs":    "Math.abs",
-  	"acos":   "Math.acos",
-  	"asin":   "Math.asin",
-  	"atan":   "Math.atan",
-  	"atan2":  "Math.atan2",
-  	"ceil":   "Math.ceil",
-  	"cos":    "Math.cos",
-  	"exp":    "Math.exp",
+    "abs":    "Math.abs",
+    "acos":   "Math.acos",
+    "asin":   "Math.asin",
+    "atan":   "Math.atan",
+    "atan2":  "Math.atan2",
+    "ceil":   "Math.ceil",
+    "cos":    "Math.cos",
+    "exp":    "Math.exp",
     "floor":  "Math.floor",
     "format": "d3.format",
     "log":    "Math.log",
-  	"max":    "Math.max",
-  	"min":    "Math.min",
-  	"pow":    "Math.pow",
-  	"random": "Math.random",
-  	"round":  "Math.round",
-  	"sin":    "Math.sin",
-  	"sqrt":   "Math.sqrt",
-  	"tan":    "Math.tan"
+    "max":    "Math.max",
+    "min":    "Math.min",
+    "pow":    "Math.pow",
+    "random": "Math.random",
+    "round":  "Math.round",
+    "sin":    "Math.sin",
+    "sqrt":   "Math.sqrt",
+    "tan":    "Math.tan"
   };
 
   var lexer = /([\"\']|[\=\<\>\~\&\|\?\:\+\-\/\*\%\!\^\,\;\[\]\{\}\(\) ]+)/;
