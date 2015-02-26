55f76f527078d0382894429394f86c1106e7ea16
Dominik Moritz <domoritz@gmail.com>
2015-02-25 21:56:51 -0800

    Add direct code evaluation if safeMode is not enabled.

--- src/parse/expr.js
+++ src/parse/expr.js
@@ -5,8 +5,34 @@ vg.parse.expr = (function() {
     idWhiteList: ['d', 'index', 'data']
   });
 
+  var lexer = /([\"\']|[\=\<\>\~\&\|\?\:\+\-\/\*\%\!\^\,\;\[\]\{\}\(\) ]+)/;
+
+  var FUNCTION = vg_expression_functions();
+
   return function(expr) {
-    var code = codegen(parse(expr));
+    var code;
+    if (vg.config.safeMode) {
+      code = codegen(parse(expr));
+    } else {
+      var tokens = expr.split(lexer),
+        t, v, i, n, sq, dq;
+
+      for (sq=0, dq=0, i=0, n=tokens.length; i<n; ++i) {
+        var t = tokens[i];
+        if (t==="'") { if (!dq) sq = !sq; continue; }
+        if (t==='"') { if (!sq) dq = !dq; continue; }
+        if (dq || sq) continue;
+        if (vg_expression_constants[t]) {
+          tokens[i] = vg_expression_constants[t];
+        }
+        if (FUNCTION[t] && (v=tokens[i+1]) && v[0]==="(") {
+          tokens[i] = FUNCTION[t];
+        }
+      }
+
+      code = tokens.join("");
+    }
+
     return Function('d', 'index', 'data',
       '"use strict"; return (' + code + ');');
   };
