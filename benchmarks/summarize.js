9e4015140c33fa53eb702a32b80fdac2c3461f35
Arvind Satyanarayan <arvindsatya1@gmail.com>
2015-03-19 20:47:42 -0700

    Summary script for benchmark results

---
+++ benchmarks/summarize.js
@@ -0,0 +1,31 @@
+var fs  = require('fs'),
+    stats = require('summary'),
+    benchmarks = [/*'alwaysPrev', 'neverPrev', 'sometimesPrev'*/]
+      // .concat(['noSkips', 'onlyHardSkips', 'onlySoftSkips', 'hard+softSkips'])
+      // .concat(['sct', 'pcp', 'trellis'])
+      .concat(['scatter', 'pcp', 'trellis']),
+    conditions = ['100.5', '1000.50', '10000.500', '100000.500'];
+
+benchmarks.forEach(function(b) {
+  var summaries = {};
+
+  conditions.forEach(function(c) {
+    var summary = summaries[c] = {},
+        results = JSON.parse(fs.readFileSync('results.raw/'+b+'.'+c+'.json').toString());
+
+    // Compile the types + times
+    results.forEach(function(r) {
+      var type = r.type.replace(b+'.'+c+' ', '');
+      summary[type] = summary[type] || [];
+      summary[type].push(r.size || r.time);
+    });
+
+    // Calculate stats
+    Object.keys(summary).forEach(function(s) {
+      var data = stats(summary[s]);
+      summary[s] = {mean: data.mean(), sd: data.sd()}
+    });
+  });
+
+  fs.writeFileSync('results/'+b+'.json', JSON.stringify(summaries, null, 2));
+})
