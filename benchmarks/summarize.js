1ed3997c42c34616311c98bc3496222ebd4f2c30
Arvind Satyanarayan <arvindsatya1@gmail.com>
2015-03-25 21:20:36 -0700

    Create folded and unfolded summaries for readability; report heap size in MB.

--- benchmarks/summarize.js
+++ benchmarks/summarize.js
@@ -1,29 +1,28 @@
 var fs  = require('fs'),
     stats = require('summary'),
-    benchmarks = [/*'alwaysPrev', 'neverPrev', 'sometimesPrev'*/]
-      // .concat(['noSkips', 'onlyHardSkips', 'onlySoftSkips', 'hard+softSkips'])
-      // .concat(['sct', 'pcp', 'trellis'])
-      .concat(['scatter', 'pcp', 'trellis']),
-    conditions = ['100.5', '1000.50', '10000.500', '100000.500'];
+    benchmarks = ['prev', 'skips', 'scatter', 'pcp', 'trellis'],
+    conditions = ['100.5', '1000.50', '10000.500', '100000.5000'];
 
 benchmarks.forEach(function(b) {
-  var summaries = [];
+  var summaries = {},
+      folded = [];
 
   conditions.forEach(function(c) {
-    var summary = {},
+    var summary = summaries[c] = {},
         results = JSON.parse(fs.readFileSync('results.raw/'+b+'.'+c+'.json').toString());
 
     // Compile the types + times
     results.forEach(function(r) {
       var type = r.type.replace(b+'.'+c+' ', '');
       summary[type] = summary[type] || [];
-      summary[type].push(r.size || r.time);
+      summary[type].push(r.size ? r.size/(1024*1024) : r.time);
     });
 
     // Calculate stats
     Object.keys(summary).forEach(function(s) {
       var data = stats(summary[s]);
-      summaries.push({
+      summary[s] = {mean: data.mean(), sd: data.sd()};
+      folded.push({
         condition: s.split(' ')[0],
         operation: s.split(' ').splice(1).join(' '),
         N: +c.split('.')[0],
@@ -35,4 +34,5 @@ benchmarks.forEach(function(b) {
   });
 
   fs.writeFileSync('results/'+b+'.json', JSON.stringify(summaries, null, 2));
+  fs.writeFileSync('results/'+b+'.folded.json', JSON.stringify(folded, null, 2));
 })
