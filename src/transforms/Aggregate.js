b69935ec309b59f9f00079541c29847a03ac612d
Dominik Moritz <domoritz@gmail.com>
2015-04-13 17:01:37 -0700

    Get started on adding support for aggregating multiple fields.

--- src/transforms/Aggregate.js
+++ src/transforms/Aggregate.js
@@ -9,8 +9,7 @@ var Transform = require('./Transform'),
 function Aggregate(graph) {
   GroupBy.prototype.init.call(this, graph);
   Transform.addParameters(this, {
-    group_by: {type: "array<field>"},
-    on: {type: "field"} 
+    group_by: {type: "array<field>"}
   });
 
   this._output = {
@@ -27,8 +26,8 @@ function Aggregate(graph) {
     "median":   "median"
   };
 
-  // Measures parameter handled manually.
-  this._Measures = null;
+  // Aggregators parameter handled manually.
+  this._Aggregators = [];
 
   // The group_by might come via the facet. Store that to 
   // short-circuit usual GroupBy methods.
@@ -39,19 +38,32 @@ function Aggregate(graph) {
 
 var proto = (Aggregate.prototype = new GroupBy());
 
-proto.measures = { 
-  set: function(transform, aggs) {
-    if(aggs.indexOf(C.COUNT) < 0) aggs.push(C.COUNT); // Need count for correct GroupBy propagation.
-    transform._Measures = meas.create(aggs.map(function(a) { 
-      return meas[a](transform._output[a]); 
-    }));
+proto.fields = { 
+  set: function(transform, fields) {
+    for (var i = 0; i < fields.length; i++) {
+      var f = fields[i];
+      if(f.ops.indexOf(C.COUNT) < 0) f.ops.push(C.COUNT); // Need count for correct GroupBy propagation.
+      transform._Aggregators.push({
+        accessor: util.accessor(f.name),
+        field: f.name,
+        measures: meas.create(f.ops.map(function(a) { 
+          return meas[a](transform._output[a]);
+        }))
+      });
+    }
     return transform;
   }
 };
 
 proto._reset = function(input, output) {
-  var k, c
-  for(k in this._cells) { 
+  var k, c;
+  console.log('reset');
+  // rebuild accessors
+  for(var i = 0; i < this._Aggregators.length; i++) {
+    var f = this._Aggregators[i];
+    f.accessor = util.accessor(f.name);
+  }
+  for(k in this._cells) {
     if(!(c = this._cells[k])) continue;
     if(!input.facet) output.rem.push(c.set());
   }
@@ -59,12 +71,14 @@ proto._reset = function(input, output) {
 };
 
 proto._keys = function(x) {
+  console.log('keys', x);
   if(this.__facet) return this.__facet;
   else if(this._refs.length) return GroupBy.prototype._keys.call(this, x);
   return {keys: [], key: ""}; // Aggregate on a flat datasource
 };
 
 proto._new_cell = function(x, k) {
+  console.log('new', x, k);
   var group_by = this.group_by.get(this._graph),
       fields = group_by.fields, acc = group_by.accessors,
       i, len;
@@ -77,21 +91,29 @@ proto._new_cell = function(x, k) {
     t = tuple.ingest(t, null);
   }
 
+  console.log(t);
+
   return new this._Measures(t);
 };
 
 proto._add = function(x) {
-  var field = this.on.get(this._graph).accessor;
-  this._cell(x).add(field(x));
+  for(var i = 0; i < this._Aggregators.length; i++) {
+    var field = this._Aggregators[i].accessor;
+    this._cell(x).add(field(x));
+  }
 };
 
 proto._rem = function(x) {
-  var field = this.on.get(this._graph).accessor;
-  this._cell(x).rem(field(x));
+  for(var i = 0; i < this._Aggregators.length; i++) {
+    var field = this._Aggregators[i].accessor;
+    this._cell(x).rem(field(x));
+  }
 };
 
 proto.transform = function(input, reset) {
   util.debug(input, ["aggregate"]);
+
+  console.log('transform');
 
   if(input.facet) {
     this.__facet = input.facet;
--- test/transforms/aggregate.js
+++ test/transforms/aggregate.js
@@ -19,7 +19,7 @@ describe('Aggregate', function() {
         "data": [{ 
           "name": "table", 
           "values": values,
-          "transform": [{"type": "aggregate", "on": "y", "measures": meas }]
+          "transform": [{"type": "aggregate", "fields": [{"name": "y", "ops": meas}]}]
         }] 
       };
     }
@@ -303,7 +303,36 @@ describe('Aggregate', function() {
     });
   });
 
-  it('should calculate stats on facets', function(done) {
+  it.only('should calculate multiple aggregations', function(done) {
+    var values = [
+      {"country":"US", "state": "washington", "area": 12, "population": 3},
+      {"country":"US", "state": "california", "area": 13, "population": 12},
+      {"country":"US", "state": "new york", "area": 3, "population": 7},
+      {"country":"Canada", "state": "british area", "count": 4, "population": 4},
+      {"country":"Canada", "state": "yukon", "area": 3, "population": 2}
+    ];
+
+    var spec = {
+      "data": [{
+        "name": "table", 
+        "values": values,
+        "transform": [{"type": "aggregate", "group_by": "country",
+          "fields": [{"name": "area", "ops": ["sum"]}, {"name": "population", "ops": ["sum"]}]}]
+      }]
+    };
+
+    parseSpec(spec, function(model) {
+      var ds = model.data('table'),
+          data = ds.values();
+
+      expect(data).to.have.length(2);
+
+      console.log(data);
+      done();
+    }, modelFactory);
+  });
+
+  it.skip('should calculate stats on facets', function(done) {
     var values = [
       {"country":"US", "type": "gold", "count": 12},
       {"country":"US", "type": "silver", "count": 13},
