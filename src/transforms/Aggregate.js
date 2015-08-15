6ffa0cb67a6ec0c692db97a3fde82e26de766208
Jeffrey Heer <jeffrey.heer@gmail.com>
2015-08-14 22:39:33 -0700

    Revise Aggregate, remove prev spoofing.

--- src/transforms/Aggregate.js
+++ src/transforms/Aggregate.js
@@ -15,7 +15,8 @@ function Aggregate(graph) {
     summarize: {
       type: 'custom', 
       set: function(summarize) {
-        var signals = {},
+        var signalDeps = {},
+            tx = this._transform,
             i, len, f, fields, name, ops;
 
         if (!dl.isArray(fields = summarize)) { // Object syntax from dl
@@ -26,33 +27,29 @@ function Aggregate(graph) {
           }
         }
 
-        function sg(x) { if (x.signal) signals[x.signal] = 1; }
+        function sg(x) { if (x.signal) signalDeps[x.signal] = 1; }
 
         for (i=0, len=fields.length; i<len; ++i) {
           f = fields[i];
-          if (f.field.signal) signals[f.field.signal] = 1;
+          if (f.field.signal) { signalDeps[f.field.signal] = 1; }
           dl.array(f.ops).forEach(sg);
           dl.array(f.as).forEach(sg);
         }
 
-        this._transform._fieldsDef = fields;
-        this._transform._aggr = null;
-        this._transform.dependency(Deps.SIGNALS, dl.keys(signals));
-        return this._transform;
+        tx._fields = fields;
+        tx._aggr = null;
+        tx.dependency(Deps.SIGNALS, dl.keys(signalDeps));
+        return tx;
       }
     }
   });
 
-  this._fieldsDef = [];
+  this._fields = [];
   this._aggr = null;  // dl.Aggregator
 
   this._type = TYPES.TUPLE; 
   this._acc = {groupby: dl.true, value: dl.true};
   this._cache = {}; // And cache them as aggregators expect original tuples.
-
-  // Aggregator needs a full instantiation of the previous tuple.
-  // Cache them to reduce creation costs.
-  this._prev = {}; 
 
   return this.router(true).revises(true);
 }
@@ -101,43 +98,37 @@ function standardize(x) {
 prototype.aggr = function() {
   if (this._aggr) return this._aggr;
 
-  var graph = this._graph,
-      groupby = this.param('groupby').field;
-
-  var fields = this._fieldsDef.map(function(field) {
-    var f = dl.duplicate(field);
-    if (field.get) f.get = field.get;
-
-    f.name = f.field.signal ? graph.signalRef(f.field.signal) : f.field;
-    f.ops  = f.ops.signal ? graph.signalRef(f.ops.signal) :
-      dl.array(f.ops).map(function(o) {
-        return o.signal ? graph.signalRef(o.signal) : o;
+  var g = this._graph,
+      groupby = this.param('groupby').field,
+      value = function(x) { return x.signal ? g.signalRef(x.signal) : x; },
+      fields = this._fields.map(function(f) {
+        return {
+          name: value(f.field),
+          as:   dl.array(f.as),
+          ops:  dl.array(value(f.ops)).map(value),
+          get:  f.get
+        };
       });
 
-    return f;
-  });
-
-  if (!fields.length) fields = {'*':'values'};
+  if (!fields.length) fields = {'*': 'values'};
 
   var aggr = this._aggr = new Facetor()
     .groupby(groupby)
     .stream(true)
     .summarize(fields);
 
-  if (this._type !== TYPES.VALUE) aggr.key('_id');
+  if (this._type !== TYPES.VALUE) { aggr.key('_id'); }
   return aggr;
 };
 
 prototype._reset = function(input, output) {
-  output.rem.push.apply(output.rem, this.aggr().result());
-  this.aggr().clear();
+  var aggr = this.aggr(),
+      rem = output.rem;
+
+  rem.push.apply(rem, aggr.result());
+  aggr.clear();
   this._aggr = null;
 };
-
-function spoof_prev(x) {
-  var prev = this._prev[x._id] || (this._prev[x._id] = Object.create(x));
-  return dl.extend(prev, x._prev);
-}
 
 prototype.transform = function(input, reset) {
   log.debug(input, ['aggregate']);
@@ -153,22 +144,27 @@ prototype.transform = function(input, reset) {
     aggr._add(tpl ? x : standardize.call(t, x));
   });
 
-  input.mod.forEach(function(x) {
-    if (reset) {
-      // Signal change triggered reflow
+  if (reset) {
+    // Signal change triggered reflow, so add everything
+    input.mod.forEach(function(x) {
       aggr._add(tpl ? x : standardize.call(t, x));
-    } else {
-      var y = Tuple.has_prev(x) ? spoof_prev.call(t, x) : x;
+    });
+  } else {
+    input.mod.forEach(function(x) {
+      var y = Tuple.prev(x);
       aggr._mod(tpl ? x : standardize.call(t, x), 
         tpl ? y : standardize.call(t, y));
-    }
-  });
+    });
+  }
 
-  input.rem.forEach(function(x) {
-    var y = Tuple.has_prev(x) ? spoof_prev.call(t, x) : x;
-    aggr._rem(tpl ? y : standardize.call(t, y));
-    t._cache[x._id] = t._prev[x._id] = null;
-  });
+  if (!reset) {
+    // not necessary upon reset, as we clear the aggregator
+    input.rem.forEach(function(x) {
+      var y = Tuple.prev(x);
+      aggr._rem(tpl ? y : standardize.call(t, y));
+      t._cache[x._id] = null;
+    });
+  }
 
   return aggr.changes(input, output);
 };
