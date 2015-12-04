bd5ea508ad68e2f53f71b2c515fd5124a27881af
Ryan Russell <russellsprouts2@gmail.com>
2015-12-04 15:54:57 -0800

    Add inrange in vega proper

--- src/parse/expr.js
+++ src/parse/expr.js
@@ -2,16 +2,30 @@ var expr = require('vega-expression'),
     args = ['datum', 'event', 'signals'];
 
 module.exports = expr.compiler(args, {
-  idWhiteList: args,
-  fieldVar:    args[0],
-  globalVar:   args[2],
-  functions:   function(codegen) {
+  idWhiteList:  args,
+  fieldVar:     args[0],
+  globalVar:    args[2],
+  functions:    function(codegen) {
     var fn = expr.functions(codegen);
     fn.eventItem = function() { return 'event.vg.item'; };
     fn.eventGroup = 'event.vg.getGroup';
     fn.eventX = 'event.vg.getX';
     fn.eventY = 'event.vg.getY';
     fn.open = 'window.open';
+    fn.inrange = 'this.defs.inrange';
     return fn;
+  },
+  functionDefs: function(codegen) {
+    return {
+      'inrange': inrange
+    };
   }
 });
+
+function inrange(val, a, b, exclusive) {
+  var min = a, max = b;
+  if (a > b) { min = b; max = a; }
+  return exclusive ?
+    (min < val && max > val) :
+    (min <= val && max >= val);
+}
