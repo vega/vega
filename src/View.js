80efa617017ac346a95baadfb0d6a442926d409e
Jeffrey Heer <jeffrey.heer@gmail.com>
2016-07-12 23:40:42 -0700

    Add headless rendering. Add touch events.

--- src/View.js
+++ src/View.js
@@ -1,6 +1,16 @@
 import * as transforms from './transforms/index';
-import {Dataflow, EventStream, changeset, inherits} from 'vega-dataflow';
-import {parse, context} from 'vega-runtime';
+
+import {
+  Dataflow,
+  EventStream,
+  changeset,
+  inherits
+} from 'vega-dataflow';
+
+import {
+  parse,
+  context
+} from 'vega-runtime';
 
 import {
   CanvasRenderer,
@@ -12,12 +22,26 @@ import {
   point
 } from 'vega-scenegraph';
 
+// constants
+var CANVAS = 'canvas',
+    PNG = 'png',
+    SVG = 'svg';
+
+/**
+ * Create a new View instance from a Vega dataflow runtime specification.
+ * The generated View will not immediately be ready for display. Callers
+ * should also invoke the initialize method (e.g., to set the parent
+ * DOM element in browser-based deployment) and then invoke the run
+ * method to evaluate the dataflow graph. Rendering will automatically
+ * be peformed upon dataflow runs.
+ * @constructor
+ * @param {object} runtimeSpec - The Vega dataflow runtime specification.
+ */
 export default function View(runtimeSpec) {
   Dataflow.call(this);
 
   this._el = null;
-  this._io = null;
-  this._renderType = 'canvas';
+  this._renderType = CANVAS;
   this._loadConfig = {};
   this._scenegraph = new Scenegraph();
   var root = this._scenegraph.root;
@@ -39,7 +63,8 @@ export default function View(runtimeSpec) {
     changeset().insert(root.items)
   );
 
-  // initialize padding
+  // initialize background, padding
+  this._backgroundColor = null;
   this._padding = {top:5, left:5, bottom:5, right:5};
 
   // initialize resize operator
@@ -51,11 +76,12 @@ export default function View(runtimeSpec) {
 
 var prototype = inherits(View, Dataflow);
 
-// -- CONFIGURATION -----
+// -- INITIALIZATION -----
 
 prototype.initialize = function(el) {
   var view = this;
 
+  // containing dom element
   if (el) {
     if (typeof el === 'string' && typeof document !== 'undefined') {
       el = document.querySelector(el);
@@ -66,48 +92,54 @@ prototype.initialize = function(el) {
     view._el = null; // headless
   }
 
-  // TODO: settings...
-  var w = view.width();
-  var h = view.height();
-  var backgroundColor = null;
-  var pad = view._padding;
-  var loadConfig = view._loadConfig;
-  var io = (view._io = getIO(view._renderType, el));
-
-  // renderer
-  view._renderer = (view._renderer || new io.Renderer(loadConfig))
-    .initialize(el, w, h, pad)
-    .background(backgroundColor);
-
-  // input handler
-  var prevHandler = view._handler;
-
-  view._handler = new io.Handler()
-    .scene(view.scenegraph().root)
-    .initialize(el, pad, view);
-
-  if (prevHandler) {
-    prevHandler.handlers().forEach(function(h) {
-      view._handler.on(h.type, h.handler);
-    });
-  }
+  // renderer and input handler
+  var io = getIO(view._renderType, el);
+  view._renderer = initializeRenderer(view, view._renderer, el, io.Renderer);
+  view._handler = initializeHandler(view, view._handler, el, io.Handler);
 
   return view;
 };
 
+function initializeRenderer(view, r, el, constructor) {
+  r = r || new constructor(view._loadConfig);
+  return r
+    .initialize(el, view.width(), view.height(), view._padding)
+    .background(view._backgroundColor);
+}
+
+function initializeHandler(view, prevHandler, el, constructor) {
+  var handler = new constructor()
+    .scene(view.scenegraph().root)
+    .initialize(el, view._padding, view);
+
+  if (prevHandler) {
+    prevHandler.handlers().forEach(function(h) {
+      handler.on(h.type, h.handler);
+    });
+  }
+
+  return handler;
+}
+
 function getIO(type, el) {
   var r = CanvasRenderer,
       h = CanvasHandler;
-  if (type === 'svg') {
+  if (type === SVG) {
     r = el ? SVGRenderer : SVGStringRenderer;
     h = SVGHandler;
   }
   return {Renderer: r, Handler: h};
 }
 
+// -- SCENEGRAPH and RENDERING -----
+
+prototype.scenegraph = function() {
+  return this._scenegraph;
+};
+
 prototype.renderer = function(type) {
   if (!arguments.length) return this._renderType;
-  if (type !== 'svg') type = 'canvas';
+  if (type !== SVG) type = CANVAS;
   if (type !== this._renderType) {
     this._renderType = type;
     if (this._renderer) {
@@ -116,25 +148,6 @@ prototype.renderer = function(type) {
     }
   }
   return this;
-};
-
-// -- SCENEGRAPH and RENDERING -----
-
-prototype.resize = function(width, height) {
-  var w = this.signal('width'),
-      h = this.signal('height');
-
-  if (w === width && h === height) {
-    this._renderer.resize(width, height, this._padding);
-  } else {
-    this.signal('width', width).signal('height', height).run();
-  }
-
-  return this;
-};
-
-prototype.scenegraph = function() {
-  return this._scenegraph;
 };
 
 prototype.render = function(items) {
@@ -147,6 +160,117 @@ prototype.renderQueue = function(items) {
   }
 };
 
+/**
+ * Generate an image URL for the visualization. Depending on the type
+ * parameter, the generated URL contains data for either a PNG or SVG image.
+ * The URL can be used (for example) to download images of the visualization.
+ * This method is asynchronous, with the generated url returned via a callback
+ * function (the underlying renderer will complete image loading, if needed,
+ * before returning).
+ * @param {string} type - The image type. One of 'svg', 'png' or 'canvas'.
+ *   The 'canvas' and 'png' types are synonyms for a PNG image.
+ * @param {function} callback - A callback function for returning the
+ *   error state (first argument) or image url (second argument).
+ */
+prototype.toImageURL = function(type, callback) {
+  if (type === PNG) type = CANVAS;
+
+  if (type !== SVG && type !== CANVAS) {
+    callback('Unrecognized image type: ' + type);
+  } else {
+    renderHeadless(this, type, function(error, renderer) {
+      if (error) { callback(error); return; }
+      try {
+        callback(null, type === CANVAS
+          ? renderer.canvas().toDataURL('image/png')
+          : window.URL.createObjectURL(
+              new Blob([renderer.svg()], {type: 'image/svg+xml'}))
+        );
+      } catch (error) {
+        callback(error);
+      }
+    });
+  }
+};
+
+/**
+ * Returns a rendered SVG string of the visualization. This method is
+ * asynchronous, with the generated url returned via a callback function
+ * (the underlying renderer will complete image loading before returning).
+ * @param {function} callback - A callback function for returning the
+ *   error state (first argument) or svg string (second argument).
+ */
+prototype.toSVG = function(callback) {
+  renderHeadless(this, SVG, function(error, renderer) {
+    callback(error, renderer ? renderer.svg() : undefined);
+  });
+};
+
+/**
+ * Returns a Canvas instance containing a rendered visualization.
+ * This method is asynchronous, with the generated url returned via a
+ * callback function (the underlying renderer will complete image loading
+ * before returning). If this View is using a canvas renderer, the
+ * returned Canvas instance will be the same as the live instance used
+ * by the renderer, and thus subject to change over time. Any
+ * methods for exporting canvas state should be called immediately.
+ * @param {function} callback - A callback function for returning the
+ *   error state (first argument) or Canvas instance (second argument).
+ */
+prototype.toCanvas = function(callback) {
+  renderHeadless(this, CANVAS, function(error, renderer) {
+    callback(error, renderer ? renderer.canvas() : undefined);
+  });
+};
+
+/**
+ * Render the current scene in a headless fashion.
+ */
+function renderHeadless(view, type, callback) {
+  var renderer = view._renderer,
+      scaled = type === CANVAS
+            && typeof window !== 'undefined'
+            && window.devicePixelRatio > 1;
+
+  if (scaled || !renderer || view.renderer() !== type) {
+    renderer = initializeRenderer(view, null, null,
+      (type === SVG) ? SVGStringRenderer : CanvasRenderer);
+  }
+
+  renderAsync(renderer, view._scenegraph.root, callback);
+}
+
+/**
+ * Perform asynchronous rendering and return the renderer via callback.
+ */
+function renderAsync(renderer, scene, callback) {
+  var redraw = false;
+
+  function poll() {
+    if (renderer.pendingImages() === 0) {
+      try {
+        if (redraw) renderer.render(scene);
+        callback(null, renderer);
+      } catch (error) {
+        callback(error);
+      }
+    } else {
+      redraw = true;
+      setTimeout(poll, 10);
+    }
+  }
+
+  try {
+    renderer.render(scene);
+  } catch (error) {
+    callback(error);
+  }
+
+  poll();
+}
+
+// -- DATAFLOW METHODS -----
+
 prototype.run = function() {
   Dataflow.prototype.run.call(this);
   if (!this._queue || this._queue.length) {
@@ -154,15 +278,26 @@ prototype.run = function() {
     this._queue = [];
   }
   return this;
-}
-
-// -- DATAFLOW METHODS -----
+};
 
 prototype.signal = function(name, value, options) {
   var op = this._signals[name];
   return arguments.length === 1
     ? (op ? op.value : undefined)
     : this.update(op, value, options);
+};
+
+prototype.resize = function(width, height) {
+  var w = this.signal('width'),
+      h = this.signal('height');
+
+  if (w === width && h === height) {
+    this._renderer.resize(width, height, this._padding);
+  } else {
+    this.signal('width', width).signal('height', height).run();
+  }
+
+  return this;
 };
 
 prototype.width = function(value) {
@@ -217,6 +352,10 @@ prototype.events = function(source, type, filter) {
 
 /**
  * Extend an event with additional view-specific information.
+ * Adds properties for this view ('view') and any actively selected
+ * item ('item'). If this view has a containing DOM element, also
+ * adds ('viewX', 'viewY') properties indicating the primary mouse or
+ * touch coordinates relative to the view's coordinate system.
  * @param {Event} event - The input event to extend.
  * @param {Item} item - The currently selected scenegraph item (if any).
  * @return {Event} - The extended input event.
@@ -225,9 +364,10 @@ prototype.extendEvent = function(event, item) {
   event.dataflow = this;
   event.item = item;
 
-  var el = this._renderer.element(), p;
+  var el = this._renderer.element(), e, p;
   if (el) {
-    p = point(event, el);
+    e = event.changedTouches ? event.changedTouches[0] : event;
+    p = point(e, el);
     event.viewX = p[0] - this._padding.left;
     event.viewY = p[1] - this._padding.top;
   }
