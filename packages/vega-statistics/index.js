156159758401dcc3d218318d27ab914d9ef307f2
Jeffrey Heer <jeffrey.heer@gmail.com>
2016-07-15 09:53:56 -0700

    Add quartiles to exports.

--- packages/vega-statistics/index.js
+++ packages/vega-statistics/index.js
@@ -3,3 +3,4 @@ export {default as bootstrapCI} from './src/bootstrapCI';
 export {default as randomInteger} from './src/integer';
 export {default as randomNormal} from './src/normal';
 export {default as randomUniform} from './src/uniform';
+export {default as quartiles} from './src/quartiles';
