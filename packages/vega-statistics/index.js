947d7f063775255270e606ea693691b74e634dee
Jeffrey Heer <jeffrey.heer@gmail.com>
2016-07-15 09:30:05 -0700

    Add bin function.

--- packages/vega-statistics/index.js
+++ packages/vega-statistics/index.js
@@ -1,3 +1,4 @@
+export {default as bin} from './src/bin';
 export {default as bootstrapCI} from './src/bootstrapCI';
 export {default as randomInteger} from './src/integer';
 export {default as randomNormal} from './src/normal';
