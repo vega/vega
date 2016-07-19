f66bed67e648899cc53600de4e9a32748c9d0acc
Jeffrey Heer <jeffrey.heer@gmail.com>
2016-07-19 12:22:06 -0700

    Add expression functions.

--- src/view/runtime.js
+++ src/view/runtime.js
@@ -12,13 +12,40 @@ import {
   hsl
 } from 'd3-color';
 
-var encode = {
-  rgb: rgb,
-  lab: lab,
-  hcl: hcl,
-  hsl: hsl
-};
+function functions(fn, ctx) {
+  fn.rgb = rgb;
+  fn.lab = lab;
+  fn.hcl = hcl;
+  fn.hsl = hsl;
 
-export default function(dataflow, spec) {
-  return parse(spec, context(dataflow, transforms, encode));
+  fn.scale = function(name, value) {
+    var s = ctx.scales[name];
+    return s ? s.value(value) : undefined;
+  };
+
+  fn.scaleInvert = function(name, value) {
+    var s = ctx.scales[name];
+    return s ? s.value.invert(value) : undefined;
+  };
+
+  fn.scaleCopy = function(source, target) {
+    var s = ctx.scales[source],
+        t = ctx.scales[target];
+    return (s && t)
+      ? null // TODO: perform copy
+      : undefined;
+  };
+
+  fn.indata = function(name, field, value) {
+    var index = ctx.data['index:' + field];
+    return index ? !!index.value.get(value) : undefined;
+  };
+
+  return ctx;
+}
+
+export default function(view, spec) {
+  var fn = {},
+      ctx = context(view, transforms, fn);
+  return parse(spec, functions(fn, ctx));
 }
