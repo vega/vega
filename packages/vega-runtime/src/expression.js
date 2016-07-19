e42b76c5770d70e090c37e3757295d50a762c15a
Jeffrey Heer <jeffrey.heer@gmail.com>
2016-07-19 10:31:39 -0700

    Streamline expression parsing.

--- packages/vega-runtime/src/expression.js
+++ packages/vega-runtime/src/expression.js
@@ -1,43 +1,46 @@
 /**
  * Parse an expression given the argument signature and body code.
  */
-export default function parseExpression(args, code, context) {
-  var fn = Function.apply(null, args.concat('return(' + code + ');'));
+export default function expression(args, code, context) {
+  // wrap code in return statement if expression does not terminate
+  if (code[code.length-1] !== ';') {
+    code = 'return(' + code + ');';
+  }
+  var fn = Function.apply(null, args.concat(code));
   return context ? fn.bind(context) : fn;
-}
-
-/**
- * Parse an expression applied to an event stream.
- */
-export function eventExpression(code, context) {
-  return parseExpression(['event'], code, context);
-}
-
-/**
- * Parse an expression used to handle an event-driven operator update.
- */
-export function handlerExpression(code, context) {
-  return parseExpression(['_', 'event'], code, context);
 }
 
 /**
  * Parse an expression used to update an operator value.
  */
 export function operatorExpression(code, context) {
-  return parseExpression(['_'], code, context);
+  return expression(['_'], code, context);
 }
 
 /**
  * Parse an expression provided as an operator parameter value.
  */
 export function parameterExpression(code, context) {
-  return parseExpression(['datum', '_'], code, context);
+  return expression(['datum', '_'], code, context);
+}
+
+/**
+ * Parse an expression applied to an event stream.
+ */
+export function eventExpression(code, context) {
+  return expression(['event'], code, context);
+}
+
+/**
+ * Parse an expression used to handle an event-driven operator update.
+ */
+export function handlerExpression(code, context) {
+  return expression(['_', 'event'], code, context);
 }
 
 /**
  * Parse an expression that performs visual encoding.
  */
 export function encodeExpression(code, context) {
-  var fn = Function.apply(null, ['item', '_', code]);
-  return context ? fn.bind(context) : fn;
+  return expression(['item', '_'], code, context);
 }
