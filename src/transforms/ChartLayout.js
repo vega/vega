a9fd7189b8ca4a799980f67934306779fce63bb6
Jeffrey Heer <jeffrey.heer@gmail.com>
2016-07-26 15:21:56 -0700

    Add legends.

--- src/transforms/ChartLayout.js
+++ src/transforms/ChartLayout.js
@@ -18,11 +18,13 @@ var prototype = inherits(ChartLayout, Transform);
 
 prototype.transform = function(_, pulse) {
   // TODO incremental update, output
-  _.mark.items.forEach(layoutGroup);
+  _.mark.items.forEach(function(group) {
+    layoutGroup(group, _);
+  });
   return pulse;
 };
 
-function layoutGroup(group) {
+function layoutGroup(group, _) {
   var items = group.items,
       width = group.width,
       height = group.height,
@@ -43,10 +45,15 @@ function layoutGroup(group) {
     bounds.union(layoutAxis(axisMarks[i], width, height));
   }
 
-  // TODO layout legends
-  // for (i=0, n=legendMarks.length; i<n; ++i) {
-  //   layoutLegend(legendMarks[i], width, height);
-  // }
+  // layout legends
+  var flow = {
+    left:   0,
+    right:  0,
+    margin: _.legendMargin || 8
+  };
+  for (i=0, n=legendMarks.length; i<n; ++i) {
+    layoutLegend(legendMarks[i], flow, bounds, width, height);
+  }
 
   // TODO automatic size adjustment
 }
@@ -54,16 +61,16 @@ function layoutGroup(group) {
 function layoutAxis(axis, width, height) {
   var item = axis.items[0],
       datum = item.datum,
-      orient = datum.orient || 'left',
+      orient = datum.orient,
       ticksIndex = datum.grid ? 1 : 0,
       labelIndex = ticksIndex + 1,
-      titleIndex = labelIndex + datum.domain ? 1 : 0,
+      titleIndex = labelIndex + (datum.domain ? 2 : 1),
       offset = item.offset,
       minExtent = item.minExtent,
       maxExtent = item.maxExtent,
       title = datum.title && item.items[titleIndex].items[0],
       titlePadding = item.titlePadding,
-      titleSize = title ? (title.fontSize || 14) + titlePadding : 0,
+      titleSize = title ? title.fontSize + titlePadding : 0,
       bounds = tempBounds,
       x, y, s;
 
@@ -119,12 +126,61 @@ function layoutAxis(axis, width, height) {
       break;
     }
   }
+
   item.x = x;
   item.y = y;
 
   return bounds;
 }
 
-// TODO legend layout
-// function layoutLegend(legend, width, height) {
-// }
+function layoutLegend(legend, flow, axisBounds, width, height) {
+  var item = legend.items[0],
+      datum = item.datum,
+      orient = datum.orient,
+      offset = item.offset,
+      bounds = item.bounds.clear(),
+      x = 0.5,
+      y = 0.5 + (flow[orient] || 0),
+      w, h;
+
+  item.items.forEach(function(_) { bounds.union(_.bounds); });
+  w = Math.ceil(bounds.width()) + item.padding * 2;
+  h = Math.ceil(bounds.height()) + item.padding * 2;
+
+  switch (orient) {
+    case 'left':
+      x -= w + offset - Math.floor(axisBounds.x1);
+      flow.left += h + flow.margin;
+      break;
+    case 'right':
+      x += offset + Math.ceil(axisBounds.x2);
+      flow.right += h + flow.margin;
+      break;
+    case 'top-left':
+      x += offset;
+      y += offset;
+      break;
+    case 'top-right':
+      x += width - w - offset;
+      y += offset;
+      break;
+    case 'bottom-left':
+      x += offset;
+      y += height - h - offset;
+      break;
+    case 'bottom-right':
+      x += width - w - offset;
+      y += height - h - offset;
+      break;
+  }
+
+  // update legend layout
+  item.x = x;
+  item.y = y;
+  item.width = w;
+  item.height = h;
+
+  // update bounds
+  bounds.set(x, y, x + w, y + h);
+  item.mark.bounds.clear().union(bounds);
+}
