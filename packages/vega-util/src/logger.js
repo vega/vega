a4f9626f9e55912de9b3d724cd3fa746a15fd3ea
Jeffrey Heer <jeffrey.heer@gmail.com>
2016-08-06 22:23:51 -0700

    Route warning messages to console.warn.

--- packages/vega-util/src/logger.js
+++ packages/vega-util/src/logger.js
@@ -1,6 +1,6 @@
-function log(level, msg) {
-  var args = [level].concat([].slice.call(msg));
-  console.log.apply(console, args); // eslint-disable-line no-console
+function log(method, level, input) {
+  var args = [level].concat([].slice.call(input));
+  console[method].apply(console, args); // eslint-disable-line no-console
 }
 
 export var None  = 0;
@@ -11,9 +11,20 @@ export var Debug = 3;
 export default function(_) {
   var level = _ || None;
   return {
-    level: function(_) { return arguments.length ? (level = +_, this) : level; },
-    warn: function()   { if (level >= Warn) log('WARN', arguments); return this; },
-    info: function()   { if (level >= Info) log('INFO', arguments); return this; },
-    debug: function()  { if (level >= Debug) log('DEBUG', arguments); return this; }
+    level: function(_) {
+      return arguments.length ? (level = +_, this) : level;
+    },
+    warn: function() {
+      if (level >= Warn) log('warn', 'WARN', arguments);
+      return this;
+    },
+    info: function() {
+      if (level >= Info) log('log', 'INFO', arguments);
+      return this;
+    },
+    debug: function() {
+      if (level >= Debug) log('log', 'DEBUG', arguments);
+      return this;
+    }
   }
 }
