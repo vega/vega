f5c97c439e0b0bf4ee48d52586a4e5576d39371a
Jeffrey Heer <jeffrey.heer@gmail.com>
2016-08-08 09:27:53 -0700

    Allow expressions in encoders.

--- packages/vega-parser/src/parsers/encode/rule.js
+++ packages/vega-parser/src/parsers/encode/rule.js
@@ -1,24 +1,16 @@
 import entry from './entry';
 import set from './set';
-import expression from '../expression';
-import {extend} from 'vega-util';
+import expression from './expression';
 
 export default function(channel, rules, scope, params, fields) {
   var code = '';
 
   rules.forEach(function(rule, index) {
-    var value = entry(channel, rule, scope, params, fields),
-        expr;
-
+    var value = entry(channel, rule, scope, params, fields);
     if (index > 0) code += 'else';
-
     if (rule.test) {
-      expr = expression(rule.test, scope);
-      expr.$fields.forEach(function(name) { fields[name] = 1; });
-      extend(params, expr.$params);
-      code += 'if(' + expr.$expr + ')';
+      code += 'if(' + expression(rule.test, scope, params, fields) + ')';
     }
-
     code += '{' + set('o', channel, value) + '}';
   });
 
