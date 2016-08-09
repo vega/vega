971e42ed8d072a949486a1591f7a7e093fb1f2c1
Jeffrey Heer <jeffrey.heer@gmail.com>
2016-08-09 11:23:24 -0700

    Add scale range defaults.

--- packages/vega-parser/src/config.js
+++ packages/vega-parser/src/config.js
@@ -96,6 +96,40 @@ function defaults() {
       titleFontWeight: 'bold',
       titleAlign: 'left',
       titleBaseline: 'top'
+    },
+
+    // defaults for scale ranges
+    range: {
+      width: [
+        0,
+        {signal: 'width'}
+      ],
+      height: [
+        {signal: 'height'},
+        0
+      ],
+      category: [
+        '#1f77b4',
+        '#ff7f0e',
+        '#2ca02c',
+        '#d62728',
+        '#9467bd',
+        '#8c564b',
+        '#e377c2',
+        '#7f7f7f',
+        '#bcbd22',
+        '#17becf'
+      ],
+      symbol: [
+        'circle',
+        'cross',
+        'diamond',
+        'square',
+        'triangle-up',
+        'triangle-down',
+        'triangle-right',
+        'triangle-left'
+      ]
     }
   };
 }
