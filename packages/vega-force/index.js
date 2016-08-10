b6a88685f62b04f20e8b4c2e1cee8730b45070b8
Jeffrey Heer <jeffrey.heer@gmail.com>
2016-08-10 16:03:50 -0700

    Break up dataflow into sub-modules.

--- packages/vega-force/index.js
+++ packages/vega-force/index.js
@@ -1,1 +1,7 @@
-export {default as Force} from './src/Force';
+import {register} from 'vega-dataflow';
+
+import Force from './src/Force';
+import ForceDefinition from './definitions/Force';
+register(ForceDefinition, Force);
+
+export {transform, definition} from 'vega-dataflow';
