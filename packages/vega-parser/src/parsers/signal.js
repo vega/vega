987c2bdf033ad80268173cbdd766cbe65d32b2f6
Jeffrey Heer <jeffrey.heer@gmail.com>
2016-08-12 11:17:11 -0700

    Add binding support.

--- packages/vega-parser/src/parsers/signal.js
+++ packages/vega-parser/src/parsers/signal.js
@@ -1,4 +1,5 @@
 export default function(signal, scope) {
   var op = scope.addSignal(signal.name, signal.value);
   if (signal.react === false) op.react = false;
+  if (signal.bind) scope.addBinding(signal.name, signal.bind);
 }
