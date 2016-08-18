9bc73e7f81d75a24cff54ea0bd067c6e3822cdc2
Jeffrey Heer <jeffrey.heer@gmail.com>
2016-08-18 09:14:50 -0700

    Update Voronoi parameter handling, extent defaults.

--- packages/vega-voronoi/definitions/Voronoi.js
+++ packages/vega-voronoi/definitions/Voronoi.js
@@ -6,6 +6,7 @@ export default {
     { "name": "y", "type": "field", "required": true },
     { "name": "size", "type": "number", "array": true, "length": 2 },
     { "name": "extent", "type": "array", "array": true, "length": 2,
+      "default": [[-1e5, -1e5], [1e5, 1e5]],
       "content": {"type": "number", "array": true, "length": 2} },
     { "name": "as", "type": "string", "default": "path" }
   ]
