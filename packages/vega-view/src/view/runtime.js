bed95705538412c5196723277c71bf3545ab4c59
Jeffrey Heer <jeffrey.heer@gmail.com>
2016-08-26 21:33:54 -0700

    Add inrange expression function.

--- packages/vega-view/src/view/runtime.js
+++ packages/vega-view/src/view/runtime.js
@@ -91,6 +91,12 @@ function functions() {
     return entry ? entry.count : entry;
   };
 
+  fn.inrange = function(value, range) {
+    var r0 = range[0], r1 = range[range.length-1], t;
+    if (r0 > r1) t = r0, r0 = r1, r1 = t;
+    return r0 <= value && value <= r1;
+  };
+
   fn.encode = function(item, name, retval) {
     if (item) {
       var df = this.context.dataflow,
