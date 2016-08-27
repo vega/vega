5222365fe25b8b4cb3872c61062c80226fd916c9
Arvind Satyanarayan <arvindsatya1@gmail.com>
2016-08-26 18:07:17 -0700

    Move to v2.6.3.

--- vega.js
+++ vega.js
@@ -1,6 +1,6 @@
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.vg = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 module.exports = {
-  version: '2.6.2',
+  version: '2.6.3',
   dataflow: require('vega-dataflow'),
   parse: require('./src/parse/'),
   scene: {
@@ -18037,7 +18037,7 @@ function dataRef(which, def, scale, group) {
       cache = getCache.apply(this, arguments),
       sort  = def.sort,
       uniques = isUniques(scale),
-      i, rlen, j, flen, ref, fields, field, data, from;
+      i, rlen, j, flen, ref, fields, field, data, from, cmp;
 
   function addDep(s) {
     self.dependency(Deps.SIGNALS, s);
@@ -18072,15 +18072,15 @@ function dataRef(which, def, scale, group) {
 
     data = cache.aggr().result();
     if (uniques) {
-      if (sort === true) {
-        cache._values = data.map(function(d) { return d[DataRef.GROUPBY]+''; })
-          .sort(dl.cmp);
-      } else {
-        if (dl.isObject(sort)) {
-          data = data.sort(dl.comparator(sort.op + '_' + DataRef.VALUE));
-        }
-        cache._values = data.map(function(d) { return d[DataRef.GROUPBY]; });
+      if (dl.isObject(sort)) {
+        cmp = sort.op + '_' + DataRef.VALUE;
+        cmp = dl.comparator(cmp);
+      } else if (sort === true) {
+        cmp = dl.comparator(DataRef.GROUPBY);
       }
+
+      if (cmp) data = data.sort(cmp);
+      cache._values = data.map(function(d) { return d[DataRef.GROUPBY]; });
     } else {
       data = data[0];
       cache._values = !dl.isValid(data) ? [] : [data[DataRef.MIN], data[DataRef.MAX]];
