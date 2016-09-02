e893398ff41692a9e5df83c0b853141fab583bfc
Jeffrey Heer <jeffrey.heer@gmail.com>
2016-09-01 20:15:13 -0700

    Update JSON schema, tests. Update examples to pass validation.

---
+++ test/schema-test.js
@@ -0,0 +1,71 @@
+var tape = require('tape'),
+    vega = require('../'), // eslint-disable-line no-unused-vars
+    tv4 = require('tv4'),
+    fs = require('fs'),
+    schema = require('../build/vega-schema.json');
+
+var dir = process.cwd() + '/test/spec/';
+
+function validate(spec) {
+  return tv4.validate(spec, schema);
+}
+
+tape('JSON schema validates correct specifications', function(test) {
+  var specs = [
+    "airports",
+    "arc",
+    "area",
+    "bandsize",
+    "bar",
+    "bar-hover-label",
+    "barley",
+    "budget-forecasts",
+    "chart",
+    "choropleth",
+    "crossfilter",
+    "crossfilter-multi",
+    "dimpvis",
+    "driving",
+    "error",
+    "falkensee",
+    "force-network",
+    "force-beeswarm",
+    "gradient",
+    "grouped-bar",
+    "heatmap",
+    "heatmap-lines",
+    "histogram",
+    "horizon",
+    "images",
+    "jobs",
+    "legends",
+    "lifelines",
+    "map",
+    "map-bind",
+    "matrix-reorder",
+    "movies-sort",
+    "nested",
+    "overview-detail",
+    "panzoom",
+    "parallel-coords",
+    "playfair",
+    "population",
+    "shift-select",
+    "splom-inner",
+    "splom-outer",
+    "stacked-area",
+    "stacked-bar",
+    "stocks-index",
+    "tree-radial",
+    "treemap",
+    "weather"
+  ];
+
+  specs.forEach(function(file) {
+    var spec = JSON.parse(fs.readFileSync(dir + file + '.vg.json')),
+        pass = validate(spec);
+    test.ok(pass, tv4.error);
+  })
+
+  test.end();
+});
