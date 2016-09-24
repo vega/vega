c381c76b1c396c91e45ce7dca0ec1e4b4943627c
Jeffrey Heer <jeffrey.heer@gmail.com>
2016-09-23 20:32:17 -0700

    Merge pull request #8 from uwdata/master
    Merge vega-expression 2.0 work.

---
+++ packages/vega-expression/index.js
@@ -0,0 +1,5 @@
+export { default as ASTNode } from './src/ast';
+export { default as parse} from './src/parser';
+export { default as codegen } from './src/codegen';
+export { default as functions } from './src/functions';
+export { default as constants } from './src/constants';
