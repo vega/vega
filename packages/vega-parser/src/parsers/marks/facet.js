fdd4072d48aeffaa448f956aa7d6877f235c6184
Jeffrey Heer <jeffrey.heer@gmail.com>
2016-10-05 13:07:57 -0700

    Include DataScope values entry for facet data sources.

--- packages/vega-parser/src/parsers/marks/facet.js
+++ packages/vega-parser/src/parsers/marks/facet.js
@@ -1,14 +1,14 @@
 import parseSpec from '../spec';
 import DataScope from '../../DataScope';
 import {ref} from '../../util';
-import {Collect, Facet, PreFacet} from '../../transforms';
+import {Collect, Facet, PreFacet, Sieve} from '../../transforms';
 import {error} from 'vega-util';
 
 export default function(spec, scope, group) {
   var facet = spec.from.facet,
       name = facet.name,
       data = ref(scope.getData(facet.data).output),
-      subscope, source, op;
+      subscope, source, values, op;
 
   if (!facet.name) {
     error('Facet must have a name: ' + JSON.stringify(facet));
@@ -35,7 +35,8 @@ export default function(spec, scope, group) {
   // initialize facet subscope
   subscope = scope.fork();
   source = subscope.add(Collect());
-  subscope.addData(name, new DataScope(subscope, source, source));
+  values = subscope.add(Sieve({pulse: ref(source)}));
+  subscope.addData(name, new DataScope(subscope, source, source, values));
   subscope.addSignal('parent', null);
 
   // parse faceted subflow
