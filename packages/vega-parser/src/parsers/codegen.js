586fe1bdc438a4b48a798a452c0a7bacd65ec018
Jeffrey Heer <jeffrey.heer@gmail.com>
2016-10-21 13:28:47 -0700

    Consolidate expression function declarations and implementation.

---
+++ packages/vega-parser/src/parsers/codegen.js
@@ -0,0 +1,285 @@
+import {ASTNode, functions, constants} from 'vega-expression';
+import {scaleGradient} from 'vega-scenegraph';
+import {
+  error, isArray, isObject, isString, pad, stringValue, truncate, truthy
+} from 'vega-util';
+import {rgb, lab, hcl, hsl} from 'd3-color';
+import {format} from 'd3-format';
+import {timeFormat, utcFormat} from 'd3-time-format';
+
+export var scalePrefix  = '%';
+
+var Literal = 'Literal',
+    Identifier = 'Identifier',
+    indexPrefix  = '@',
+    tuplePrefix  = ':',
+    eventPrefix  = 'event.vega.',
+    thisPrefix   = 'this.';
+
+// Expression Functions
+
+var eventFunctions = ['view', 'item', 'group', 'xy', 'x', 'y'];
+
+var scaleFunctions = ['bandwidth', 'copy', 'domain', 'range', 'gradient', 'invert', 'scale'];
+
+function getScale(name, ctx) {
+  var s = isString(name) ? ctx.scales[name]
+    : isObject(name) && name.signal ? ctx.signals[name.signal]
+    : undefined;
+  return s && s.value;
+}
+
+function formatter(method) {
+  var cache = {};
+  return function(specifier, _) {
+    var f = cache[specifier] || (cache[specifier] = method(specifier));
+    return f(_);
+  };
+}
+
+function expressionFunctions(codegen) {
+  var fn = functions(codegen);
+  eventFunctions.forEach(function(name) {
+    fn[name] = eventPrefix + name;
+  });
+  for (var name in extendedFunctions) {
+    fn[name] = thisPrefix + name;
+  }
+  return fn;
+}
+
+export var extendedFunctions = {
+  format: formatter(format),
+  timeFormat: formatter(timeFormat),
+  utcFormat: formatter(utcFormat),
+  pad: pad,
+  truncate: truncate,
+
+  rgb: rgb,
+  lab: lab,
+  hcl: hcl,
+  hsl: hsl,
+  gradient: scaleGradient,
+
+  clampRange: function(range, min, max) {
+      var lo = range[0],
+          hi = range[1],
+          span;
+
+      if (hi < lo) span = hi, hi = lo, lo = span;
+      span = hi - lo;
+
+      return [
+        Math.min(Math.max(lo, min), max - span),
+        Math.min(Math.max(hi, span), max)
+      ];
+    },
+
+  pinchDistance: function() {
+      return 'Math.sqrt('
+        + 'Math.pow(event.touches[0].clientX - event.touches[1].clientX, 2) + '
+        + 'Math.pow(event.touches[0].clientY - event.touches[1].clientY, 2)'
+        + ')';
+    },
+
+  pinchAngle: function() {
+      return 'Math.atan2('
+        + 'event.touches[1].clientY - event.touches[0].clientY,'
+        + 'event.touches[1].clientX - event.touches[0].clientX'
+        + ')';
+    },
+
+  open: function(uri, name) {
+      var df = this.context.dataflow;
+      if (typeof window !== 'undefined' && window && window.open) {
+        df.loader().sanitize(uri, {context:'open', name:name})
+          .then(function(url) { window.open(url, name); })
+          .catch(function(e) { df.warn('Open url failed: ' + e); });
+      } else {
+        df.warn('Open function can only be invoked in a browser.');
+      }
+    },
+
+  screen: function() { return window.screen; },
+
+  windowsize: function() { return [window.innerWidth, window.innerHeight]; },
+
+  span: function(array) { return array[array.length-1] - array[0]; },
+
+  range: function(name, group) {
+    var s = getScale(name, (group || this).context);
+    return s && s.range ? s.range() : [0, 0];
+  },
+
+  domain: function(name, group) {
+    var s = getScale(name, (group || this).context);
+    return s ? s.domain() : [];
+  },
+
+  bandwidth: function(name, group) {
+      var s = getScale(name, (group || this).context);
+      return s && s.bandwidth ? s.bandwidth() : 0;
+    },
+
+  copy: function(name, group) {
+      var s = getScale(name, (group || this).context);
+      return s ? s.copy() : undefined;
+    },
+
+  scale:  function(name, value, group) {
+      var s = getScale(name, (group || this).context);
+      return s ? s(value) : undefined;
+    },
+
+  invert: function(name, range, group) {
+      var s = getScale(name, (group || this).context);
+      return !s ? undefined
+        : isArray(range) ? (s.invertRange || s.invert)(range)
+        : (s.invert || s.invertExtent)(range);
+    },
+
+  tuples: function(name) {
+      var data = this.context.data[name];
+      return data ? data.values.value : [];
+    },
+
+  indata: function(name, field, value) {
+      var index = this.context.data[name]['index:' + field],
+          entry = index ? index.value[value] : undefined;
+      return entry ? entry.count : entry;
+    },
+
+  inrange: function(value, range) {
+      var r0 = range[0], r1 = range[range.length-1], t;
+      if (r0 > r1) t = r0, r0 = r1, r1 = t;
+      return r0 <= value && value <= r1;
+    },
+
+  encode: function(item, name, retval) {
+      if (item) {
+        var df = this.context.dataflow,
+            target = item.mark.source;
+        df.pulse(target, df.changeset().encode(item, name));
+      }
+      return retval !== undefined ? retval : item;
+    },
+
+  modify: function(name, insert, remove, toggle, modify, values) {
+      var df = this.context.dataflow,
+          data = this.context.data[name],
+          input = data.input,
+          changes = data.changes,
+          stamp = df.stamp(),
+          predicate, key;
+
+      if (!(input.value.length || insert || toggle)) {
+        // nothing to do!
+        return 0;
+      }
+
+      if (!changes || changes.stamp < stamp) {
+        data.changes = (changes = df.changeset());
+        changes.stamp = stamp;
+        df.runAfter(function() { df.pulse(input, changes).run(); });
+      }
+
+      if (remove) {
+        changes.remove(remove === true ? truthy : remove);
+      }
+
+      if (insert) {
+        changes.insert(insert);
+      }
+
+      if (toggle) {
+        predicate = function(_) {
+          for (key in toggle) {
+            if (_[key] !== toggle[key]) return false;
+          }
+          return true;
+        };
+
+        if (input.value.filter(predicate).length) {
+          changes.remove(predicate);
+        } else {
+          changes.insert(toggle);
+        }
+      }
+
+      if (modify) {
+        for (key in values) {
+          changes.modify(modify, key, values[key]);
+        }
+      }
+
+      return 1;
+    }
+};
+
+
+// AST visitors for dependency analysis
+
+function scaleVisitor(name, args, scope, params) {
+  if (args[0].type === Literal) { // scale dependency
+    name = args[0].value;
+    var scaleName = scalePrefix + name;
+
+    if (!params.hasOwnProperty(scaleName)) {
+      try {
+        params[scaleName] = scope.scaleRef(name);
+      } catch (err) {
+        // TODO: error handling? warning?
+      }
+    }
+  }
+
+  else if (args[0].type === Identifier) { // forward reference to signal
+    name = args[0].name;
+    args[0] = new ASTNode(Literal);
+    args[0].raw = '{signal:"' + name + '"}';
+  }
+}
+
+function indataVisitor(name, args, scope, params) {
+  if (args[0].type !== Literal) error('First argument to indata must be a string literal.');
+  if (args[1].type !== Literal) error('Second argument to indata must be a string literal.');
+
+  var data = args[0].value,
+      field = args[1].value,
+      indexName = indexPrefix + field;
+
+  if (!params.hasOwnProperty(indexName)) {
+    params[indexName] = scope.getData(data).indataRef(scope, field);
+  }
+}
+
+function tuplesVisitor(name, args, scope, params) {
+  if (args[0].type !== Literal) error('First argument to tuples must be a string literal.');
+
+  var data = args[0].value,
+      dataName = tuplePrefix + data;
+
+  if (!params.hasOwnProperty(dataName)) {
+    params[dataName] = scope.getData(data).tuplesRef();
+  }
+}
+
+function visitors() {
+  var v = {
+    indata: indataVisitor,
+    tuples: tuplesVisitor
+  };
+  scaleFunctions.forEach(function(_) { v[_] = scaleVisitor; });
+  return v;
+}
+
+// Export code generator parameters
+export default {
+  blacklist:  ['_'],
+  whitelist:  ['datum', 'event'],
+  fieldvar:   'datum',
+  globalvar:  function(id) { return '_[' + stringValue('$' + id) + ']'; },
+  functions:  expressionFunctions,
+  constants:  constants,
+  visitors:   visitors()
+};
--- packages/vega-parser/src/parsers/expression.js
+++ packages/vega-parser/src/parsers/expression.js
@@ -1,169 +1,40 @@
-import {
-  ASTNode,
-  parse,
-  codegen,
-  functions as baseFunctions,
-  constants
-} from 'vega-expression';
-import {error, stringValue} from 'vega-util';
-
-var Literal = 'Literal';
-var Identifier = 'Identifier';
+import codegenParams from './codegen';
+import {parse, codegen} from 'vega-expression';
+import {error} from 'vega-util';
 
 export var signalPrefix = '$';
-export var scalePrefix  = '%';
-export var indexPrefix  = '@';
-export var tuplePrefix  = ':';
-export var eventPrefix  = 'event.vega.';
-
-function signalCode(id) {
-  return '_[' + stringValue('$' + id) + ']';
-}
-
-export var functions = function(codegen) {
-  var fn = baseFunctions(codegen);
-
-  // view-specific event information
-  fn.view   = eventPrefix + 'view';
-  fn.item   = eventPrefix + 'item';
-  fn.group  = eventPrefix + 'group';
-  fn.xy     = eventPrefix + 'xy';
-  fn.x      = eventPrefix + 'x';
-  fn.y      = eventPrefix + 'y';
-  fn.encode = 'this.encode';
-  fn.modify = 'this.modify';
-
-  // format functions
-  fn.format = 'this.format';
-  fn.timeFormat = 'this.timeFormat';
-  fn.utcFormat = 'this.utcFormat';
-  fn.pad = 'this.pad';
-  fn.truncate = 'this.truncate';
-
-  // color functions
-  fn.rgb = 'this.rgb';
-  fn.lab = 'this.lab';
-  fn.hcl = 'this.hcl';
-  fn.hsl = 'this.hsl';
-  fn.gradient = 'this.gradient';
-
-  // scales, projections, data
-  fn.copy = 'this.scaleCopy';
-  fn.domain = 'this.domain';
-  fn.bandwidth = 'this.bandwidth';
-  fn.indata = 'this.indata';
-  fn.inrange = 'this.inrange';
-  fn.invert = 'this.scaleInvert';
-  fn.range = 'this.range';
-  fn.scale = 'this.scale';
-  fn.span = 'this.span';
-  fn.tuples = 'this.tuples';
-
-  // interaction support
-  fn.clampRange    = 'this.clampRange';
-  fn.pinchDistance = 'this.pinchDistance';
-  fn.pinchAngle    = 'this.pinchAngle';
-
-  // environment functions
-  fn.open   = 'this.open';
-  fn.screen = function() { return 'window.screen'; };
-  fn.windowsize = function() {
-    return '[window.innerWidth, window.innerHeight]';
-  };
-
-  return fn;
-};
-
-export var generator = codegen({
-  blacklist:  ['_'],
-  whitelist:  ['datum', 'event'],
-  fieldvar:   'datum',
-  globalvar:  signalCode,
-  functions:  functions,
-  constants:  constants
-});
-
-function signal(name, scope, params) {
-  var signalName = signalPrefix + name;
-  if (!params.hasOwnProperty(signalName) && scope.getSignal(name)) {
-    params[signalName] = scope.signalRef(name);
-  }
-}
-
-function scale(name, scope, params) {
-  var scaleName = scalePrefix + name;
-  if (!params.hasOwnProperty(scaleName)) {
-    try {
-      params[scaleName] = scope.scaleRef(name);
-    } catch (err) {
-      // TODO: error handling? warning?
-    }
-  }
-}
-
-function index(data, field, scope, params) {
-  var indexName = indexPrefix + field;
-  if (!params.hasOwnProperty(indexName)) {
-    params[indexName] = scope.getData(data).indataRef(scope, field);
-  }
-}
-
-function tuples(data, scope, params) {
-  var dataName = tuplePrefix + data;
-  if (!params.hasOwnProperty(dataName)) {
-    params[dataName] = scope.getData(data).tuplesRef();
-  }
-}
+export var generator = codegen(codegenParams);
 
 export default function(expr, scope, preamble) {
   var params = {}, ast, gen;
 
+  // parse the expression to an abstract syntax tree (ast)
   try {
     ast = parse(expr);
   } catch (err) {
     error('Expression parse error: ' + expr);
   }
 
-  // analyze ast for dependencies
+  // analyze ast function calls for dependencies
   ast.visit(function visitor(node) {
     if (node.type !== 'CallExpression') return;
-
     var name = node.callee.name,
-        args = node.arguments;
-
-    switch (name) {
-      case 'bandwidth':
-      case 'copy':
-      case 'domain':
-      case 'range':
-      case 'gradient':
-      case 'invert':
-      case 'scale':
-        if (args[0].type === Literal) {           // scale dependency
-          scale(args[0].value, scope, params);
-        } else if (args[0].type === Identifier) { // forward reference to signal
-          name = args[0].name;
-          args[0] = new ASTNode(Literal);
-          args[0].raw = '{signal:"' + name + '"}';
-        }
-        break;
-      case 'indata':
-        if (args[0].type !== Literal) error('First argument to indata must be a string literal.');
-        if (args[1].type !== Literal) error('Second argument to indata must be a string literal.');
-        index(args[0].value, args[1].value, scope, params);
-        break;
-      case 'tuples':
-        if (args[0].type !== Literal) error('First argument to tuples must be a string literal.');
-        tuples(args[0].value, scope, params);
-        break;
-    }
+        visit = codegenParams.visitors[name];
+    if (visit) visit(name, node.arguments, scope, params);
   });
 
   // perform code generation
   gen = generator(ast);
-  gen.globals.forEach(function(name) { signal(name, scope, params); });
 
-  // returned parsed expression
+  // collect signal dependencies
+  gen.globals.forEach(function(name) {
+    var signalName = signalPrefix + name;
+    if (!params.hasOwnProperty(signalName) && scope.getSignal(name)) {
+      params[signalName] = scope.signalRef(name);
+    }
+  });
+
+  // return generated expression code and dependencies
   return {
     $expr:   preamble ? preamble + 'return(' + gen.code + ');' : gen.code,
     $fields: gen.fields,
