c518d0a6f615ce7cd3bc1a5a95e7b24faadf8b76
Jeffrey Heer <jeffrey.heer@gmail.com>
2016-11-28 14:36:27 -0800

    Make truncate and pad perform string coercion.

--- packages/vega-util/test/truncate-test.js
+++ packages/vega-util/test/truncate-test.js
@@ -13,3 +13,16 @@ tape('truncate truncates strings', function(test) {
 
   test.end();
 });
+
+tape('truncate truncates numbers', function(test) {
+  // should reduce length
+  test.equal(vega.truncate(123456789, 5), '1234…');
+  test.equal(vega.truncate(123456789, 5, null, ''), '12345');
+
+  // should respect position argument
+  test.equal(vega.truncate(123456789, 5, 'right'), '1234…');
+  test.equal(vega.truncate(123456789, 5, 'left'), '…6789');
+  test.equal(vega.truncate(123456789, 5, 'center'), '12…89');
+
+  test.end();
+});
