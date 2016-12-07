581b7ac46c30f1c4723b6c0b0ec5c8cafbb719e7
Jeffrey Heer <jeffrey.heer@gmail.com>
2016-12-07 13:10:31 -0800

    Track vega-scenegraph changes.

--- packages/vega-view/src/view/render-to-svg.js
+++ packages/vega-view/src/view/render-to-svg.js
@@ -1,5 +1,5 @@
 import renderHeadless from './render-headless';
-import {ModuleType} from 'vega-scenegraph';
+import {RenderType} from 'vega-scenegraph';
 
 /**
  * Produce a rendered SVG string of the visualization.
@@ -7,6 +7,6 @@ import {ModuleType} from 'vega-scenegraph';
  * @return {Promise} - A promise that resolves to an SVG string.
  */
 export default function() {
-  return renderHeadless(this, ModuleType.SVG)
+  return renderHeadless(this, RenderType.SVG)
     .then(function(renderer) { return renderer.svg(); });
 }
