29be1ef9480bc54804f8ce2718e292b72c6976a2
Jeffrey Heer <jeffrey.heer@gmail.com>
2017-01-06 12:05:07 -0800

    Add support for signal updates to outer scope.

--- packages/vega-parser/src/parsers/signal.js
+++ packages/vega-parser/src/parsers/signal.js
@@ -1,5 +1,17 @@
+import {error} from 'vega-util';
+
 export default function(signal, scope) {
-  var op = scope.addSignal(signal.name, signal.value);
-  if (signal.react === false) op.react = false;
-  if (signal.bind) scope.addBinding(signal.name, signal.bind);
+  var name = signal.name;
+
+  if (signal.outer) {
+    // signal must already be defined, raise error if not
+    if (!scope.signals[name]) {
+      error('Missing prior signal definition for "outer": ' + name);
+    }
+  } else {
+    // define a new signal in the current scope
+    var op = scope.addSignal(name, signal.value);
+    if (signal.react === false) op.react = false;
+    if (signal.bind) scope.addBinding(name, signal.bind);
+  }
 }
