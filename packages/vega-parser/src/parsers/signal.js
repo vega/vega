2da46f402c134224d795ed70bc50aed79d86dd1d
Jeffrey Heer <jeffrey.heer@gmail.com>
2017-01-08 08:05:37 -0800

    Rename outer:true to push:outer.

--- packages/vega-parser/src/parsers/signal.js
+++ packages/vega-parser/src/parsers/signal.js
@@ -1,13 +1,22 @@
-import {error} from 'vega-util';
+import {error, stringValue} from 'vega-util';
+
+var OUTER = 'outer',
+    OUTER_INVALID = ['value', 'update', 'react', 'bind'];
+
+function outerError(prefix, name) {
+  error(prefix + ' for "outer" push: ' + stringValue(name));
+}
 
 export default function(signal, scope) {
   var name = signal.name;
 
-  if (signal.outer) {
+  if (signal.push === OUTER) {
     // signal must already be defined, raise error if not
-    if (!scope.signals[name]) {
-      error('Missing prior signal definition for "outer": ' + name);
-    }
+    if (!scope.signals[name]) outerError('No prior signal definition', name);
+    // signal push must not use properties reserved for standard definition
+    OUTER_INVALID.forEach(function(prop) {
+      if (signal[prop] !== undefined) error('Invalid property', prop);
+    });
   } else {
     // define a new signal in the current scope
     var op = scope.addSignal(name, signal.value);
