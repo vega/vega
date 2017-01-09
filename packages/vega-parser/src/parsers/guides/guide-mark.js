5f0639157b0f098978a895579ae02b74ef5f3138
Jeffrey Heer <jeffrey.heer@gmail.com>
2017-01-09 10:47:32 -0800

    Add name support to legend/axis custom encode blocks.

--- packages/vega-parser/src/parsers/guides/guide-mark.js
+++ packages/vega-parser/src/parsers/guides/guide-mark.js
@@ -1,12 +1,15 @@
 import {extendEncode} from '../encode/encode-util';
 
+var skip = {name: 1, interactive: 1};
+
 export default function(type, role, key, dataRef, encode, extras) {
   return {
     type: type,
+    name: extras ? extras.name : undefined,
     role: role,
     key:  key,
     from: dataRef,
     interactive: !!(extras && extras.interactive),
-    encode: extendEncode(encode, extras)
+    encode: extendEncode(encode, extras, skip)
   };
 }
