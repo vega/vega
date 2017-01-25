fe3ff78ae03af5ead1ddb6731dfdf79f167f1d21
Jeffrey Heer <jeffrey.heer@gmail.com>
2017-01-24 22:08:45 -0800

    Rewrite access path parser.

--- packages/vega-util/src/splitAccessPath.js
+++ packages/vega-util/src/splitAccessPath.js
@@ -1,11 +1,43 @@
-export default function(p) {
-  return String(p)
-    .match(/\[(.*?)\]|[^.\[]+/g)
-    .map(path_trim);
-}
+import error from './error';
 
-function path_trim(d) {
-  return d[0] !== '[' ? d
-    : d[1] !== "'" && d[1] !== '"' ? d.slice(1, -1)
-    : d.slice(2, -2).replace(/\\(["'])/g, '$1');
+export default function(p) {
+  var path = [],
+      q = null,
+      b = 0,
+      n = p.length,
+      s = '',
+      i, j, c;
+
+  p = p + '';
+
+  function push() {
+    path.push(s + p.substring(i, j));
+    s = '';
+    i = j + 1;
+  }
+
+  for (i=j=0; j<n; ++j) {
+    c = p[j];
+    if (c === '\\') s += p.substring(i, j), i = ++j;
+    else if (c === q) push(), q = null, b = -1;
+    else if (q) continue;
+    else if (i === b && c === '"') i = j + 1, q = c;
+    else if (i === b && c === "'") i = j + 1, q = c;
+    else if (c === '.' && !b) (j > i) ? push() : (i = j + 1);
+    else if (c === '[') {
+      if (j > i) push();
+      b = i = j + 1;
+    }
+    else if (c === ']') {
+      if (!b) error('Access path missing open bracket: ' + p);
+      if (b > 0) push();
+      b = 0;
+      i = j + 1;
+    }
+  }
+
+  if (b) error('Access path missing closing bracket: ' + p);
+  if (q) error('Access path missing closing quote: ' + p);
+  if (j > i) ++j, push();
+  return path;
 }
