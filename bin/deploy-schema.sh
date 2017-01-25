fffad72e6cc5e3098f28941632973417fb940acc
Dominik Moritz <domoritz@gmail.com>
2017-01-25 15:59:52 -0800

    git commit -a does not add untracked files so we have to add them before we make a commit.
    cc @jheer

--- bin/deploy-schema.sh
+++ bin/deploy-schema.sh
@@ -20,7 +20,8 @@ while echo "$prefix" | grep -q '\.'; do
 done
 
 if [ -n "$(git status --porcelain)" ]; then
-    git commit -am"Add Vega $version"
+    git add *.json
+    git commit -m"Add Vega $version"
     git push
 else
   echo "Nothing has changed"
