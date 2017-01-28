4ab37d03d456058ec7af8527e260504bb8f4b7ab
Jeffrey Heer <jeffrey.heer@gmail.com>
2017-01-27 16:39:38 -0800

    Fix typo bug in boundStroke.

--- packages/vega-scenegraph/src/bound/boundStroke.js
+++ packages/vega-scenegraph/src/bound/boundStroke.js
@@ -1,5 +1,5 @@
 export default function(bounds, item) {
-  if (item.stroke && item.opacity !== 0 && item.stokeOpacity !== 0) {
+  if (item.stroke && item.opacity !== 0 && item.strokeOpacity !== 0) {
     bounds.expand(item.strokeWidth != null ? +item.strokeWidth : 1);
   }
   return bounds;
