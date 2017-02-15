f27b62728cdab63c349d22f1ef15c2149a67a027
Jeffrey Heer <jeffrey.heer@gmail.com>
2017-02-15 15:38:22 -0800

    Add new color palettes.

---
+++ packages/vega-scale/src/palettes.js
@@ -0,0 +1,25 @@
+function colors(specifier) {
+  var n = specifier.length / 6 | 0, colors = new Array(n), i = 0;
+  while (i < n) colors[i] = "#" + specifier.slice(i * 6, ++i * 6);
+  return colors;
+}
+
+export var tableau10 = colors(
+  '4c78a8f58518e4575672b7b254a24beeca3bb279a2ff9da69d755dbab0ac'
+);
+
+export var tableau20 = colors(
+  '4c78a89ecae9f58518ffbf7954a24b88d27ab79a20f2cf5b43989483bcb6e45756ff9d9879706ebab0acd67195fcbfd2b279a2d6a5c99e765fd8b5a5'
+);
+
+export var blueOrange = new Array(3).concat(
+  "67a9cff7f7f7f1a340",
+  "0571b092c5defdb863e66101",
+  "0571b092c5def7f7f7fdb863e66101",
+  "2166ac67a9cfd1e5f0fee0b6f1a340b35806",
+  "2166ac67a9cfd1e5f0f7f7f7fee0b6f1a340b35806",
+  "2166ac4393c392c5ded1e5f0fee0b6fdb863e08214b35806",
+  "2166ac4393c392c5ded1e5f0f7f7f7fee0b6fdb863e08214b35806",
+  "0530612166ac4393c392c5ded1e5f0fee0b6fdb863e08214b358067f3b08",
+  "0530612166ac4393c392c5ded1e5f0f7f7f7fee0b6fdb863e08214b358067f3b08"
+).map(colors);
