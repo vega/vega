f15a20de3b523e12605ea3d223b36282cccf334f
Jeffrey Heer <jeffrey.heer@gmail.com>
2017-02-21 14:59:51 -0800

    Rename Range to Sequence.

--- packages/vega-dataflow/src/transforms/Sequence.js
+++ packages/vega-dataflow/src/transforms/Sequence.js
@@ -4,18 +4,18 @@ import {inherits} from 'vega-util';
 import {range} from 'd3-array';
 
 /**
- * Generates data tuples for a specified range of numbers.
+ * Generates data tuples for a specified sequence range of numbers.
  * @constructor
  * @param {object} params - The parameters for this operator.
- * @param {number} params.start - The first number in the range.
- * @param {number} params.stop - The last number (exclusive) in the range.
- * @param {number} [params.step=1] - The step size between numbers in the range.
+ * @param {number} params.start - The first number in the sequence.
+ * @param {number} params.stop - The last number (exclusive) in the sequence.
+ * @param {number} [params.step=1] - The step size between numbers in the sequence.
  */
-export default function Range(params) {
+export default function Sequence(params) {
   Transform.call(this, [], params);
 }
 
-var prototype = inherits(Range, Transform);
+var prototype = inherits(Sequence, Transform);
 
 prototype.transform = function(_, pulse) {
   if (!_.modified()) return;
