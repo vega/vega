65bf5f27574f979356b6e4dc8b27ad4acb0de151
Jeffrey Heer <jeffrey.heer@gmail.com>
2017-03-16 09:28:55 -0700

    Move event selector parser to new module.

--- packages/vega-parser/index.js
+++ packages/vega-parser/index.js
@@ -1,6 +1,5 @@
 export {default as parse} from './src/parse';
 
-export {default as selector} from './src/parsers/event-selector';
 export {default as signal} from './src/parsers/signal';
 export {default as signalUpdates} from './src/parsers/signal-updates';
 export {default as stream} from './src/parsers/stream';
@@ -27,7 +26,6 @@ export {
   LegendSymbolRole,
   LegendTitleRole
 } from './src/parsers/marks/roles';
-export {marktypes, isMarkType} from './src/parsers/marks/marktypes';
 
 export {default as Scope} from './src/Scope';
 export {default as DataScope} from './src/DataScope';
