faee85cb93e32263d206ffde050b588767a19292
Jeffrey Heer <jeffrey.heer@gmail.com>
2017-05-07 15:46:07 -0700

    Update Lookup transform to write field values, not just tuple values.

--- packages/vega-dataflow/test/transforms/lookup-test.js
+++ packages/vega-dataflow/test/transforms/lookup-test.js
@@ -55,3 +55,56 @@ tape('Lookup looks up matching tuples', function(test) {
 
   test.end();
 });
+
+tape('Lookup looks up matching values', function(test) {
+  var lut = [
+    {'id': 1, 'value': 'foo'},
+    {'id': 3, 'value': 'bar'},
+    {'id': 5, 'value': 'baz'}
+  ];
+
+  var data = [
+    {'id': 0, 'x': 5, 'y': 1},
+    {'id': 1, 'x': 3, 'y': 5},
+    {'id': 2, 'x': 1, 'y': 5},
+    {'id': 3, 'x': 3, 'y': 3}
+  ];
+
+  var value = util.field('value'),
+      id = util.field('id'),
+      x = util.field('x'),
+      y = util.field('y'),
+
+      df = new vega.Dataflow(),
+      c0 = df.add(Collect),
+      ti = df.add(TupleIndex, {field:id, pulse:c0}),
+      c1 = df.add(Collect),
+      lk = df.add([x]),
+      lu = df.add(Lookup, {index:ti, fields:lk, values:[value], pulse:c1});
+
+  df.run(); // initialize
+
+  // add lookup table
+  df.pulse(c0, changeset().insert(lut)).run();
+  test.equal(ti.value.size, 3);
+
+  // add primary data
+  df.pulse(c1, changeset().insert(data)).run();
+  var p = lu.pulse.add;
+  test.equal(p.length, 4);
+  test.deepEqual(p.map(value), ['baz', 'bar', 'foo', 'bar']);
+
+  // swap lookup keys
+  df.update(lk, [y]).run();
+  p = lu.pulse.mod;
+  test.equal(p.length, 4);
+  test.deepEqual(p.map(value), ['foo', 'baz', 'baz', 'bar']);
+
+  // modify lookup table
+  df.pulse(c0, changeset().modify(lut[0], 'value', 'fud')).run();
+  p = lu.pulse.mod;
+  test.equal(p.length, 4);
+  test.deepEqual(p.map(value), ['fud', 'baz', 'baz', 'bar']);
+
+  test.end();
+});
