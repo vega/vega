32225d9dfdf4b3d03e8b59f11e865c1c41811f78
Jeffrey Heer <jeffrey.heer@gmail.com>
2017-05-29 14:23:32 -0700

    Update canvas construction method.

--- packages/vega-scenegraph/src/util/canvas/canvas.js
+++ packages/vega-scenegraph/src/util/canvas/canvas.js
@@ -6,11 +6,15 @@ try { Canvas = require('canvas'); } catch (e) { Canvas = null; }
 
 export default function(w, h) {
   var canvas = domCreate(null, 'canvas');
-  if (canvas) {
+  if (canvas && canvas.getContext) {
     canvas.width = w;
     canvas.height = h;
   } else if (Canvas) {
-    canvas = new Canvas(w, h);
+    try {
+      canvas = new Canvas(w, h);
+    } catch (e) {
+      canvas = null;
+    }
   }
   return canvas;
 }
