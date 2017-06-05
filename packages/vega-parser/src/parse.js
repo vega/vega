cdab2fefc68fa1e5e85e61a17397564b52646f14
Jeffrey Heer <jeffrey.heer@gmail.com>
2017-06-04 20:34:00 -0700

    Allow cascaded configs. (vega/vega#841)

--- packages/vega-parser/src/parse.js
+++ packages/vega-parser/src/parse.js
@@ -5,6 +5,6 @@ import {error, isObject} from 'vega-util';
 
 export default function(spec, config) {
   if (!isObject(spec)) error('Input Vega specification must be an object.');
-  return parseView(spec, new Scope(defaults(config || spec.config)))
+  return parseView(spec, new Scope(defaults([config, spec.config])))
     .toRuntime();
 }
