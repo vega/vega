584565a88c0e7816d19e41f5b495986d7103cb91
Jeffrey Heer <jeffrey.heer@gmail.com>
2017-06-08 10:24:35 -0700

    Add Overlap transform.

--- packages/vega-view/index.js
+++ packages/vega-view/index.js
@@ -2,11 +2,13 @@ import {transform} from 'vega-dataflow';
 
 import Bound from './src/transforms/Bound';
 import Mark from './src/transforms/Mark';
+import Overlap from './src/transforms/Overlap';
 import Render from './src/transforms/Render';
 import ViewLayout from './src/transforms/ViewLayout';
 
 transform('Bound', Bound);
 transform('Mark', Mark);
+transform('Overlap', Overlap);
 transform('Render', Render);
 transform('ViewLayout', ViewLayout);
 
