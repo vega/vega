492a02abbb6f3da75e148d1c54d94874fea885c0
Yuri Astrakhan <yurik@wikimedia.org>
2017-06-17 23:08:31 -0400

    removed comma expressions

--- packages/vega-scale/src/invertRangeExtent.js
+++ packages/vega-scale/src/invertRangeExtent.js
@@ -5,7 +5,11 @@ export default function(scale) {
         hi = _[1],
         min = -1, max, t, i, n;
 
-    if (hi < lo) t = lo, lo = hi, hi = t;
+    if (hi < lo) {
+      t = lo;
+      lo = hi;
+      hi = t;
+    }
 
     for (i=0, n=range.length; i<n; ++i) {
       if (range[i] >= lo && range[i] <= hi) {
