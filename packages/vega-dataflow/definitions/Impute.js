e49fd64e79e4a2d381788bb892f9613a23cfdb83
Jeffrey Heer <jeffrey.heer@gmail.com>
2017-07-01 20:59:40 -0700

    Revise Impute transform.

--- packages/vega-dataflow/definitions/Impute.js
+++ packages/vega-dataflow/definitions/Impute.js
@@ -3,8 +3,9 @@ export default {
   "metadata": {"changes": true},
   "params": [
     { "name": "field", "type": "field", "required": true },
+    { "name": "key", "type": "field", "required": true },
+    { "name": "keyvals", "array": true },
     { "name": "groupby", "type": "field", "array": true },
-    { "name": "orderby", "type": "field", "array": true },
     { "name": "method", "type": "enum", "default": "value",
       "values": ["value", "mean", "median", "max", "min"] },
     { "name": "value", "default": 0 }
--- packages/vega-dataflow/src/transforms/Impute.js
+++ packages/vega-dataflow/src/transforms/Impute.js
@@ -20,8 +20,13 @@ var Empty = [];
  * @param {function(object): *} params.field - The value field to impute.
  * @param {Array<function(object): *>} [params.groupby] - An array of
  *   accessors to determine series within which to perform imputation.
- * @param {Array<function(object): *>} [params.orderby] - An array of
- *   accessors to determine the ordering within a series.
+ * @param {function(object): *} params.key - An accessor for a key value.
+ *   Each key value should be unique within a group. New tuples will be
+ *   imputed for any key values that are not found within a group.
+ * @param {Array<*>} [params.keyvals] - Optional array of required key
+ *   values. New tuples will be imputed for any key values that are not
+ *   found within a group. In addition, these values will be automatically
+ *   augmented with the key values observed in the input data.
  * @param {string} [method='value'] - The imputation method to use. One of
  *   'value', 'mean', 'median', 'max', 'min'.
  * @param {*} [value=0] - The constant value to use for imputation
@@ -56,13 +61,13 @@ prototype.transform = function(_, pulse) {
       impute = getValue(_),
       field = getField(_),
       fName = accessorName(_.field),
-      gNames = _.groupby.map(accessorName),
-      oNames = _.orderby.map(accessorName),
-      groups = partition(pulse.source, _.groupby, _.orderby),
+      kName = accessorName(_.key),
+      gNames = (_.groupby || []).map(accessorName),
+      groups = partition(pulse.source, _.groupby, _.key, _.keyvals),
       curr = [],
       prev = this.value,
       m = groups.domain.length,
-      group, value, gVals, oVals, g, i, j, l, n, t;
+      group, value, gVals, kVal, g, i, j, l, n, t;
 
   for (g=0, l=groups.length; g<l; ++g) {
     group = groups[g];
@@ -72,11 +77,11 @@ prototype.transform = function(_, pulse) {
     // add tuples for missing values
     for (j=0; j<m; ++j) {
       if (group[j] != null) continue;
-      oVals = groups.domain[j];
+      kVal = groups.domain[j];
 
       t = {_impute: true};
       for (i=0, n=gVals.length; i<n; ++i) t[gNames[i]] = gVals[i];
-      for (i=0, n=oVals.length; i<n; ++i) t[oNames[i]] = oVals[i];
+      t[kName] = kVal;
       t[fName] = isNaN(value) ? (value = impute(group, field)) : value;
 
       curr.push(ingest(t));
@@ -91,19 +96,20 @@ prototype.transform = function(_, pulse) {
   return out;
 };
 
-function partition(data, groupby, orderby) {
+function partition(data, groupby, key, keyvals) {
   var get = function(f) { return f(t); },
       groups = [],
-      domain = [],
-      oMap = {}, oVals, oKey,
+      domain = keyvals ? keyvals.slice() : [],
+      kMap = {},
       gMap = {}, gVals, gKey,
-      group, i, j, n, t;
+      group, i, j, k, n, t;
+
+  domain.forEach(function(k, i) { kMap[k] = i + 1; });
 
   for (i=0, n=data.length; i<n; ++i) {
     t = data[i];
-
-    oKey = (oVals = orderby.map(get)) + '';
-    j = oMap[oKey] || (oMap[oKey] = domain.push(oVals));
+    k = key(t);
+    j = kMap[k] || (kMap[k] = domain.push(k));
 
     gKey = (gVals = groupby ? groupby.map(get) : Empty) + '';
     if (!(group = gMap[gKey])) {
