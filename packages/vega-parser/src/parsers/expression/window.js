48b8f2fdda38015e45c14b15e545a3e98f0f6b5e
Jeffrey Heer <jeffrey.heer@gmail.com>
2017-07-11 15:44:44 -0700

    Add containerSize expression function. Rename windowSize.

--- packages/vega-parser/src/parsers/expression/window.js
+++ packages/vega-parser/src/parsers/expression/window.js
@@ -4,8 +4,16 @@ export function screen() {
   return _window ? _window.screen : {};
 }
 
-export function windowsize() {
+export function windowSize() {
   return _window
     ? [_window.innerWidth, _window.innerHeight]
+    : [undefined, undefined];
+}
+
+export function containerSize() {
+  var view = this.context.dataflow,
+      el = view.container && view.container();
+  return el
+    ? [el.clientWidth, el.clientHeight]
     : [undefined, undefined];
 }
