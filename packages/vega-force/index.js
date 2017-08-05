b72a580e18f4b524080891048582b623c5b8615d
Jeffrey Heer <jeffrey.heer@gmail.com>
2017-08-04 22:01:31 -0700

    Update to use new build / module system.

--- packages/vega-force/index.js
+++ packages/vega-force/index.js
@@ -1,7 +1,1 @@
-import {register} from 'vega-dataflow';
-
-import Force from './src/Force';
-import ForceDefinition from './definitions/Force';
-register(ForceDefinition, Force);
-
-export {transform, definition} from 'vega-dataflow';
+export {default as force} from './src/Force';
