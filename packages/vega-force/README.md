77a4b5104ac86211b315d8aebfec06d4eb87251d
Jeffrey Heer <jeffrey.heer@gmail.com>
2017-08-04 22:01:31 -0700

    Update to use new build / module system.

--- packages/vega-force/README.md
+++ packages/vega-force/README.md
@@ -2,6 +2,6 @@
 
 Force simulation transform for Vega dataflows.
 
-This module provides the following data transform:
+This module provides the following Vega data transform:
 
 - [**Force**](https://vega.github.io/vega/docs/transforms/force/) [&lt;&gt;](https://github.com/vega/vega-force/blob/master/src/Force.js "Source")
--- packages/vega-force/src/Force.js
+++ packages/vega-force/src/Force.js
@@ -32,6 +32,75 @@ export default function Force(params) {
   Transform.call(this, null, params);
 }
 
+Force.Definition = {
+  "type": "Force",
+  "metadata": {"modifies": true},
+  "params": [
+    { "name": "static", "type": "boolean", "default": false },
+    { "name": "restart", "type": "boolean", "default": false },
+    { "name": "iterations", "type": "number", "default": 300 },
+    { "name": "alpha", "type": "number", "default": 1 },
+    { "name": "alphaMin", "type": "number", "default": 0.001 },
+    { "name": "alphaTarget", "type": "number", "default": 0 },
+    { "name": "velocityDecay", "type": "number", "default": 0.4 },
+    { "name": "forces", "type": "param", "array": true,
+      "params": [
+        {
+          "key": {"force": "center"},
+          "params": [
+            { "name": "x", "type": "number", "default": 0 },
+            { "name": "y", "type": "number", "default": 0 }
+          ]
+        },
+        {
+          "key": {"force": "collide"},
+          "params": [
+            { "name": "radius", "type": "number", "expr": true },
+            { "name": "strength", "type": "number", "default": 0.7 },
+            { "name": "iterations", "type": "number", "default": 1 }
+          ]
+        },
+        {
+          "key": {"force": "nbody"},
+          "params": [
+            { "name": "strength", "type": "number", "default": -30 },
+            { "name": "theta", "type": "number", "default": 0.9 },
+            { "name": "distanceMin", "type": "number", "default": 1 },
+            { "name": "distanceMax", "type": "number" }
+          ]
+        },
+        {
+          "key": {"force": "link"},
+          "params": [
+            { "name": "links", "type": "data" },
+            { "name": "id", "type": "field" },
+            { "name": "distance", "type": "number", "default": 30, "expr": true },
+            { "name": "strength", "type": "number", "expr": true },
+            { "name": "iterations", "type": "number", "default": 1 }
+          ]
+        },
+        {
+          "key": {"force": "x"},
+          "params": [
+            { "name": "strength", "type": "number", "default": 0.1 },
+            { "name": "x", "type": "field" }
+          ]
+        },
+        {
+          "key": {"force": "y"},
+          "params": [
+            { "name": "strength", "type": "number", "default": 0.1 },
+            { "name": "y", "type": "field" }
+          ]
+        }
+      ] },
+    {
+      "name": "as", "type": "string", "array": true, "modify": false,
+      "default": ForceOutput
+    }
+  ]
+};
+
 var prototype = inherits(Force, Transform);
 
 prototype.transform = function(_, pulse) {
