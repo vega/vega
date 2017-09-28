482c336f42cf16479b903d36ecca7cc0dfe27a8b
Jeffrey Heer <jeffrey.heer@gmail.com>
2017-09-27 23:43:09 -0700

    Fix export for random function.

--- packages/vega-statistics/index.js
+++ packages/vega-statistics/index.js
@@ -1,9 +1,9 @@
 export {default as bin} from './src/bin';
 export {default as bootstrapCI} from './src/bootstrapCI';
-export {default as random, setRandom} from './src/random';
+export {default as quartiles} from './src/quartiles';
+export {random, setRandom} from './src/random';
 export {default as randomInteger} from './src/integer';
 export {default as randomKDE} from './src/kde';
 export {default as randomMixture} from './src/mixture';
 export {default as randomNormal} from './src/normal';
 export {default as randomUniform} from './src/uniform';
-export {default as quartiles} from './src/quartiles';
