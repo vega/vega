f7d76d45665cb2253e9f2c137a5fe772d4a893c2
Jeffrey Heer <jeffrey.heer@gmail.com>
2017-09-28 09:29:09 -0700

    Add Project transform.

---
+++ packages/vega-transforms/src/Project.js
@@ -0,0 +1,79 @@
+import {ingest, rederive, Transform, tupleid} from 'vega-dataflow';
+import {accessorName, inherits} from 'vega-util';
+
+/**
+ * Performs a relational projection, copying selected fields from source
+ * tuples to a new set of derived tuples.
+ * @param {object} params - The parameters for this operator.
+ * @param {Array<function(object): *} params.fields - The fields to project,
+ *   as an array of field accessors. If unspecified, all fields will be
+ *  copied with names unchanged.
+ * @param {Array<string>} params.as - Output field names for each projected
+ *   field. Any unspecified fields will use the field name provided by
+ *   the field accessor.
+ * @constructor
+ */
+export default function Project(params) {
+  Transform.call(this, null, params);
+}
+
+Project.Definition = {
+  "type": "Project",
+  "metadata": {"generates": true, "changes": true, "modifies": true},
+  "params": [
+    { "name": "fields", "type": "field", "array": true },
+    { "name": "as", "type": "string", "null": true, "array": true },
+  ]
+};
+
+var prototype = inherits(Project, Transform);
+
+prototype.transform = function(_, pulse) {
+  var fields = _.fields,
+      as = output(_.fields, _.as),
+      derive = fields
+        ? function(s, t) { return project(s, t, fields, as); }
+        : rederive,
+      out, lut;
+
+  if (this.value) {
+    lut = this.value;
+  } else {
+    pulse = pulse.addAll();
+    lut = this.value = {};
+  }
+
+  out = pulse.fork();
+
+  pulse.visit(pulse.REM, function(t) {
+    var id = tupleid(t);
+    out.rem.push(lut[id]);
+    lut[id] = null;
+  });
+
+  pulse.visit(pulse.ADD, function(t) {
+    var dt = derive(t, ingest({}));
+    lut[tupleid(t)] = dt;
+    out.add.push(dt);
+  });
+
+  pulse.visit(pulse.MOD, function(t) {
+    out.mod.push(derive(t, lut[tupleid(t)]));
+  });
+
+  return out;
+};
+
+function output(fields, as) {
+  if (!fields) return null;
+  return fields.map(function(f, i) {
+    return as[i] || accessorName(f);
+  });
+}
+
+function project(s, t, fields, as) {
+  for (var i=0, n=fields.length; i<n; ++i) {
+    t[as[i]] = fields[i](s);
+  }
+  return t;
+}
---
+++ packages/vega-transforms/test/project-test.js
@@ -0,0 +1,126 @@
+var tape = require('tape'),
+    util = require('vega-util'),
+    vega = require('vega-dataflow'),
+    tx = require('../'),
+    changeset = vega.changeset,
+    Collect = tx.collect,
+    Project = tx.project;
+
+tape('Project copies tuples', function(test) {
+  var data = [{'id': 0}, {'id': 1}];
+
+  var id = util.field('id'),
+      df = new vega.Dataflow(),
+      c = df.add(Collect),
+      r = df.add(Project, {pulse:c}),
+      p;
+
+  // test initial insert
+  df.pulse(c, changeset().insert(data)).run();
+  p = r.pulse;
+  test.equal(p.add.length, 2);
+  test.equal(p.rem.length, 0);
+  test.equal(p.mod.length, 0);
+  test.notEqual(p.add[0], data[0]);
+  test.notEqual(p.add[1], data[1]);
+  test.deepEqual(p.add.map(id), [0, 1]);
+
+  // test simultaneous remove and add
+  df.pulse(c, changeset().remove(data[0]).insert(data[0])).run();
+  p = r.pulse;
+  test.equal(p.add.length, 1);
+  test.equal(p.rem.length, 1);
+  test.equal(p.mod.length, 0);
+  test.notEqual(p.add[0], data[0]);
+  test.notEqual(p.rem[0], data[0]);
+  test.equal(id(p.add[0]), 0);
+  test.equal(id(p.rem[0]), 0);
+
+  // test tuple modification
+  df.pulse(c, changeset()
+    .modify(function() { return 1; }, 'id', function(t) { return t.id + 2; }))
+    .run();
+  p = r.pulse;
+  test.equal(p.add.length, 0);
+  test.equal(p.rem.length, 0);
+  test.equal(p.mod.length, 2);
+  test.notEqual(p.mod[0], data[0]);
+  test.notEqual(p.mod[1], data[1]);
+  test.deepEqual(p.mod.map(id), [2, 3]);
+
+  // test tuple removal
+  df.pulse(c, changeset().remove(data)).run();
+  p = r.pulse;
+  test.equal(p.add.length, 0);
+  test.equal(p.rem.length, 2);
+  test.equal(p.mod.length, 0);
+  test.notEqual(p.rem[0], data[0]);
+  test.notEqual(p.rem[1], data[1]);
+  test.deepEqual(p.rem.map(id), [2, 3]);
+
+  test.end();
+});
+
+tape('Project aliases tuples', function(test) {
+  var data = [{'id': 0, 'foo': 'a'}, {'id': 1, 'foo': 'b'}];
+
+  var id = util.field('id'),
+      foo = util.field('foo'),
+      key = util.field('key'),
+      df = new vega.Dataflow(),
+      c = df.add(Collect),
+      r = df.add(Project, {
+          fields: [id, foo],
+          as: ['key'],
+          pulse: c
+        }),
+      p;
+
+  // test initial insert
+  df.pulse(c, changeset().insert(data)).run();
+  p = r.pulse;
+  test.equal(p.add.length, 2);
+  test.equal(p.rem.length, 0);
+  test.equal(p.mod.length, 0);
+  test.notEqual(p.add[0], data[0]);
+  test.notEqual(p.add[1], data[1]);
+  test.deepEqual(p.add.map(key), [0, 1]);
+  test.deepEqual(p.add.map(foo), ['a', 'b']);
+
+  // test simultaneous remove and add
+  df.pulse(c, changeset().remove(data[0]).insert(data[0])).run();
+  p = r.pulse;
+  test.equal(p.add.length, 1);
+  test.equal(p.rem.length, 1);
+  test.equal(p.mod.length, 0);
+  test.notEqual(p.add[0], data[0]);
+  test.notEqual(p.rem[0], data[0]);
+  test.equal(key(p.add[0]), 0);
+  test.equal(key(p.rem[0]), 0);
+
+  // test tuple modification
+  df.pulse(c, changeset()
+    .modify(function() { return 1; }, 'id', function(t) { return t.id + 2; }))
+    .run();
+  p = r.pulse;
+  test.equal(p.add.length, 0);
+  test.equal(p.rem.length, 0);
+  test.equal(p.mod.length, 2);
+  test.notEqual(p.mod[0], data[0]);
+  test.notEqual(p.mod[1], data[1]);
+  test.deepEqual(p.mod.map(key), [2, 3]);
+  test.deepEqual(p.mod.map(foo), ['a', 'b']);
+
+  // test tuple removal
+  df.pulse(c, changeset().remove(data)).run();
+  p = r.pulse;
+  test.equal(p.add.length, 0);
+  test.equal(p.rem.length, 2);
+  test.equal(p.mod.length, 0);
+  test.notEqual(p.rem[0], data[0]);
+  test.notEqual(p.rem[1], data[1]);
+  test.deepEqual(p.rem.map(key), [2, 3]);
+  test.deepEqual(p.rem.map(foo), ['a', 'b']);
+
+  test.end();
+});
