33569bc69f8bc4181b44753222cf83f86afb7ec5
Jeffrey Heer <jeffrey.heer@gmail.com>
2017-09-28 16:22:20 -0700

    Code clean-up, valid tick refinement.

--- packages/vega-encode/test/ticks-test.js
+++ packages/vega-encode/test/ticks-test.js
@@ -3,25 +3,29 @@ validTicks = require('../').validTicks,
 timeInterval = require('vega-scale').timeInterval;
 
 tape('validTicks uses count correctly', function(test) {
-var data = [0, 1, 2, 3, 4, 5, 6, 7];
+  var data = [0, 1, 2, 3, 4, 5, 6, 7];
 
-var identity = function(x) { return x; };
-identity.range = function() { return [0, 10]; };
+  var identity = function(x) { return x; };
+  identity.range = function() { return [0, 10]; };
 
-var t1 = validTicks(identity, data, 5);
-test.deepEqual(t1, [0, 2, 4, 6]);
+  var t1 = validTicks(identity, data, 5);
+  test.deepEqual(t1, [0, 2, 4, 6]);
 
-// don't change ticks if count is large
-var t2 = validTicks(identity, data, 100);
-test.deepEqual(t2, data);
+  // don't change ticks if count is large
+  var t2 = validTicks(identity, data, 100);
+  test.deepEqual(t2, data);
 
-// special case for low number of ticks
-var t3 = validTicks(identity, data, 3);
-test.deepEqual(t3, [0, 7]);
+  // special case for low number of ticks
+  var t3 = validTicks(identity, data, 3);
+  test.deepEqual(t3, [0, 7]);
 
-// validTicks ignores interval function
-var t4 = validTicks(identity, data, timeInterval('hour'));
-test.deepEqual(t4, data);
+  // validTicks ignores interval function
+  var t4 = validTicks(identity, data, timeInterval('hour'));
+  test.deepEqual(t4, data);
 
-test.end();
+  // single tick should pass through
+  var t5 = validTicks(identity, [1], 5);
+  test.deepEqual(t5, [1]);
+
+  test.end();
 });
