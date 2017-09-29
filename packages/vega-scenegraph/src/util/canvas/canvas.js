1519caa1820fa5ddf3729dd7dc744c335836b2c6
Jeffrey Heer <jeffrey.heer@gmail.com>
2017-09-29 09:41:29 -0700

    Refine support for canvas-prebuilt.

--- packages/vega-scenegraph/src/util/canvas/canvas.js
+++ packages/vega-scenegraph/src/util/canvas/canvas.js
@@ -3,9 +3,17 @@ import {domCreate} from '../dom';
 export var Canvas;
 
 try {
+  // try to load canvas module
   Canvas = require('canvas');
-  Canvas = Canvas || require('canvas-prebuilt');
-} catch (e) { Canvas = null; }
+} catch (e) {
+  try {
+    // if canvas fails, try to load canvas-prebuilt
+    Canvas = require('canvas-prebuilt');
+  } catch (e2) {
+    // if all options fail, set to null
+    Canvas = null;
+  }
+}
 
 export default function(w, h) {
   var canvas = domCreate(null, 'canvas');
