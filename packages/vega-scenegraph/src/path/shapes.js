d2e9f72feea4e09b4f324c2755c1209e7b1aa0ec
Jeffrey Heer <jeffrey.heer@gmail.com>
2017-09-29 09:25:57 -0700

    Add safer arc shape accessors.

--- packages/vega-scenegraph/src/path/shapes.js
+++ packages/vega-scenegraph/src/path/shapes.js
@@ -18,13 +18,18 @@ function ts(item)   { return item.size || 1; }
 function h(item)    { return item.height || 0; }
 function xw(item)   { return (item.x || 0) + (item.width || 0); }
 function yh(item)   { return (item.y || 0) + (item.height || 0); }
-function cr(item)   { return item.cornerRadius || 0; }
+function sa(item)   { return item.startAngle || 0; }
+function ea(item)   { return item.endAngle || 0; }
 function pa(item)   { return item.padAngle || 0; }
+function ir(item)   { return item.innerRadius || 0; }
+function or(item)   { return item.outerRadius || 0; }
+function cr(item)   { return item.cornerRadius || 0; }
 function def(item)  { return !(item.defined === false); }
 function size(item) { return item.size == null ? 64 : item.size; }
 function type(item) { return symbols(item.shape || 'circle'); }
 
-var arcShape    = d3_arc().cornerRadius(cr).padAngle(pa),
+var arcShape    = d3_arc().startAngle(sa).endAngle(ea).padAngle(pa)
+                          .innerRadius(ir).outerRadius(or).cornerRadius(cr),
     areavShape  = d3_area().x(x).y1(y).y0(yh).defined(def),
     areahShape  = d3_area().y(y).x1(x).x0(xw).defined(def),
     lineShape   = d3_line().x(x).y(y).defined(def),
