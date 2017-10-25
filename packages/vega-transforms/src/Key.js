c6a32889bd2f8c571fea5b57d9683f2607dc35bd
Jeffrey Heer <jeffrey.heer@gmail.com>
2017-10-24 21:34:53 -0700

    Add support for flat properties in Key transform.

--- packages/vega-transforms/src/Key.js
+++ packages/vega-transforms/src/Key.js
@@ -6,6 +6,9 @@ import {inherits, key} from 'vega-util';
  * @constructor
  * @param {object} params - The parameters for this operator.
  * @param {Array<string>} params.fields - The field name(s) for the key function.
+ * @param {boolean} params.flat - A boolean flag indicating if the field names
+ *  should be treated as flat property names, side-stepping nested field
+ *  lookups normally indicated by dot or bracket notation.
  */
 export default function Key(params) {
   Operator.call(this, null, update, params);
@@ -14,5 +17,5 @@ export default function Key(params) {
 inherits(Key, Operator);
 
 function update(_) {
-  return (this.value && !_.modified()) ? this.value : key(_.fields);
+  return (this.value && !_.modified()) ? this.value : key(_.fields, _.flat);
 }
