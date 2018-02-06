e8055b9776848904798c9ca06232c2a525a59bdc
Jeffrey Heer <jeffrey.heer@gmail.com>
2018-02-05 18:02:49 -0800

    Upgrade to d3-scale@2.0, d3-scale-chromatic@1.2.

--- packages/vega-scale/src/palettes.js
+++ packages/vega-scale/src/palettes.js
@@ -4,6 +4,18 @@ function colors(specifier) {
   return colors;
 }
 
+export var category20 = colors(
+  '1f77b4aec7e8ff7f0effbb782ca02c98df8ad62728ff98969467bdc5b0d58c564bc49c94e377c2f7b6d27f7f7fc7c7c7bcbd22dbdb8d17becf9edae5'
+);
+
+export var category20b = colors(
+  '393b795254a36b6ecf9c9ede6379398ca252b5cf6bcedb9c8c6d31bd9e39e7ba52e7cb94843c39ad494ad6616be7969c7b4173a55194ce6dbdde9ed6'
+);
+
+export var category20c = colors(
+  '3182bd6baed69ecae1c6dbefe6550dfd8d3cfdae6bfdd0a231a35474c476a1d99bc7e9c0756bb19e9ac8bcbddcdadaeb636363969696bdbdbdd9d9d9'
+);
+
 export var tableau10 = colors(
   '4c78a8f58518e4575672b7b254a24beeca3bb279a2ff9da69d755dbab0ac'
 );
