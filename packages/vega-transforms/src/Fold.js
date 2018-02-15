3b064c21add3ceb8816b1401d7bc6557a22beb32
Jeffrey Heer <jeffrey.heer@gmail.com>
2018-02-14 21:19:58 -0800

    Simplify Fold transform implementation.

--- packages/vega-transforms/src/Fold.js
+++ packages/vega-transforms/src/Fold.js
@@ -1,5 +1,5 @@
-import {derive, rederive, Transform, tupleid} from 'vega-dataflow';
-import {inherits} from 'vega-util';
+import {derive, Transform} from 'vega-dataflow';
+import {inherits, accessorName} from 'vega-util';
 
 /**
  * Folds one more tuple fields into multiple tuples in which the field
@@ -8,14 +8,16 @@ import {inherits} from 'vega-util';
  * @param {object} params - The parameters for this operator.
  * @param {function(object): *} params.fields - An array of field accessors
  *   for the tuple fields that should be folded.
+ * @param {Array<string>} [params.as] - Output field names for folded key
+ *   and value fields, defaults to ['key', 'value'].
  */
 export default function Fold(params) {
-  Transform.call(this, {}, params);
+  Transform.call(this, [], params);
 }
 
 Fold.Definition = {
   "type": "Fold",
-  "metadata": {"generates": true, "changes": true},
+  "metadata": {"generates": true, "source": true},
   "params": [
     { "name": "fields", "type": "field", "array": true, "required": true },
     { "name": "as", "type": "string", "array": true, "length": 2, "default": ["key", "value"] }
@@ -24,63 +26,26 @@ Fold.Definition = {
 
 var prototype = inherits(Fold, Transform);
 
-function keyFunction(f) {
-  return f.fields.join('|');
-}
-
 prototype.transform = function(_, pulse) {
-  var cache = this.value,
-      reset = _.modified('fields'),
+  var out = pulse.fork(pulse.NO_SOURCE),
       fields = _.fields,
+      fnames = fields.map(accessorName),
       as = _.as || ['key', 'value'],
-      key = as[0],
-      value = as[1],
-      keys = fields.map(keyFunction),
-      n = fields.length,
-      stamp = pulse.stamp,
-      out = pulse.fork(pulse.NO_SOURCE),
-      i = 0, mask = 0, id;
+      k = as[0],
+      v = as[1],
+      n = fields.length;
 
-  function add(t) {
-    var f = (cache[tupleid(t)] = Array(n)); // create cache of folded tuples
-    for (var i=0, ft; i<n; ++i) { // for each key, derive folds
-      ft = (f[i] = derive(t));
-      ft[key] = keys[i];
-      ft[value] = fields[i](t);
-      out.add.push(ft);
+  out.rem = this.value;
+
+  pulse.visit(pulse.SOURCE, function(t) {
+    for (var i=0, d; i<n; ++i) {
+      d = derive(t);
+      d[k] = fnames[i];
+      d[v] = fields[i](t);
+      out.add.push(d);
     }
-  }
+  });
 
-  function mod(t) {
-    var f = cache[tupleid(t)]; // get cache of folded tuples
-    for (var i=0, ft; i<n; ++i) { // for each key, rederive folds
-      if (!(mask & (1 << i))) continue; // field is unchanged
-      ft = rederive(t, f[i], stamp);
-      ft[key] = keys[i];
-      ft[value] = fields[i](t);
-      out.mod.push(ft);
-    }
-  }
-
-  if (reset) {
-    // on reset, remove all folded tuples and clear cache
-    for (id in cache) out.rem.push.apply(out.rem, cache[id]);
-    cache = this.value = {};
-    pulse.visit(pulse.SOURCE, add);
-  } else {
-    pulse.visit(pulse.ADD, add);
-
-    for (; i<n; ++i) {
-      if (pulse.modified(fields[i].fields)) mask |= (1 << i);
-    }
-    if (mask) pulse.visit(pulse.MOD, mod);
-
-    pulse.visit(pulse.REM, function(t) {
-      var id = tupleid(t);
-      out.rem.push.apply(out.rem, cache[id]);
-      cache[id] = null;
-    });
-  }
-
+  this.value = out.source = out.add;
   return out.modifies(as);
 };
