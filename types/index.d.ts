4df0af148e751eac68938c4d4ed72606b1701834
Tom Crockett <pelotom@gmail.com>
2018-02-22 01:22:23 -0800

    Reexport all types from the root

--- types/index.d.ts
+++ types/index.d.ts
@@ -9,6 +9,30 @@ import { Background } from './background';
 import { Padding } from './padding';
 import { Scope } from './scope';
 import { EncodeEntry, Encode } from './encode';
+
+export * from './autosize'
+export * from './axis'
+export * from './background'
+export * from './bind'
+export * from './data'
+export * from './encode'
+export * from './expr'
+export * from './layout'
+export * from './legend'
+export * from './mark'
+export * from './marktype'
+export * from './on-events'
+export * from './on-trigger'
+export * from './padding'
+export * from './projection'
+export * from './scale'
+export * from './scope'
+export * from './selector'
+export * from './signal'
+export * from './stream'
+export * from './title'
+export * from './transform'
+
 export interface Spec extends Scope, Encode<EncodeEntry> {
     '$schema': string;
     width?: number;
