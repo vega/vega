62b869038b948346bda2242250d4300b5cce457b
Tom Crockett <pelotom@gmail.com>
2018-02-22 12:44:55 -0800

    Add a spec title test

---
+++ tests/spec/valid/spec-title.ts
@@ -0,0 +1,23 @@
+import { Spec } from 'vega';
+
+const spec: Spec = {
+  $schema: 'https://vega.github.io/schema/vega/v3.json',
+
+  title: {
+    text: 'Hello World',
+    offset: 15,
+    interactive: true,
+    encode: {
+      update: {
+        fontWeight: { value: '400' },
+        fontSize: { value: 12 },
+        fontStyle: { value: 'italic' },
+        fill: { value: 'rgba(0, 0, 0, 0.54)' },
+      },
+      hover: {
+        fontStyle: { value: 'italic' },
+        cursor: { value: 'help' },
+      },
+    },
+  },
+};
