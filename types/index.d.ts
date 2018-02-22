e5cd22b6eaa2f6836729b7bdd78de6efe5f8b565
Tom Crockett <pelotom@gmail.com>
2018-02-22 01:49:07 -0800

    Move spec types into a subdirectory

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,48 +1,6 @@
 // TypeScript Version: 2.2
 
-// ============================================================================================
-// Spec typings adapted from https://github.com/vega/vega-parser/tree/as/typings
-// ============================================================================================
-
-import { AutoSize } from './autosize';
-import { Background } from './background';
-import { Padding } from './padding';
-import { Scope } from './scope';
-import { EncodeEntry, Encode } from './encode';
-
-export * from './autosize';
-export * from './axis';
-export * from './background';
-export * from './bind';
-export * from './data';
-export * from './encode';
-export * from './expr';
-export * from './layout';
-export * from './legend';
-export * from './mark';
-export * from './marktype';
-export * from './on-events';
-export * from './on-trigger';
-export * from './padding';
-export * from './projection';
-export * from './scale';
-export * from './scope';
-export * from './selector';
-export * from './signal';
-export * from './stream';
-export * from './title';
-export * from './transform';
-
-export interface Spec extends Scope, Encode<EncodeEntry> {
-  $schema: string;
-  width?: number;
-  height?: number;
-  config?: any;
-  description?: string;
-  padding?: Padding;
-  autosize?: AutoSize;
-  background?: Background;
-}
+export * from './spec';
 
 // ============================================================================================
 // Runtime typings adapted from https://github.com/vega/vega-lite/blob/master/typings/vega.d.ts
---
+++ types/spec/index.d.ts
@@ -0,0 +1,38 @@
+import { AutoSize } from './autosize';
+import { Background } from './background';
+import { Padding } from './padding';
+import { Scope } from './scope';
+import { EncodeEntry, Encode } from './encode';
+
+export interface Spec extends Scope, Encode<EncodeEntry> {
+  $schema: string;
+  width?: number;
+  height?: number;
+  config?: any;
+  description?: string;
+  padding?: Padding;
+  autosize?: AutoSize;
+  background?: Background;
+}
+
+export * from './autosize';
+export * from './axis';
+export * from './background';
+export * from './bind';
+export * from './data';
+export * from './encode';
+export * from './expr';
+export * from './layout';
+export * from './legend';
+export * from './mark';
+export * from './marktype';
+export * from './on-events';
+export * from './on-trigger';
+export * from './padding';
+export * from './projection';
+export * from './scale';
+export * from './scope';
+export * from './selector';
+export * from './signal';
+export * from './stream';
+export * from './title';
