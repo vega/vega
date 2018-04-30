5145f3ff6d831e128554eac99d745dd1eaaa1877
Jeffrey Heer <jeffrey.heer@gmail.com>
2018-04-30 10:06:39 -0700

    Centralize error traps, add trap for signal listeners.

--- packages/vega-view/src/initialize-handler.js
+++ packages/vega-view/src/initialize-handler.js
@@ -1,10 +1,13 @@
 import {offset} from './render-size';
+import trap from './trap';
 
 export default function(view, prevHandler, el, constructor) {
-  var handler = new constructor(view.loader(), tooltip(view))
+  // instantiate scenegraph handler
+  var handler = new constructor(view.loader(), trap(view, view.tooltip()))
     .scene(view.scenegraph().root)
     .initialize(el, offset(view), view);
 
+  // transfer event handlers
   if (prevHandler) {
     prevHandler.handlers().forEach(function(h) {
       handler.on(h.type, h.handler);
@@ -12,22 +15,4 @@ export default function(view, prevHandler, el, constructor) {
   }
 
   return handler;
-}
-
-// wrap tooltip handler to trap errors
-function tooltip(view) {
-  var handler = view.tooltip(),
-      tooltip = null;
-
-  if (handler) {
-    tooltip = function() {
-      try {
-        handler.apply(this, arguments);
-      } catch (error) {
-        view.error(error);
-      }
-    };
-  }
-
-  return tooltip;
 }
