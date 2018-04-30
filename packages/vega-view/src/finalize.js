87ab84c1cb8d7be5893fa81769671cb5e04431e0
Jeffrey Heer <jeffrey.heer@gmail.com>
2018-04-30 10:07:50 -0700

    Add tooltip reset to finalize method.

--- packages/vega-view/src/finalize.js
+++ packages/vega-view/src/finalize.js
@@ -1,8 +1,11 @@
 /**
+ * Finalize a View instance that is being removed.
  * Remove all external event listeners.
+ * Remove any currently displayed tooltip.
  */
 export default function() {
-  var listeners = this._eventListeners,
+  var tooltip = this._tooltip,
+      listeners = this._eventListeners,
       n = listeners.length, m, e;
 
   while (--n >= 0) {
@@ -11,5 +14,9 @@ export default function() {
     while (--m >= 0) {
       e.sources[m].removeEventListener(e.type, e.handler);
     }
+  }
+
+  if (tooltip) {
+    tooltip.call(this, this._handler, null, null, null);
   }
 }
