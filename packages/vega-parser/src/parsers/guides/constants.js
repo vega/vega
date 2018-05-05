16c66fbc72ff285e72c422575cd00b766cc59da3
Jeffrey Heer <jeffrey.heer@gmail.com>
2018-05-04 22:26:21 -0700

    Update guide constants.

--- packages/vega-parser/src/parsers/guides/constants.js
+++ packages/vega-parser/src/parsers/guides/constants.js
@@ -2,22 +2,35 @@ export var Top = 'top';
 export var Left = 'left';
 export var Right = 'right';
 export var Bottom = 'bottom';
+export var Center = 'center';
+
+export var Vertical = 'vertical';
+
+export var Start = 'start';
+export var Middle = 'middle';
+export var End = 'end';
 
 export var Index  = 'index';
 export var Label  = 'label';
 export var Offset = 'offset';
 export var Perc   = 'perc';
+export var Perc2  = 'perc2';
 export var Size   = 'size';
-export var Total  = 'total';
 export var Value  = 'value';
 
 export var GuideLabelStyle = 'guide-label';
 export var GuideTitleStyle = 'guide-title';
 export var GroupTitleStyle = 'group-title';
 
+export var Symbols = 'symbol';
+export var Gradient = 'gradient';
+export var Discrete = 'discrete';
+
+// Encoding channels supported by legends
+// In priority order of 'canonical' scale
 export var LegendScales = [
-  'shape',
   'size',
+  'shape',
   'fill',
   'stroke',
   'strokeDash',
