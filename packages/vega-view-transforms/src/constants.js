c4d2a6f0f452b2e502a770828e988b77ed669ccc
Jeffrey Heer <jeffrey.heer@gmail.com>
2018-05-04 21:59:16 -0700

    Lift constants to shared file.

---
+++ packages/vega-view-transforms/src/constants.js
@@ -0,0 +1,40 @@
+export var Top = 'top';
+export var Left = 'left';
+export var Right = 'right';
+export var Bottom = 'bottom';
+
+export var Start = 'start';
+export var Middle = 'middle';
+export var End = 'end';
+
+export var Group = 'group';
+
+export var AxisRole = 'axis';
+export var TitleRole = 'title';
+export var FrameRole = 'frame';
+export var ScopeRole = 'scope';
+export var LegendRole = 'legend';
+
+export var RowHeader = 'row-header';
+export var RowFooter = 'row-footer';
+export var RowTitle  = 'row-title';
+export var ColHeader = 'column-header';
+export var ColFooter = 'column-footer';
+export var ColTitle  = 'column-title';
+
+export var Padding = 'padding';
+
+export var Fit  = 'fit';
+export var FitX = 'fit-x';
+export var FitY = 'fit-y';
+export var Pad  = 'pad';
+export var None = 'none';
+
+export var All = 'all';
+export var Each = 'each';
+export var Flush = 'flush';
+
+export var Column = 'column';
+export var Row = 'row';
+
+
