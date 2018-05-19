b0d73bd947385e2bf83e448e559754187eaaa54f
Jeffrey Heer <jeffrey.heer@gmail.com>
2018-05-18 20:31:49 -0700

    Fix flush axis label alignment. (vega/vega#1278)

--- packages/vega-parser/src/parsers/expression/flush.js
+++ packages/vega-parser/src/parsers/expression/flush.js
@@ -1,8 +1,20 @@
 import {peek} from 'vega-util';
 
 export default function(range, value, threshold, left, right, center) {
-  var l = Math.abs(value - range[0]),
-      r = Math.abs(peek(range) - value);
+  var a = range[0],
+      b = peek(range),
+      l, r;
+
+  // swap endpoints if range is reversed
+  if (b < a) {
+    l = a; a = b; b = l;
+  }
+
+  // compare value to endpoints
+  l = Math.abs(value - a);
+  r = Math.abs(b - value);
+
+  // adjust if value within threshold distance of endpoint
   return l < r && l <= threshold ? left
     : r <= threshold ? right
     : center;
