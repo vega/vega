c21e95d711d59259a878899c25b1a3c1da612001
Jeffrey Heer <jeffrey.heer@gmail.com>
2018-06-18 10:20:27 -0700

    Export scaleImplicit.

--- packages/vega-scale/index.js
+++ packages/vega-scale/index.js
@@ -3,3 +3,4 @@ export {default as scale} from './src/scales';
 export {interpolate, interpolateRange, scaleFraction} from './src/interpolate';
 export {scheme, schemeDiscretized} from './src/schemes';
 export {timeInterval, utcInterval} from './src/timeInterval';
+export {scaleImplicit} from 'd3-scale';
