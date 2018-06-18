ff1e25448bc3ad409aa8a2fecdcd0fc1488b2527
Jeffrey Heer <jeffrey.heer@gmail.com>
2018-06-18 15:22:58 -0700

    Update tests.

--- packages/vega-transforms/test/project-test.js
+++ packages/vega-transforms/test/project-test.js
@@ -26,7 +26,14 @@ tape('Project copies tuples', function(test) {
   test.deepEqual(p.add.map(id), [0, 1]);
 
   // test simultaneous remove and add
-  df.pulse(c, changeset().remove(data[0]).insert(data[0])).run();
+  // fake changeset to test invalid configuration
+  df.pulse(c, {
+    pulse: function(p) {
+      p.add.push(data[0]);
+      p.rem.push(data[0]);
+      return p;
+    }
+  }).run();
   p = r.pulse;
   test.equal(p.add.length, 1);
   test.equal(p.rem.length, 1);
@@ -54,6 +61,7 @@ tape('Project copies tuples', function(test) {
   test.equal(p.add.length, 0);
   test.equal(p.rem.length, 2);
   test.equal(p.mod.length, 0);
+  p.rem.sort(function(a, b) { return a.id - b.id; });
   test.notEqual(p.rem[0], data[0]);
   test.notEqual(p.rem[1], data[1]);
   test.deepEqual(p.rem.map(id), [2, 3]);
@@ -84,7 +92,14 @@ tape('Project projects tuples', function(test) {
   test.deepEqual(p.add.map(id), [0, 1]);
 
   // test simultaneous remove and add
-  df.pulse(c, changeset().remove(data[0]).insert(data[0])).run();
+  // fake changeset to test invalid configuration
+  df.pulse(c, {
+    pulse: function(p) {
+      p.add.push(data[0]);
+      p.rem.push(data[0]);
+      return p;
+    }
+  }).run();
   p = r.pulse;
   test.equal(p.add.length, 1);
   test.equal(p.rem.length, 1);
@@ -112,6 +127,7 @@ tape('Project projects tuples', function(test) {
   test.equal(p.add.length, 0);
   test.equal(p.rem.length, 2);
   test.equal(p.mod.length, 0);
+  p.rem.sort(function(a, b) { return a.id - b.id; });
   test.notEqual(p.rem[0], data[0]);
   test.notEqual(p.rem[1], data[1]);
   test.deepEqual(p.rem.map(id), [2, 3]);
@@ -146,7 +162,14 @@ tape('Project aliases tuples', function(test) {
   test.deepEqual(p.add.map(foo), ['a', 'b']);
 
   // test simultaneous remove and add
-  df.pulse(c, changeset().remove(data[0]).insert(data[0])).run();
+  // fake changeset to test invalid configuration
+  df.pulse(c, {
+    pulse: function(p) {
+      p.add.push(data[0]);
+      p.rem.push(data[0]);
+      return p;
+    }
+  }).run();
   p = r.pulse;
   test.equal(p.add.length, 1);
   test.equal(p.rem.length, 1);
@@ -175,6 +198,7 @@ tape('Project aliases tuples', function(test) {
   test.equal(p.add.length, 0);
   test.equal(p.rem.length, 2);
   test.equal(p.mod.length, 0);
+  p.rem.sort(function(a, b) { return a.key - b.key; });
   test.notEqual(p.rem[0], data[0]);
   test.notEqual(p.rem[1], data[1]);
   test.deepEqual(p.rem.map(key), [2, 3]);
@@ -213,7 +237,14 @@ tape('Project projects tuples with nested properties', function(test) {
   test.deepEqual(p.add.map(foo), ['a', 'b']);
 
   // test simultaneous remove and add
-  df.pulse(c, changeset().remove(data[0]).insert(data[0])).run();
+  // fake changeset to test invalid configuration
+  df.pulse(c, {
+    pulse: function(p) {
+      p.add.push(data[0]);
+      p.rem.push(data[0]);
+      return p;
+    }
+  }).run();
   p = r.pulse;
   test.equal(p.add.length, 1);
   test.equal(p.rem.length, 1);
@@ -242,6 +273,7 @@ tape('Project projects tuples with nested properties', function(test) {
   test.equal(p.add.length, 0);
   test.equal(p.rem.length, 2);
   test.equal(p.mod.length, 0);
+  p.rem.sort(function(a, b) { return a.id - b.id; });
   test.notEqual(p.rem[0], data[0]);
   test.notEqual(p.rem[1], data[1]);
   test.deepEqual(p.rem.map(id), [2, 3]);
