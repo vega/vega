8ed83609a274635d84b7b2551dc38e2f79383913
Jeffrey Heer <jeffrey.heer@gmail.com>
2018-06-19 22:15:09 -0700

    Add field/expr as property to schema.

--- packages/vega-parser/schema/expr.js
+++ packages/vega-parser/schema/expr.js
@@ -4,7 +4,8 @@ export default {
       "title": "ExpressionRef",
       "type": "object",
       "properties": {
-        "expr": {"type": "string"}
+        "expr": {"type": "string"},
+        "as": {"type": "string"}
       },
       "required": ["expr"]
     },
