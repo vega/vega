843c5d3c52c52dcd3bbf4e8c5c71ba1d82dde84c
Dominik Moritz <domoritz@gmail.com>
2018-07-08 15:16:20 -0700

    Improve title declarations

--- packages/vega-typings/types/spec/config.d.ts
+++ packages/vega-typings/types/spec/config.d.ts
@@ -27,6 +27,7 @@ import {
 } from './values';
 import { SignalRef } from './signal';
 import { LayoutAlign } from './layout';
+import { TitleBase } from './title';
 
 export interface Config
   extends Partial<Record<MarkConfigKeys, MarkConfig>>,
@@ -393,57 +394,4 @@ export interface LegendConfig<
   strokeDash?: number[];
 }
 
-export interface TitleConfig {
-  /**
-   * The anchor position for placing the title. One of `"start"`, `"middle"`, or `"end"`. For example, with an orientation of top these anchor positions map to a left-, center-, or right-aligned title.
-   *
-   * __Default value:__ `"middle"` for [single](spec.html) and [layered](layer.html) views.
-   * `"start"` for other composite views.
-   *
-   * __Note:__ [For now](https://github.com/vega/vega-lite/issues/2875), `anchor` is only customizable only for [single](spec.html) and [layered](layer.html) views.  For other composite views, `anchor` is always `"start"`.
-   */
-  anchor?: TitleAnchor;
-  /**
-   * Angle in degrees of title text.
-   */
-  angle?: number;
-  /**
-   * Vertical text baseline for title text.
-   */
-  baseline?: TextBaseline;
-  /**
-   * Text color for title text.
-   */
-  color?: string;
-  /**
-   * Font name for title text.
-   */
-  font?: string;
-  /**
-   * Font size in pixels for title text.
-   *
-   * __Default value:__ `10`.
-   *
-   * @minimum 0
-   */
-  fontSize?: number;
-  /**
-   * Font weight for title text.
-   * This can be either a string (e.g `"bold"`, `"normal"`) or a number (`100`, `200`, `300`, ..., `900` where `"normal"` = `400` and `"bold"` = `700`).
-   */
-  fontWeight?: FontWeight;
-  /**
-   * The maximum allowed length in pixels of legend labels.
-   *
-   * @minimum 0
-   */
-  limit?: number;
-  /**
-   * Offset in pixels of the title from the chart body and axes.
-   */
-  offset?: number;
-  /**
-   * Default title orientation ("top", "bottom", "left", or "right")
-   */
-  orient?: TitleOrient;
-}
+export type TitleConfig = TitleBase;
