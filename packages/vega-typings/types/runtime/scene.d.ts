7432123cdeea16bcab31ab1ce08efb3a7ac3ad6b
Dan Marshall <danmar@microsoft.com>
2018-09-07 15:18:34 -0700

    add props to SceneLine, remove SceneCube (#44)

--- packages/vega-typings/types/runtime/scene.d.ts
+++ packages/vega-typings/types/runtime/scene.d.ts
@@ -17,23 +17,20 @@ export interface SceneItem {
 }
 
 export type SceneRect = SceneItem & {
+  fill: string;
   height: number;
   width: number;
 };
 
 export type SceneLine = SceneItem & {
+  opacity: number;
+  stroke: string;
+  strokeWidth: number;
   x2: number;
   y2: number;
 };
 
 export type SceneGroup = SceneRect;
-
-export type SceneCube = SceneRect & {
-  id: number;
-  depth: number;
-  fill: string;
-  z: number;
-};
 
 export type SceneSymbol = SceneItem & {
   fill: string;
