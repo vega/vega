fdf4e408a63d8e5eec323fb24f53dc345b8fe4aa
Jeffrey Heer <jeffrey.heer@gmail.com>
2018-10-04 15:20:25 -0700

    Minor clean up, use peek.

--- packages/vega-parser/src/parsers/expression/scale-gradient.js
+++ packages/vega-parser/src/parsers/expression/scale-gradient.js
@@ -1,6 +1,7 @@
 import {getScale} from './scale';
 import {Gradient} from 'vega-scenegraph';
 import {scaleFraction} from 'vega-scale';
+import {peek} from 'vega-util';
 
 export default function(scale, p0, p1, count, group) {
   scale = getScale(scale, (group || this).context);
@@ -8,13 +9,13 @@ export default function(scale, p0, p1, count, group) {
   var gradient = Gradient(p0, p1),
       stops = scale.domain(),
       min = stops[0],
-      max = stops[stops.length-1],
+      max = peek(stops),
       fraction = scaleFraction(scale, min, max);
 
   if (scale.ticks) {
     stops = scale.ticks(+count || 15);
     if (min !== stops[0]) stops.unshift(min);
-    if (max !== stops[stops.length-1]) stops.push(max);
+    if (max !== peek(stops)) stops.push(max);
   }
 
   for (var i=0, n=stops.length; i<n; ++i) {
