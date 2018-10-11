4b9b4601bfc7ac9684daa995d36ccf83e2b833ff
Jeffrey Heer <jeffrey.heer@gmail.com>
2018-10-11 13:42:37 -0700

    Add test cases.

---
+++ packages/vega-force/test/force-test.js
@@ -0,0 +1,36 @@
+var tape = require('tape'),
+    vega = require('vega-dataflow'),
+    Collect = require('vega-transforms').collect,
+    Force = require('../').force;
+
+tape('Force places points', function(test) {
+  var data = [
+    {label: 'a'},
+    {label: 'b'},
+    {label: 'c'},
+    {label: 'd'}
+  ];
+
+  var df = new vega.Dataflow(),
+      c0 = df.add(Collect);
+
+  df.add(Force, {
+    static: true,
+    forces: [
+      {force: 'x', x: 100},
+      {force: 'y', y: 100},
+      {force: 'nbody'}
+    ],
+    pulse: c0
+  });
+
+  df.pulse(c0, vega.changeset().insert(data)).run();
+  test.equal(c0.value.length, data.length);
+
+  for (var i=0, n=data.length; i<n; ++i) {
+    test.ok(data[i].x != null && !isNaN(data[i].x));
+    test.ok(data[i].y != null && !isNaN(data[i].y));
+  }
+
+  test.end();
+});
