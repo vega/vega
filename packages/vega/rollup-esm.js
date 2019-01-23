5109a3cc8d07c959b74cb1fcc716b1b6fbf01ad4
eterna2 <eterna2@hotmail.com>
2019-01-23 19:42:54 +0800

    updated vega to build es modules

---
+++ packages/vega/rollup-esm.js
@@ -0,0 +1,57 @@
+const rollup = require('rollup'),
+      json = require('rollup-plugin-json'),
+      nodeResolve = require('rollup-plugin-node-resolve'),
+      externals = process.argv[2] === '-e',
+      output = externals ? 'vega-core.mjs' : 'vega.mjs';
+
+const external = [].concat(!externals ? [] : [
+  'd3-array',
+  'd3-color',
+  'd3-contour',
+  'd3-dsv',
+  'd3-force',
+  'd3-format',
+  'd3-geo',
+  'd3-hierarchy',
+  'd3-interpolate',
+  'd3-path',
+  'd3-scale',
+  'd3-scale-chromatic',
+  'd3-shape',
+  'd3-time',
+  'd3-time-format',
+  'd3-timer',
+  'd3-voronoi',
+  'topojson-client'
+]);
+
+const globals = external.reduce(function(map, _) {
+  map[_] = _.split('-')[0]; // map package name to prefix
+  return map;
+}, {});
+
+rollup.rollup({
+  input: 'index.js',
+  external: external,
+  plugins: [
+    nodeResolve({module: true, browser: true}),
+    json()
+  ],
+  onwarn: function(warning) {
+    // suppress circular dependency warnings
+    if (warning.code !== 'CIRCULAR_DEPENDENCY') {
+      console.warn(warning.code, warning.message);
+    }
+  }
+}).then(function(bundle) {
+  return bundle.write({
+    file: 'build/' + output,
+    format: 'es'
+  });
+}).then(function() {
+  console.warn('↳ build/' + output);
+}).catch(abort);
+
+function abort(error) {
+  console.error(error.stack);
+}
