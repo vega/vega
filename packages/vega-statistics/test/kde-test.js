8dec28c45d525972b38b84e04a77af3b15bf82cd
Jeffrey Heer <jeffrey.heer@gmail.com>
2019-02-21 11:40:32 -0800

    Add seeded random to more statistics tests.

--- packages/vega-statistics/test/kde-test.js
+++ packages/vega-statistics/test/kde-test.js
@@ -1,16 +1,8 @@
 var tape = require('tape'),
     d3 = require('d3-array'),
+    lcg = require('./lcg'),
     stats = require('../'),
     gaussian = stats.randomNormal();
-
-function lcg(seed) {
-  // Random numbers using a Linear Congruential Generator with seed value
-  // Uses glibc values from https://en.wikipedia.org/wiki/Linear_congruential_generator
-  return function() {
-    seed = (1103515245 * seed + 12345) % 2147483647;
-    return seed / 2147483647;
-  };
-}
 
 // seeded RNG for deterministic tests
 stats.setRandom(lcg(123456789));
