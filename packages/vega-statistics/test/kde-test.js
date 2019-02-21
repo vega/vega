f792738f2efbdb7174fa1e3db059f291fe065641
Jeffrey Heer <jeffrey.heer@gmail.com>
2019-02-21 10:47:32 -0800

    Make KDE tests deterministic.

--- packages/vega-statistics/test/kde-test.js
+++ packages/vega-statistics/test/kde-test.js
@@ -3,6 +3,18 @@ var tape = require('tape'),
     stats = require('../'),
     gaussian = stats.randomNormal();
 
+function lcg(seed) {
+  // Random numbers using a Linear Congruential Generator with seed value
+  // Uses glibc values from https://en.wikipedia.org/wiki/Linear_congruential_generator
+  return function() {
+    seed = (1103515245 * seed + 12345) % 2147483647;
+    return seed / 2147483647;
+  };
+}
+
+// seeded RNG for deterministic tests
+stats.setRandom(lcg(123456789));
+
 function closeTo(t, a, b, delta) {
   t.equal(Math.abs(a-b) < delta, true);
 }
