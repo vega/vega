766945ff56a68356e1aed73e53b9888dba6cdddd
Jeffrey Heer <jeffrey.heer@gmail.com>
2019-02-25 21:33:53 -0800

    Fix legend clipHeight bounds flag bug.

--- packages/vega-parser/src/parsers/guides/legend-symbol-groups.js
+++ packages/vega-parser/src/parsers/guides/legend-symbol-groups.js
@@ -118,7 +118,7 @@ export default function(spec, config, userEncode, dataRef, columns) {
   // -- LEGEND ENTRY GROUPS --
   encode = {
     enter: {
-      noBound: {value: true}, // ignore width/height in bounds calc
+      noBound: {value: !height}, // ignore width/height in bounds calc
       width: zero,
       height: height ? encoder(height) : zero,
       opacity: zero
