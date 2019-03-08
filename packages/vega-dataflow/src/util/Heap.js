01a8cec098955d7fdb63f37479a93f3f105a2728
Jeffrey Heer <jeffrey.heer@gmail.com>
2019-03-07 20:29:41 -0800

    Simplify dataflow heap util.

--- packages/vega-dataflow/src/util/Heap.js
+++ packages/vega-dataflow/src/util/Heap.js
@@ -1,61 +1,25 @@
-export default function Heap(comparator) {
-  this.cmp = comparator;
-  this.nodes = [];
+export default function Heap(cmp) {
+  var nodes = [];
+  return {
+    size: () => nodes.length,
+    peek: () => nodes[0],
+    push: x => {
+      nodes.push(x);
+      return siftdown(nodes, 0, nodes.length - 1, cmp);
+    },
+    pop: () => {
+      var last = nodes.pop(), item;
+      if (nodes.length) {
+        item = nodes[0];
+        nodes[0] = last;
+        siftup(nodes, 0, cmp);
+      } else {
+        item = last;
+      }
+      return item;
+    }
+  };
 }
-
-var prototype = Heap.prototype;
-
-prototype.size = function() {
-  return this.nodes.length;
-};
-
-prototype.clear = function() {
-  this.nodes = [];
-  return this;
-};
-
-prototype.peek = function() {
-  return this.nodes[0];
-};
-
-prototype.push = function(x) {
-  var array = this.nodes;
-  array.push(x);
-  return siftdown(array, 0, array.length-1, this.cmp);
-};
-
-prototype.pop = function() {
-  var array = this.nodes,
-      last = array.pop(),
-      item;
-
-  if (array.length) {
-    item = array[0];
-    array[0] = last;
-    siftup(array, 0, this.cmp);
-  } else {
-    item = last;
-  }
-  return item;
-};
-
-prototype.replace = function(item) {
-  var array = this.nodes,
-      retval = array[0];
-  array[0] = item;
-  siftup(array, 0, this.cmp);
-  return retval;
-};
-
-prototype.pushpop = function(item) {
-  var array = this.nodes, ref = array[0];
-  if (array.length && this.cmp(ref, item) < 0) {
-    array[0] = item;
-    item = ref;
-    siftup(array, 0, this.cmp);
-  }
-  return item;
-};
 
 function siftdown(array, start, idx, cmp) {
   var item, parent, pidx;
@@ -78,7 +42,7 @@ function siftup(array, idx, cmp) {
   var start = idx,
       end = array.length,
       item = array[idx],
-      cidx = 2 * idx + 1, ridx;
+      cidx = (idx << 1) + 1, ridx;
 
   while (cidx < end) {
     ridx = cidx + 1;
@@ -87,7 +51,7 @@ function siftup(array, idx, cmp) {
     }
     array[idx] = array[cidx];
     idx = cidx;
-    cidx = 2 * idx + 1;
+    cidx = (idx << 1) + 1;
   }
   array[idx] = item;
   return siftdown(array, start, idx, cmp);
