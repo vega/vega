d53253a7f259b231fd8757c13c816956220d3bc3
palewire <ben.welsh@gmail.com>
2019-04-08 21:09:49 -0700

    Source schema item for #1458

---
+++ packages/vega-schema/src/source.js
@@ -0,0 +1,51 @@
+import {
+  numberValue, stringValue, stringOrSignal, anchorValue,
+  alignValue, baselineValue, colorValue, fontWeightValue,
+  def, enums, object, oneOf, pattern, ref,
+  booleanType, numberType, stringType, orSignal
+} from './util';
+
+// types defined elsewhere
+const encodeEntryRef = def('encodeEntry');
+const stringValueRef = ref('stringValue');
+const styleRef = ref('style');
+
+const sourceOrientEnum = ['none', 'left', 'right', 'top', 'bottom'];
+const sourceFrameEnum = ['group', 'bounds'];
+
+const sourceEncode = pattern({
+  '^(?!interactive|name|style).+$': encodeEntryRef,
+});
+
+const title = oneOf(
+  stringType,
+  object({
+    name: stringType,
+    orient: orSignal(enums(sourceOrientEnum, {default: 'bottom'})),
+    anchor: anchorValue,
+    frame: oneOf(enums(sourceFrameEnum), stringValueRef),
+    offset: numberValue,
+    style: styleRef,
+    text: stringOrSignal,
+    zindex: numberType,
+    interactive: booleanType,
+    align: alignValue,
+    angle: numberValue,
+    baseline: baselineValue,
+    color: colorValue,
+    dx: numberValue,
+    dy: numberValue,
+    font: stringValue,
+    fontSize: numberValue,
+    fontStyle: stringValue,
+    fontWeight: fontWeightValue,
+    limit: numberValue,
+    encode: sourceEncode
+  })
+);
+
+export default {
+  defs: {
+    title
+  }
+};
