d89ce009c53bf295fbf94a5e46670f28b2ce1553
Jeffrey Heer <jeffrey.heer@gmail.com>
2019-05-15 15:21:11 -0700

    Add helpful comments to sampleCurve.

--- packages/vega-statistics/src/sampleCurve.js
+++ packages/vega-statistics/src/sampleCurve.js
@@ -34,11 +34,17 @@ export default function(f, extent, minSteps, maxSteps) {
       p1 = next[next.length - 1];
 
   while (p1) {
+    // midpoint for potential curve subdivision
     const pm = point((p0[0] + p1[0]) / 2);
 
     if (pm[0] - p0[0] >= stop && angleDelta(p0, pm, p1) > MIN_RADIANS) {
+      // maximum resolution has not yet been met, and
+      // subdivision midpoint sufficiently different from endpoint
+      // save subdivision, push midpoint onto the visitation stack
       next.push(pm);
     } else {
+      // subdivision midpoint sufficiently similar to endpoint
+      // skip subdivision, store endpoint, move to next point on the stack
       p0 = p1;
       prev.push(p1);
       next.pop();
