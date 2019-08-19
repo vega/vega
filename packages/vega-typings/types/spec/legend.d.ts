98bcde00123771018f25d6bd76665ab42c51122a
Dan Marshall <danmar@microsoft.com>
2019-08-19 12:54:46 -0700

    add deprecated gradientWidth

--- packages/vega-typings/types/spec/legend.d.ts
+++ packages/vega-typings/types/spec/legend.d.ts
@@ -302,6 +302,14 @@ export interface BaseLegend<
    */
   gradientStrokeWidth?: N;
 
+  /**
+   * Deprecated, use gradientLength instead. If gradientLength is not defined, this value will be used instead.
+   *
+   * __Default value:__ `100`.
+   * @minimum 0
+   */
+  gradientWidth?: NS;
+
   // ---------- Symbol Layout ----------
   /**
    * The height in pixels to clip symbol legend entries and limit their size.
