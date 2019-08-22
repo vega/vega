783fb09e737398f2916c97f2a69b9b8d44a76cbc
Jeffrey Heer <jeffrey.heer@gmail.com>
2019-08-22 09:49:22 +0200

    Clean sel compare code, remove Function ctr

--- packages/vega-selections/src/comparisonTest.js
+++ packages/vega-selections/src/comparisonTest.js
@@ -1,32 +1,21 @@
-import { field } from 'vega-util';
-
-var LT = 'lt',
-    LTE  = 'lte',
-    GT = 'gt',
-    GTE = 'gte',
-    EQUAL = 'equal',
-    NEQUAL = 'nequal'
-
-
-var ops = {};
-ops[LT] = '<';
-ops[LTE] = '<=';
-ops[GT] = '>';
-ops[GTE] = '>=';
-ops[EQUAL] = '==';
-ops[NEQUAL] = '!=';
+import {error, field} from 'vega-util';
 
 export function comparisonTest(store, datum, def) {
-    var data = this.context.data[store],
-        aggregates = data ? data.values.value : [];
-
+    const data = this.context.data[store],
+          aggregates = data ? data.values.value : [];
+  
     if (!aggregates.length) return true;
 
-    var sval = aggregates[0][def.sfieldAggregate]
-    var on = def.on;
-    var getter = field(on);
-    var dval = getter(datum);
+    const b = aggregates[0][def.sfieldAggregate],
+          a = field(def.on)(datum);
 
-    var result = new Function('dval', 'sval', `return Boolean(dval ${ops[def.operator]} sval)`);
-    return result(dval, sval)
+    switch (def.operator) {
+      case '<':   return a < b;
+      case '<=':  return a <= b;
+      case '>':   return a > b;
+      case '>=':  return a >= b;
+      case '==':  return a == b;
+      case '!=':  return a != b;
+      default:    error('Unrecognized comparison operator: ' + def.operator);
+    }
 }
