a8fa93e2132cc69fa48d7059da4b9822d4dcd7dd
Jeffrey Heer <jeffrey.heer@gmail.com>
2019-08-22 10:49:15 +0200

    Use d3-delaunay instead of d3-voronoi. (#1943)
    * Replace d3-voronoi with d3-delaunay. Fixes #1492
    * Fix extent, expansion
    * Suppress duplicate Voronoi vertices, more tests.

--- packages/vega-voronoi/src/Voronoi.js
+++ packages/vega-voronoi/src/Voronoi.js
@@ -1,6 +1,6 @@
 import {Transform} from 'vega-dataflow';
 import {inherits} from 'vega-util';
-import {voronoi} from 'd3-voronoi';
+import {Delaunay} from 'd3-delaunay';
 
 export default function Voronoi(params) {
   Transform.call(this, null, params);
@@ -20,29 +20,36 @@ Voronoi.Definition = {
   ]
 };
 
-var prototype = inherits(Voronoi, Transform);
+const prototype = inherits(Voronoi, Transform);
 
-var defaultExtent = [[-1e5, -1e5], [1e5, 1e5]];
+const defaultExtent = [-1e5, -1e5, 1e5, 1e5];
 
 prototype.transform = function(_, pulse) {
-  var as = _.as || 'path',
-      data = pulse.source,
-      diagram, polygons, i, n;
+  const as = _.as || 'path',
+      data = pulse.source;
+  let delaunay, extent, voronoi, polygon, i, n;
 
   // configure and construct voronoi diagram
-  diagram = voronoi().x(_.x).y(_.y);
-  if (_.size) diagram.size(_.size);
-  else diagram.extent(_.extent || defaultExtent);
-
-  this.value = (diagram = diagram(data));
+  delaunay = Delaunay.from(data, _.x, _.y);
+  extent = _.size ? [0, 0 , ..._.size] : _.extent ? [..._.extent[0], ..._.extent[1]] : defaultExtent;
+  this.value = (voronoi = delaunay.voronoi(extent))
 
   // map polygons to paths
-  polygons = diagram.polygons();
   for (i=0, n=data.length; i<n; ++i) {
-    data[i][as] = polygons[i]
-      ? 'M' + polygons[i].join('L') + 'Z'
-      : null;
+    polygon = voronoi.cellPolygon(i);
+    data[i][as] = polygon ? toPathString(polygon) : null;
   }
 
   return pulse.reflow(_.modified()).modifies(as);
 };
+
+// suppress duplicated end point vertices
+function toPathString(p) {
+  const x = p[0][0],
+        y = p[0][1];
+
+  let n = p.length - 1;
+  for (; p[n][0] === x && p[n][1] === y; --n);
+
+  return 'M' + p.slice(0, n + 1).join('L') + 'Z';
+}
