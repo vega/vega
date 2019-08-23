96cd1bc9dd96cb6d2e2ed8e6fab99b7e8afeb35f
Jeffrey Heer <jeffrey.heer@gmail.com>
2019-08-23 15:19:59 +0200

    Add KDE shared test cases.

--- packages/vega-transforms/test/kde-test.js
+++ packages/vega-transforms/test/kde-test.js
@@ -39,6 +39,36 @@ tape('KDE computes kernel density estimates', function(t) {
   t.end();
 });
 
+tape('KDE computes estimates with shared configurations', function(t) {
+  var data = [
+    {k:'a', v:1}, {k:'a', v:2}, {k:'a', v:2}, {k:'a', v:3},
+    {k:'b', v:1}, {k:'b', v:1}, {k:'b', v:2}
+  ];
+
+  var key = util.field('k'),
+      val = util.field('v'),
+      df = new vega.Dataflow(),
+      col = df.add(Collect),
+      kde = df.add(KDE, {
+        groupby: [key],
+        resolve: 'shared',
+        maxsteps: 10,
+        field: val,
+        pulse: col
+      }),
+      out = df.add(Collect, {pulse: kde});
+
+  // -- test adds
+  df.pulse(col, changeset().insert(data)).run();
+  var d = out.value;
+  t.equal(d.length, 22);
+  for (let i=0; i<11; ++i) {
+    t.equal(d[i].value, d[i+11].value);
+  }
+
+  t.end();
+});
+
 tape('KDE computes unnormalized kernel density estimates', function(t) {
   var data = [
     {k:'a', v:1}, {k:'a', v:2}, {k:'a', v:2}, {k:'a', v:3},
