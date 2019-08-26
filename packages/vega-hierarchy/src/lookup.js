e2b66a13155b1b783882a6156b8065ce5db300a2
Jeffrey Heer <jeffrey.heer@gmail.com>
2019-08-26 21:07:27 +0200

    Use stable sort directives for transforms.

--- packages/vega-hierarchy/src/lookup.js
+++ packages/vega-hierarchy/src/lookup.js
@@ -1,9 +1,15 @@
+import {replace} from 'vega-dataflow';
+
 // Build lookup table mapping tuple keys to tree node instances
+// Also copy tupleid to tree node, to enable stable sorting
 export default function(tree, key, filter) {
   var map = {};
   tree.each(function(node) {
     var t = node.data;
-    if (filter(t)) map[key(t)] = node;
+    if (filter(t)) {
+      map[key(t)] = node;
+      replace(t, node);
+    }
   });
   tree.lookup = map;
   return tree;
