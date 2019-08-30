1917ed3036bd7883b67c14b73527ee05ddfd7b28
Jeffrey Heer <jeffrey.heer@gmail.com>
2019-08-30 12:15:01 +0200

    Update vega-hierarchy to use stable compare.

--- packages/vega-hierarchy/src/lookup.js
+++ packages/vega-hierarchy/src/lookup.js
@@ -1,15 +1,9 @@
-import {replace} from 'vega-dataflow';
-
 // Build lookup table mapping tuple keys to tree node instances
-// Also copy tupleid to tree node, to enable stable sorting
 export default function(tree, key, filter) {
   var map = {};
   tree.each(function(node) {
     var t = node.data;
-    if (filter(t)) {
-      map[key(t)] = node;
-      replace(t, node);
-    }
+    if (filter(t)) map[key(t)] = node;
   });
   tree.lookup = map;
   return tree;
