581bf937ee50c9405fd420c5d08d82abf276e91d
benib <beni.buess@nzz.ch>
2019-09-17 09:25:39 +0200

    luminance calls rgb function, takes a color string as argument now

--- packages/vega-functions/src/luminance.js
+++ packages/vega-functions/src/luminance.js
@@ -1,16 +1,19 @@
+import { rgb } from "d3-color";
+
 // https://www.w3.org/TR/2008/REC-WCAG20-20081211/#relativeluminancedef
 function channel_luminance_value(channelValue) {
   const val = channelValue / 255;
   if (val <= 0.03928) {
-    return val / 12.92
+    return val / 12.92;
   }
   return Math.pow((val + 0.055) / 1.055, 2.4);
 }
 
 export function luminance(color) {
-  const r = channel_luminance_value(color.r);
-  const g = channel_luminance_value(color.g);
-  const b = channel_luminance_value(color.b);
+  const c = rgb(color);
+  const r = channel_luminance_value(c.r);
+  const g = channel_luminance_value(c.g);
+  const b = channel_luminance_value(c.b);
   return 0.2126 * r + 0.7152 * g + 0.0722 * b;
 }
 
