3ea9224f9fcb98340afae63c1363788ec8e15452
benib <beni.buess@nzz.ch>
2019-09-17 09:31:55 +0200

    pass color string to luminance and contrast functions in test

--- packages/vega-functions/test/luminance-test.js
+++ packages/vega-functions/test/luminance-test.js
@@ -1,15 +1,14 @@
 var tape = require('tape'),
-    {luminance, contrast} = require('../').functionContext,
-    {rgb} = require('d3-color');
+  { luminance, contrast } = require('../').functionContext;
 
 tape('luminance calculation extremes', function(t) {
-  t.equal(luminance(rgb("#000000")), 0);
-  t.equal(luminance(rgb("#FFFFFF")), 1);
-  t.end()
+  t.equal(luminance('#000000'), 0);
+  t.equal(luminance('#FFFFFF'), 1);
+  t.end();
 });
 
 tape('contrast calculation extremes', function(t) {
-  t.equal(contrast(rgb("black"), rgb("white")), 21);
-  t.equal(contrast(rgb("black"), rgb("black")), 1);
-  t.end()
+  t.equal(contrast('black', 'white'), 21);
+  t.equal(contrast('black', 'black'), 1);
+  t.end();
 });
