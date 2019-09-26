a63648cabbe863382b117a38a814ce52f8d5aedf
Jeffrey Heer <jeffrey.heer@gmail.com>
2019-09-26 14:38:02 +0200

    Export and document new stats methods.

--- packages/vega-statistics/index.js
+++ packages/vega-statistics/index.js
@@ -1,13 +1,34 @@
 export {default as bin} from './src/bin';
 export {default as bootstrapCI} from './src/bootstrapCI';
+export {default as dotbin} from './src/dotbin';
+export {default as quantiles} from './src/quantiles';
 export {default as quartiles} from './src/quartiles';
 export {random, setRandom} from './src/random';
 export {default as randomLCG} from './src/lcg';
 export {default as randomInteger} from './src/integer';
 export {default as randomKDE} from './src/kde';
+export {
+  default as randomLogNormal,
+  sampleLogNormal,
+  densityLogNormal,
+  cumulativeLogNormal,
+  quantileLogNormal
+} from './src/lognormal';
 export {default as randomMixture} from './src/mixture';
-export {default as randomNormal} from './src/normal';
-export {default as randomUniform} from './src/uniform';
+export {
+  default as randomNormal,
+  sampleNormal,
+  densityNormal,
+  cumulativeNormal,
+  quantileNormal
+} from './src/normal';
+export {
+  default as randomUniform,
+  sampleUniform,
+  densityUniform,
+  cumulativeUniform,
+  quantileUniform
+} from './src/uniform';
 export {default as regressionLinear} from './src/regression/linear';
 export {default as regressionLog} from './src/regression/log';
 export {default as regressionExp} from './src/regression/exp';
