89bf064f61191f6a5b5caf3b1c2b5d964998e03f
Jeffrey Heer <jeffrey.heer@gmail.com>
2019-09-26 14:34:32 +0200

    Add quantiles statistics utility.

---
+++ packages/vega-statistics/test/quantiles-test.js
@@ -0,0 +1,19 @@
+var tape = require('tape'),
+    quantiles = require('../').quantiles;
+
+tape('quantiles calculates quantile values', function(t) {
+  // unsorted
+  var a = [9, 7, 8, 1, 2, 3, 4, 5, 6];
+
+  // with number array
+  t.deepEqual([3, 5, 7], quantiles(a, [0.25, 0.5, 0.75]));
+
+  // with object array
+  t.deepEqual([3, 5, 7], quantiles(
+    a.map(_ => ({v:_})),
+    [0.25, 0.5, 0.75],
+    _ => _.v
+  ));
+
+  t.end();
+});
