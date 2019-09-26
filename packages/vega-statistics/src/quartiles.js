6a5f4c68cf59f34753454a96811eee0ee27ecf11
Jeffrey Heer <jeffrey.heer@gmail.com>
2019-09-26 14:34:32 +0200

    Add quantiles statistics utility.

--- packages/vega-statistics/src/quartiles.js
+++ packages/vega-statistics/src/quartiles.js
@@ -1,16 +1,5 @@
-import numbers from './numbers';
-import {quantile, ascending} from 'd3-array';
+import quantiles from './quantiles';
 
 export default function(array, f) {
-  var values = Float64Array.from(numbers(array, f));
-
-  // don't depend on return value from typed array sort call
-  // protects against undefined sort results in Safari (vega/vega-lite#4964)
-  values.sort(ascending);
-
-  return [
-    quantile(values, 0.25),
-    quantile(values, 0.50),
-    quantile(values, 0.75)
-  ];
+  return quantiles(array, [0.25, 0.50, 0.75], f);
 }
