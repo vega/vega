031af4cc0fda8e64262bd4c441ff618cf673380d
Bob Matcuk <robert.matcuk@omnisci.com>
2019-10-01 13:47:42 -0400

    Fix bug in svg renderer

--- packages/vega-scenegraph/src/SVGRenderer.js
+++ packages/vega-scenegraph/src/SVGRenderer.js
@@ -390,14 +390,18 @@ function bind(item, el, sibling, tag, svg) {
   }
 
   // (re-)insert if (a) not contained in SVG or (b) sibling order has changed
-  if (node.ownerSVGElement !== svg || hasSiblings(item) && node.previousSibling !== sibling) {
+  if (node.ownerSVGElement !== svg || hasSiblings(node, item) && node.previousSibling !== sibling) {
     el.insertBefore(node, sibling ? sibling.nextSibling : el.firstChild);
   }
 
   return node;
 }
 
-function hasSiblings(item) {
+function hasSiblings(node, item) {
+  if (node && node.parentNode) {
+    return node.parentNode.childNodes.length > 1;
+  }
+
   var parent = item.mark || item.group;
   return parent && parent.items.length > 1;
 }
