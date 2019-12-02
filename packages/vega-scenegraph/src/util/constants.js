d66eb350d378b34251e89014cc666460c519f26f
Jeffrey Heer <jeffrey.heer@gmail.com>
2019-12-02 11:13:40 +0100

    Tighten bounds calculation for beziers. (#2213)
    * Tighten bounds calculation for beziers.
    * Optimize bezier bound code.
    * Streamline boundContext code.

--- packages/vega-scenegraph/src/util/constants.js
+++ packages/vega-scenegraph/src/util/constants.js
@@ -1,4 +1,5 @@
-export var DegToRad = Math.PI / 180;
-export var HalfPi = Math.PI / 2;
-export var Tau = Math.PI * 2;
-export var HalfSqrt3 = Math.sqrt(3) / 2;
+export const DegToRad = Math.PI / 180;
+export const Epsilon = 1e-14;
+export const HalfPi = Math.PI / 2;
+export const Tau = Math.PI * 2;
+export const HalfSqrt3 = Math.sqrt(3) / 2;
