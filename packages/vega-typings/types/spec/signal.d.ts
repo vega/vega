fa2d0ef91ad2ba82c805f6e086cfa38d9435340a
Kanit Wongsuphasawat <kanitw@gmail.com>
2020-02-05 14:48:44 -0800

    fix: change @hide to @hidden
    cc: @domoritz It's actually @hidden

--- packages/vega-typings/types/spec/signal.d.ts
+++ packages/vega-typings/types/spec/signal.d.ts
@@ -1,8 +1,8 @@
 import { Binding, Expr, OnEvent } from '.';
 
+// Hide from Vega-Lite schema (Since @hidden isn't used by Vega, we can just add it here for now.)
 /**
- * @hide
- * // Hide from Vega-Lite schema (Since @hide isn't used by Vega, we can just add it here for now.)
+ * @hidden
  */
 export interface SignalRef {
   signal: string;
