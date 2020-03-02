319e4efbf5fdde546b2158bca07f9c2b379b5eff
Jeffrey Heer <jheer@trifacta.com>
2020-03-02 10:26:24 +0100

    Add line-based baseline options to scenegraph schema.

--- packages/vega-scenegraph/schema.js
+++ packages/vega-scenegraph/schema.js
@@ -11,20 +11,20 @@ function extend(_) {
 }
 
 function build(strict) {
-  var schema = duplicate(BASE);
-  for (var type in MARKS) {
+  const schema = duplicate(BASE);
+  for (const type in MARKS) {
     buildMark(type, schema.refs, strict);
   }
   return schema;
 }
 
 function buildMark(type, refs, strict) {
-  var mark = duplicate(MARK_BASE);
+  const mark = duplicate(MARK_BASE);
   mark.properties.marktype.enum = [ type ];
   mark.properties.items.items.$ref += type;
   if (strict) mark.additionalProperties = false;
 
-  var item = duplicate(ITEM_BASE);
+  const item = duplicate(ITEM_BASE);
   extend(item.properties, MARKS[type].properties || {});
   if (strict) item.additionalProperties = false;
 
@@ -35,41 +35,41 @@ function buildMark(type, refs, strict) {
 
 function svg_path() {
   // Based on http://www.w3.org/TR/SVG/paths.html#PathDataBNF
-  var wsp  = '[ \t\r\f]*',
-      csp  = '([ \t\r\f]+,?[ \t\r\f]*|,[ \t\r\f]*)?',
-      sep  = csp + '?',
-      pos  = '[0-9]*\\.?[0-9]+([eE][-+]?[0-9]+)?',
-      num  = '[-+]?' + pos,
-      nseq = num + '(' + sep + num + ')*',
-      p    = num + sep + num,
-      p2   = p + sep + p,
-      p3   = p + '(' + sep + p + '){2}',
-      pseq = p + '(' + sep + p + ')*',
-      arc  = pos + sep + pos + sep + num + csp + '[01]' + sep + '[01]' + sep + p,
-      m  = '[Mm]'   + wsp + pseq,
-      lt = '[LlTt]' + wsp + pseq,
-      hv = '[HhVv]' + wsp + nseq,
-      c  = '[Cc]'   + wsp + p3 + '(' + sep + p3 + ')*',
-      qs = '[QqSs]' + wsp + p2 + '(' + sep + p2 + ')*',
-      a  = '[Aa]'   + wsp + arc + '(' + sep + arc + ')*',
-      z  = '[Zz]',
-      draw = '(' + [z, lt, hv, c, qs, a].join('|') + ')',
-      move = m + wsp + '(' + draw + '(' + sep + draw + ')*' + ')?',
-      path = wsp + '(' + move + '(' + sep + move + ')*' + ')?' + wsp;
+  const wsp  = '[ \t\r\f]*',
+        csp  = '([ \t\r\f]+,?[ \t\r\f]*|,[ \t\r\f]*)?',
+        sep  = csp + '?',
+        pos  = '[0-9]*\\.?[0-9]+([eE][-+]?[0-9]+)?',
+        num  = '[-+]?' + pos,
+        nseq = num + '(' + sep + num + ')*',
+        p    = num + sep + num,
+        p2   = p + sep + p,
+        p3   = p + '(' + sep + p + '){2}',
+        pseq = p + '(' + sep + p + ')*',
+        arc  = pos + sep + pos + sep + num + csp + '[01]' + sep + '[01]' + sep + p,
+        m  = '[Mm]'   + wsp + pseq,
+        lt = '[LlTt]' + wsp + pseq,
+        hv = '[HhVv]' + wsp + nseq,
+        c  = '[Cc]'   + wsp + p3 + '(' + sep + p3 + ')*',
+        qs = '[QqSs]' + wsp + p2 + '(' + sep + p2 + ')*',
+        a  = '[Aa]'   + wsp + arc + '(' + sep + arc + ')*',
+        z  = '[Zz]',
+        draw = '(' + [z, lt, hv, c, qs, a].join('|') + ')',
+        move = m + wsp + '(' + draw + '(' + sep + draw + ')*' + ')?',
+        path = wsp + '(' + move + '(' + sep + move + ')*' + ')?' + wsp;
   return path;
 }
 
+const COLOR_NAMES = 'aliceblue|antiquewhite|aqua|aquamarine|azure|beige|bisque|black|blanchedalmond|blue|blueviolet|brown|burlywood|cadetblue|chartreuse|chocolate|coral|cornflowerblue|cornsilk|crimson|cyan|darkblue|darkcyan|darkgoldenrod|darkgray|darkgreen|darkkhaki|darkmagenta|darkolivegreen|darkorange|darkorchid|darkred|darksalmon|darkseagreen|darkslateblue|darkslategray|darkturquoise|darkviolet|deeppink|deepskyblue|dimgray|dodgerblue|firebrick|floralwhite|forestgreen|fuchsia|gainsboro|ghostwhite|gold|goldenrod|gray|green|greenyellow|honeydew|hotpink|indianred|indigo|ivory|khaki|lavender|lavenderblush|lawngreen|lemonchiffon|lightblue|lightcoral|lightcyan|lightgoldenrodyellow|lightgray|lightgreen|lightpink|lightsalmon|lightseagreen|lightskyblue|lightslategray|lightsteelblue|lightyellow|lime|limegreen|linen|magenta|maroon|mediumaquamarine|mediumblue|mediumorchid|mediumpurple|mediumseagreen|mediumslateblue|mediumspringgreen|mediumturquoise|mediumvioletred|midnightblue|mintcream|mistyrose|moccasin|navajowhite|navy|oldlace|olive|olivedrab|orange|orangered|orchid|palegoldenrod|palegreen|paleturquoise|palevioletred|papayawhip|peachpuff|peru|pink|plum|powderblue|purple|rebeccapurple|red|rosybrown|royalblue|saddlebrown|salmon|sandybrown|seagreen|seashell|sienna|silver|skyblue|slateblue|slategray|snow|springgreen|steelblue|tan|teal|thistle|tomato|turquoise|violet|wheat|white|whitesmoke|yellow|yellowgreen';
+
 function css_color_names() {
-  return COLOR_NAMES.split('|').map(function(name) {
-    return name.split('').map(function(c) {
-      return '[' + c.toUpperCase() + c.toLowerCase() + ']';
-    }).join('');
-  }).join('|');
+  return COLOR_NAMES.split('|').map(
+    name => name.split('')
+      .map(c => `[${c.toUpperCase()}${c.toLowerCase()}]`)
+      .join('')
+  ).join('|');
 }
 
-var COLOR_NAMES = 'aliceblue|antiquewhite|aqua|aquamarine|azure|beige|bisque|black|blanchedalmond|blue|blueviolet|brown|burlywood|cadetblue|chartreuse|chocolate|coral|cornflowerblue|cornsilk|crimson|cyan|darkblue|darkcyan|darkgoldenrod|darkgray|darkgreen|darkkhaki|darkmagenta|darkolivegreen|darkorange|darkorchid|darkred|darksalmon|darkseagreen|darkslateblue|darkslategray|darkturquoise|darkviolet|deeppink|deepskyblue|dimgray|dodgerblue|firebrick|floralwhite|forestgreen|fuchsia|gainsboro|ghostwhite|gold|goldenrod|gray|green|greenyellow|honeydew|hotpink|indianred|indigo|ivory|khaki|lavender|lavenderblush|lawngreen|lemonchiffon|lightblue|lightcoral|lightcyan|lightgoldenrodyellow|lightgray|lightgreen|lightpink|lightsalmon|lightseagreen|lightskyblue|lightslategray|lightsteelblue|lightyellow|lime|limegreen|linen|magenta|maroon|mediumaquamarine|mediumblue|mediumorchid|mediumpurple|mediumseagreen|mediumslateblue|mediumspringgreen|mediumturquoise|mediumvioletred|midnightblue|mintcream|mistyrose|moccasin|navajowhite|navy|oldlace|olive|olivedrab|orange|orangered|orchid|palegoldenrod|palegreen|paleturquoise|palevioletred|papayawhip|peachpuff|peru|pink|plum|powderblue|purple|rebeccapurple|red|rosybrown|royalblue|saddlebrown|salmon|sandybrown|seagreen|seashell|sienna|silver|skyblue|slateblue|slategray|snow|springgreen|steelblue|tan|teal|thistle|tomato|turquoise|violet|wheat|white|whitesmoke|yellow|yellowgreen';
-
-var BASE = {
+const BASE = {
   "$schema": "http://json-schema.org/draft-06/schema#",
   "title": "Vega scenegraph",
   "description": "Vega scenegraph model.",
@@ -158,7 +158,7 @@ var BASE = {
   }
 };
 
-var MARK_BASE = {
+const MARK_BASE = {
   "type": "object",
   "properties": {
     "marktype": { "enum": null },
@@ -174,7 +174,7 @@ var MARK_BASE = {
   "required": [ "marktype" ]
 };
 
-var BLEND_MODES = [
+const BLEND_MODE = [
   null,
   'multiply',
   'screen',
@@ -193,14 +193,14 @@ var BLEND_MODES = [
   'luminosity',
 ];
 
-var ITEM_BASE = {
+const ITEM_BASE = {
   "type": "object",
   "properties": {
     "x": { "type": "number" },
     "y": { "type": "number" },
     "width": { "type": "number" },
     "height": { "type": "number" },
-    "blend": { "enum": BLEND_MODES, "default": null },
+    "blend": { "enum": BLEND_MODE, "default": null },
     "opacity": { "type": "number", "default": 1 },
     "fill": { "$ref": "#/refs/paint" },
     "fillOpacity": { "type": "number", "default": 1 },
@@ -219,7 +219,7 @@ var ITEM_BASE = {
   }
 };
 
-var AREA_INTERPOLATE = [
+const AREA_INTERPOLATE = [
   'basis',
   'cardinal',
   'catmull-rom',
@@ -231,7 +231,7 @@ var AREA_INTERPOLATE = [
   'step-before'
 ];
 
-var LINE_INTERPOLATE = [
+const LINE_INTERPOLATE = [
   'basis',
   'basis-closed',
   'basis-open',
@@ -251,7 +251,32 @@ var LINE_INTERPOLATE = [
   'step-before'
 ];
 
-var MARKS = {
+const TEXT_BASELINE = [
+  'alphabetic',
+  'top',
+  'middle',
+  'bottom',
+  'line-top',
+  'line-bottom'
+];
+
+const TEXT_FONT_WEIGHT = [
+  'normal',
+  'bold',
+  'bolder',
+  'lighter',
+  100,
+  200,
+  300,
+  400,
+  500,
+  600,
+  700,
+  800,
+  900
+];
+
+const MARKS = {
   "group": {
     "properties": {
       "clip": { "type": "boolean" },
@@ -354,7 +379,7 @@ var MARKS = {
         "default": "left"
       },
       "baseline": {
-        "enum": [ "top", "middle", "bottom", "alphabetic" ],
+        "enum": TEXT_BASELINE,
         "default": "alphabetic"
       },
       "angle": { "type": "number", "default": 0 },
@@ -369,10 +394,7 @@ var MARKS = {
         "default": "normal"
       },
       "fontWeight": {
-        "enum": [
-          "normal", "bold", "bolder", "lighter",
-          100, 200, 300, 400, 500, 600, 700, 800, 900
-        ],
+        "enum": TEXT_FONT_WEIGHT,
         "default": "normal"
       },
       "fontVariant": {
@@ -389,5 +411,5 @@ var MARKS = {
   }
 };
 
-var schema = build(true);
+const schema = build(true);
 process.stdout.write(JSON.stringify(schema, null, 2));
