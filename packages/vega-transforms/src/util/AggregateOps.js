fd52dcd3345982524556140bc304da8d8573cc6d
Jeffrey Heer <jheer@trifacta.com>
2020-05-09 00:04:55 +0200

    refactor(vega-transforms): Remove Function constructor usage.

--- packages/vega-transforms/src/util/AggregateOps.js
+++ packages/vega-transforms/src/util/AggregateOps.js
@@ -1,211 +1,214 @@
-import {extend, identity, stringValue} from 'vega-util';
+import {extend, identity} from 'vega-util';
 
 export function measureName(op, field, as) {
   return as || (op + (!field ? '' : '_' + field));
 }
 
-export var AggregateOps = {
-  'values': measure({
-    name: 'values',
-    init: 'cell.store = true;',
-    set:  'cell.data.values()', idx: -1
-  }),
-  'count': measure({
-    name: 'count',
-    set:  'cell.num'
-  }),
-  '__count__': measure({
-    name: 'count',
-    set:  'this.missing + this.valid'
-  }),
-  'missing': measure({
-    name: 'missing',
-    set:  'this.missing'
-  }),
-  'valid': measure({
-    name: 'valid',
-    set:  'this.valid'
-  }),
-  'sum': measure({
-    name: 'sum',
-    init: 'this.sum = 0;',
-    add:  'this.sum += +v;',
-    rem:  'this.sum -= v;',
-    set:  'this.sum'
-  }),
-  'product': measure({
-    name: 'product',
-    init: 'this.product = 1;',
-    add:  'this.product *= v;',
-    rem:  'this.product /= v;',
-    set:  'this.valid ? this.product : undefined'
-  }),
-  'mean': measure({
-    name: 'mean',
-    init: 'this.mean = 0;',
-    add:  'var d = v - this.mean; this.mean += d / this.valid;',
-    rem:  'var d = v - this.mean; this.mean -= this.valid ? d / this.valid : this.mean;',
-    set:  'this.valid ? this.mean : undefined'
-  }),
-  'average': measure({
-    name: 'average',
-    set:  'this.valid ? this.mean : undefined',
-    req:  ['mean'], idx: 1
-  }),
-  'variance': measure({
-    name: 'variance',
-    init: 'this.dev = 0;',
-    add:  'this.dev += d * (v - this.mean);',
-    rem:  'this.dev -= d * (v - this.mean);',
-    set:  'this.valid > 1 ? this.dev / (this.valid-1) : undefined',
-    req:  ['mean'], idx: 1
-  }),
-  'variancep': measure({
-    name: 'variancep',
-    set:  'this.valid > 1 ? this.dev / this.valid : undefined',
-    req:  ['variance'], idx: 2
-  }),
-  'stdev': measure({
-    name: 'stdev',
-    set:  'this.valid > 1 ? Math.sqrt(this.dev / (this.valid-1)) : undefined',
-    req:  ['variance'], idx: 2
-  }),
-  'stdevp': measure({
-    name: 'stdevp',
-    set:  'this.valid > 1 ? Math.sqrt(this.dev / this.valid) : undefined',
-    req:  ['variance'], idx: 2
-  }),
-  'stderr': measure({
-    name: 'stderr',
-    set:  'this.valid > 1 ? Math.sqrt(this.dev / (this.valid * (this.valid-1))) : undefined',
-    req:  ['variance'], idx: 2
-  }),
-  'distinct': measure({
-    name: 'distinct',
-    set:  'cell.data.distinct(this.get)',
-    req:  ['values'], idx: 3
-  }),
-  'ci0': measure({
-    name: 'ci0',
-    set:  'cell.data.ci0(this.get)',
-    req:  ['values'], idx: 3
-  }),
-  'ci1': measure({
-    name: 'ci1',
-    set:  'cell.data.ci1(this.get)',
-    req:  ['values'], idx: 3
-  }),
-  'median': measure({
-    name: 'median',
-    set:  'cell.data.q2(this.get)',
-    req:  ['values'], idx: 3
-  }),
-  'q1': measure({
-    name: 'q1',
-    set:  'cell.data.q1(this.get)',
-    req:  ['values'], idx: 3
-  }),
-  'q3': measure({
-    name: 'q3',
-    set:  'cell.data.q3(this.get)',
-    req:  ['values'], idx: 3
-  }),
-  'argmin': measure({
-    name: 'argmin',
-    init: 'this.argmin = undefined;',
-    add:  'if (v < this.min) this.argmin = t;',
-    rem:  'if (v <= this.min) this.argmin = undefined;',
-    set:  'this.argmin || cell.data.argmin(this.get)',
-    req:  ['min'], str: ['values'], idx: 3
-  }),
-  'argmax': measure({
-    name: 'argmax',
-    init: 'this.argmax = undefined;',
-    add:  'if (v > this.max) this.argmax = t;',
-    rem:  'if (v >= this.max) this.argmax = undefined;',
-    set:  'this.argmax || cell.data.argmax(this.get)',
-    req:  ['max'], str: ['values'], idx: 3
-  }),
-  'min': measure({
-    name: 'min',
-    init: 'this.min = undefined;',
-    add:  'if (v < this.min || this.min === undefined) this.min = v;',
-    rem:  'if (v <= this.min) this.min = NaN;',
-    set:  'this.min = (Number.isNaN(this.min) ? cell.data.min(this.get) : this.min)',
-    str:  ['values'], idx: 4
-  }),
-  'max': measure({
-    name: 'max',
-    init: 'this.max = undefined;',
-    add:  'if (v > this.max || this.max === undefined) this.max = v;',
-    rem:  'if (v >= this.max) this.max = NaN;',
-    set:  'this.max = (Number.isNaN(this.max) ? cell.data.max(this.get) : this.max)',
-    str:  ['values'], idx: 4
-  })
+const noop = () => {};
+
+const base_op = {
+  init: noop,
+  add:  noop,
+  rem:  noop,
+  idx:  0,
 };
 
-export var ValidAggregateOps = Object.keys(AggregateOps);
+export const AggregateOps = {
+  values: {
+    init:  m => m.cell.store = true,
+    value: m => m.cell.data.values(),
+    idx:  -1,
+  },
+  count: {
+    value: m => m.cell.num,
+  },
+  __count__: {
+    value: m => m.missing + m.valid,
+  },
+  missing: {
+    value: m => m.missing,
+  },
+  valid: {
+    value: m => m.valid,
+  },
+  sum: {
+    init:  m => m.sum = 0,
+    value: m => m.sum,
+    add:  (m, v) => m.sum += v,
+    rem:  (m, v) => m.sum -= v,
+  },
+  product: {
+    init:  m => m.product = 1,
+    value: m => m.valid ? m.product : undefined,
+    add:  (m, v) => m.product *= v,
+    rem:  (m, v) => m.product /= v,
+  },
+  mean: {
+    init:  m => m.mean = 0,
+    value: m => m.valid ? m.mean : undefined,
+    add:  (m, v) => (m.mean_d = v - m.mean, m.mean += m.mean_d / m.valid),
+    rem:  (m, v) => (m.mean_d = v - m.mean, m.mean -= m.valid ? m.mean_d / m.valid : m.mean),
+  },
+  average: {
+    value: m => m.valid ? m.mean : undefined,
+    req:  ['mean'], idx: 1,
+  },
+  variance: {
+    init:  m => m.dev = 0,
+    value: m => m.valid > 1 ? m.dev / (m.valid - 1) : undefined,
+    add:  (m, v) => m.dev += m.mean_d * (v - m.mean),
+    rem:  (m, v) => m.dev -= m.mean_d * (v - m.mean),
+    req:  ['mean'], idx: 1,
+  },
+  variancep: {
+    value: m => m.valid > 1 ? m.dev / m.valid : undefined,
+    req:  ['variance'], idx: 2,
+  },
+  stdev: {
+    value: m => m.valid > 1 ? Math.sqrt(m.dev / (m.valid - 1)) : undefined,
+    req:  ['variance'], idx: 2,
+  },
+  stdevp: {
+    value: m => m.valid > 1 ? Math.sqrt(m.dev / m.valid) : undefined,
+    req:  ['variance'], idx: 2,
+  },
+  stderr: {
+    value: m => m.valid > 1 ? Math.sqrt(m.dev / (m.valid * (m.valid - 1))) : undefined,
+    req:  ['variance'], idx: 2,
+  },
+  distinct: {
+    value: m => m.cell.data.distinct(m.get),
+    req:  ['values'], idx: 3,
+  },
+  ci0: {
+    value: m => m.cell.data.ci0(m.get),
+    req:  ['values'], idx: 3,
+  },
+  ci1: {
+    value: m => m.cell.data.ci1(m.get),
+    req:  ['values'], idx: 3,
+  },
+  median: {
+    value: m => m.cell.data.q2(m.get),
+    req:  ['values'], idx: 3,
+  },
+  q1: {
+    value: m => m.cell.data.q1(m.get),
+    req: ['values'], idx: 3,
+  },
+  q3: {
+    value: m => m.cell.data.q3(m.get),
+    req:  ['values'], idx: 3,
+  },
+  min: {
+    init:  m => m.min = undefined,
+    value: m => m.min = (Number.isNaN(m.min) ? m.cell.data.min(m.get) : m.min),
+    add:  (m, v) => { if (v < m.min || m.min === undefined) m.min = v; },
+    rem:  (m, v) => { if (v <= m.min) m.min = NaN; },
+    req:  ['values'], idx: 4,
+  },
+  max: {
+    init:  m => m.max = undefined,
+    value: m => m.max = (Number.isNaN(m.max) ? m.cell.data.max(m.get) : m.max),
+    add:  (m, v) => { if (v > m.max || m.max === undefined) m.max = v; },
+    rem:  (m, v) => { if (v >= m.max) m.max = NaN; },
+    req:  ['values'], idx: 4,
+  },
+  argmin: {
+    init:  m => m.argmin = undefined,
+    value: m => m.argmin || m.cell.data.argmin(m.get),
+    add:  (m, v, t) => { if (v < m.min) m.argmin = t; },
+    rem:  (m, v) => { if (v <= m.min) m.argmin = undefined; },
+    req:  ['min', 'values'], idx: 3,
+  },
+  argmax: {
+    init:  m => m.argmax = undefined,
+    value: m => m.argmax || m.cell.data.argmax(m.get),
+    add:  (m, v, t) => { if (v > m.max) m.argmax = t; },
+    rem:  (m, v) => { if (v >= m.max) m.argmax = undefined; },
+    req:  ['max', 'values'], idx: 3,
+  }
+};
+
+export const ValidAggregateOps = Object.keys(AggregateOps);
+
+function measure(key, value) {
+  return out => extend({
+    name: key,
+    out: out || key
+  }, base_op, value);
+}
+
+ValidAggregateOps.forEach(key => {
+  AggregateOps[key] = measure(key, AggregateOps[key]);
+});
 
 export function createMeasure(op, name) {
   return AggregateOps[op](name);
-}
-
-function measure(base) {
-  return function(out) {
-    var m = extend({init:'', add:'', rem:'', idx:0}, base);
-    m.out = out || base.name;
-    return m;
-  };
 }
 
 function compareIndex(a, b) {
   return a.idx - b.idx;
 }
 
-function resolve(agg, stream) {
-  function collect(m, a) {
-    function helper(r) { if (!m[r]) collect(m, m[r] = AggregateOps[r]()); }
-    if (a.req) a.req.forEach(helper);
-    if (stream && a.str) a.str.forEach(helper);
-    return m;
-  }
-  var map = agg.reduce(
-    collect,
-    agg.reduce(function(m, a) {
-      m[a.name] = a;
-      return m;
-    }, {})
-  );
-  var values = [], key;
-  for (key in map) values.push(map[key]);
-  return values.sort(compareIndex);
+function resolve(agg) {
+  const map = {};
+  agg.forEach(a => map[a.name] = a);
+
+  const getreqs = a => {
+    if (!a.req) return;
+    a.req.forEach(key => {
+      if (!map[key]) getreqs(map[key] = AggregateOps[key]());
+    });
+  };
+  agg.forEach(getreqs);
+
+  return Object.values(map).sort(compareIndex);
+}
+
+function init() {
+  this.valid = 0;
+  this.missing = 0;
+  this._ops.forEach(op => op.init(this));
+}
+
+function add(v, t) {
+  if (v == null) { ++this.missing; return; }
+  if (v !== v) return;
+  ++this.valid;
+  this._ops.forEach(op => op.add(this, v, t));
+}
+
+function rem(v, t) {
+  if (v == null) { --this.missing; return; }
+  if (v !== v) return;
+  --this.valid;
+  this._ops.forEach(op => op.rem(this, v, t));
+}
+
+function set(t) {
+  this._out.forEach(op => t[op.out] = op.value(this));
+  return t;
 }
 
 export function compileMeasures(agg, field) {
   var get = field || identity,
-      all = resolve(agg, true), // assume streaming removes may occur
-      init = 'var cell = this.cell; this.valid = 0; this.missing = 0;',
-      ctr = 'this.cell = cell; this.init();',
-      add = 'if(v==null){++this.missing; return;} if(v!==v) return; ++this.valid;',
-      rem = 'if(v==null){--this.missing; return;} if(v!==v) return; --this.valid;',
-      set = 'var cell = this.cell;';
+      ops = resolve(agg),
+      out = agg.slice().sort(compareIndex);
 
-  all.forEach(function(a) {
-    init += a.init;
-    add += a.add;
-    rem += a.rem;
-  });
-  agg.slice().sort(compareIndex).forEach(function(a) {
-    set += 't[' + stringValue(a.out) + ']=' + a.set + ';';
-  });
-  set += 'return t;';
+  function ctr(cell) {
+    this._ops = ops;
+    this._out = out;
+    this.cell = cell;
+    this.init();
+  }
 
-  ctr = Function('cell', ctr);
-  ctr.prototype.init = Function(init);
-  ctr.prototype.add = Function('v', 't', add);
-  ctr.prototype.rem = Function('v', 't', rem);
-  ctr.prototype.set = Function('t', set);
+  ctr.prototype.init = init;
+  ctr.prototype.add = add;
+  ctr.prototype.rem = rem;
+  ctr.prototype.set = set;
   ctr.prototype.get = get;
-  ctr.fields = agg.map(function(_) { return _.out; });
+  ctr.fields = agg.map(op => op.out);
+
   return ctr;
 }
