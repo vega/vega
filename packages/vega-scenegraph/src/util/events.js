28c5c83ad719b9ec750fc83aa7ca4d9ef3045e3f
Jeffrey Heer <jheer@trifacta.com>
2020-05-21 22:58:50 +0200

    fix: Add all necessary canvas event handlers.

--- packages/vega-scenegraph/src/util/events.js
+++ packages/vega-scenegraph/src/util/events.js
@@ -1,26 +1,45 @@
-export var Events = [
-  'keydown',
-  'keypress',
-  'keyup',
-  'dragenter',
-  'dragleave',
-  'dragover',
-  'mousedown',
-  'mouseup',
-  'mousemove',
-  'mouseout',
-  'mouseover',
-  'click',
-  'dblclick',
-  'wheel',
-  'mousewheel',
-  'touchstart',
-  'touchmove',
-  'touchend'
+export const KeyDownEvent = 'keydown';
+export const KeyPressEvent = 'keypress';
+export const KeyUpEvent = 'keyup';
+export const DragEnterEvent = 'dragenter';
+export const DragLeaveEvent = 'dragleave';
+export const DragOverEvent = 'dragover';
+export const MouseDownEvent = 'mousedown';
+export const MouseUpEvent = 'mouseup';
+export const MouseMoveEvent = 'mousemove';
+export const MouseOutEvent = 'mouseout';
+export const MouseOverEvent = 'mouseover';
+export const ClickEvent = 'click';
+export const DoubleClickEvent = 'dblclick';
+export const WheelEvent = 'wheel';
+export const MouseWheelEvent = 'mousewheel';
+export const TouchStartEvent = 'touchstart';
+export const TouchMoveEvent = 'touchmove';
+export const TouchEndEvent = 'touchend';
+
+export const Events = [
+  KeyDownEvent,
+  KeyPressEvent,
+  KeyUpEvent,
+  DragEnterEvent,
+  DragLeaveEvent,
+  DragOverEvent,
+  MouseDownEvent,
+  MouseUpEvent,
+  MouseMoveEvent,
+  MouseOutEvent,
+  MouseOverEvent,
+  ClickEvent,
+  DoubleClickEvent,
+  WheelEvent,
+  MouseWheelEvent,
+  TouchStartEvent,
+  TouchMoveEvent,
+  TouchEndEvent
 ];
 
-export var TooltipShowEvent = 'mousemove';
+export const TooltipShowEvent = MouseMoveEvent;
 
-export var TooltipHideEvent = 'mouseout';
+export const TooltipHideEvent = MouseOutEvent;
 
-export var HrefEvent = 'click';
+export const HrefEvent = ClickEvent;
