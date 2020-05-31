534cf4fc97019df785ed7269d8f7c86f41908fe9
Jeffrey Heer <jheer@trifacta.com>
2020-05-31 14:37:28 +0200

    refactor(vega-encode): Use leaner syntax.

--- packages/vega-encode/src/Encode.js
+++ packages/vega-encode/src/Encode.js
@@ -17,63 +17,63 @@ export default function Encode(params) {
   Transform.call(this, null, params);
 }
 
-var prototype = inherits(Encode, Transform);
+inherits(Encode, Transform, {
+  transform(_, pulse) {
+    var out = pulse.fork(pulse.ADD_REM),
+        fmod = _.mod || false,
+        encoders = _.encoders,
+        encode = pulse.encode;
 
-prototype.transform = function(_, pulse) {
-  var out = pulse.fork(pulse.ADD_REM),
-      fmod = _.mod || false,
-      encoders = _.encoders,
-      encode = pulse.encode;
-
-  // if an array, the encode directive includes additional sets
-  // that must be defined in order for the primary set to be invoked
-  // e.g., only run the update set if the hover set is defined
-  if (isArray(encode)) {
-    if (out.changed() || encode.every(function(e) { return encoders[e]; })) {
-      encode = encode[0];
-      out.encode = null; // consume targeted encode directive
-    } else {
-      return pulse.StopPropagation;
+    // if an array, the encode directive includes additional sets
+    // that must be defined in order for the primary set to be invoked
+    // e.g., only run the update set if the hover set is defined
+    if (isArray(encode)) {
+      if (out.changed() || encode.every(function(e) { return encoders[e]; })) {
+        encode = encode[0];
+        out.encode = null; // consume targeted encode directive
+      } else {
+        return pulse.StopPropagation;
+      }
     }
-  }
 
-  // marshall encoder functions
-  var reenter = encode === 'enter',
-      update = encoders.update || falsy,
-      enter = encoders.enter || falsy,
-      exit = encoders.exit || falsy,
-      set = (encode && !reenter ? encoders[encode] : update) || falsy;
+    // marshall encoder functions
+    var reenter = encode === 'enter',
+        update = encoders.update || falsy,
+        enter = encoders.enter || falsy,
+        exit = encoders.exit || falsy,
+        set = (encode && !reenter ? encoders[encode] : update) || falsy;
 
-  if (pulse.changed(pulse.ADD)) {
-    pulse.visit(pulse.ADD, function(t) { enter(t, _); update(t, _); });
-    out.modifies(enter.output);
-    out.modifies(update.output);
-    if (set !== falsy && set !== update) {
-      pulse.visit(pulse.ADD, function(t) { set(t, _); });
-      out.modifies(set.output);
+    if (pulse.changed(pulse.ADD)) {
+      pulse.visit(pulse.ADD, function(t) { enter(t, _); update(t, _); });
+      out.modifies(enter.output);
+      out.modifies(update.output);
+      if (set !== falsy && set !== update) {
+        pulse.visit(pulse.ADD, function(t) { set(t, _); });
+        out.modifies(set.output);
+      }
     }
-  }
 
-  if (pulse.changed(pulse.REM) && exit !== falsy) {
-    pulse.visit(pulse.REM, function(t) { exit(t, _); });
-    out.modifies(exit.output);
-  }
-
-  if (reenter || set !== falsy) {
-    var flag = pulse.MOD | (_.modified() ? pulse.REFLOW : 0);
-    if (reenter) {
-      pulse.visit(flag, function(t) {
-        var mod = enter(t, _) || fmod;
-        if (set(t, _) || mod) out.mod.push(t);
-      });
-      if (out.mod.length) out.modifies(enter.output);
-    } else {
-      pulse.visit(flag, function(t) {
-        if (set(t, _) || fmod) out.mod.push(t);
-      });
+    if (pulse.changed(pulse.REM) && exit !== falsy) {
+      pulse.visit(pulse.REM, function(t) { exit(t, _); });
+      out.modifies(exit.output);
     }
-    if (out.mod.length) out.modifies(set.output);
-  }
 
-  return out.changed() ? out : pulse.StopPropagation;
-};
+    if (reenter || set !== falsy) {
+      var flag = pulse.MOD | (_.modified() ? pulse.REFLOW : 0);
+      if (reenter) {
+        pulse.visit(flag, function(t) {
+          var mod = enter(t, _) || fmod;
+          if (set(t, _) || mod) out.mod.push(t);
+        });
+        if (out.mod.length) out.modifies(enter.output);
+      } else {
+        pulse.visit(flag, function(t) {
+          if (set(t, _) || fmod) out.mod.push(t);
+        });
+      }
+      if (out.mod.length) out.modifies(set.output);
+    }
+
+    return out.changed() ? out : pulse.StopPropagation;
+  }
+});
