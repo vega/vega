c4f99fc62fd697052901fd5aee9bba0edccb6493
Jeffrey Heer <jheer@trifacta.com>
2020-05-31 14:41:25 +0200

    refactor(vega-wordcloud): Use leaner syntax.

--- packages/vega-wordcloud/src/Wordcloud.js
+++ packages/vega-wordcloud/src/Wordcloud.js
@@ -30,79 +30,78 @@ Wordcloud.Definition = {
   ]
 };
 
-var prototype = inherits(Wordcloud, Transform);
+inherits(Wordcloud, Transform, {
+  transform(_, pulse) {
+    if (_.size && !(_.size[0] && _.size[1])) {
+      error('Wordcloud size dimensions must be non-zero.');
+    }
 
-prototype.transform = function(_, pulse) {
-  if (_.size && !(_.size[0] && _.size[1])) {
-    error('Wordcloud size dimensions must be non-zero.');
+    function modp(param) {
+      var p = _[param];
+      return isFunction(p) && pulse.modified(p.fields);
+    }
+
+    const mod = _.modified();
+    if (!(mod || pulse.changed(pulse.ADD_REM) || Params.some(modp))) return;
+
+    const data = pulse.materialize(pulse.SOURCE).source,
+          layout = this.value,
+          as = _.as || Output;
+
+    let fontSize = _.fontSize || 14,
+        range;
+
+    isFunction(fontSize)
+      ? (range = _.fontSizeRange)
+      : (fontSize = constant(fontSize));
+
+    // create font size scaling function as needed
+    if (range) {
+      const fsize = fontSize,
+            sizeScale = scale('sqrt')()
+              .domain(extent(data, fsize))
+              .range(range);
+      fontSize = x => sizeScale(fsize(x));
+    }
+
+    data.forEach(t => {
+      t[as[0]] = NaN;
+      t[as[1]] = NaN;
+      t[as[3]] = 0;
+    });
+
+    // configure layout
+    const words = layout
+      .words(data)
+      .text(_.text)
+      .size(_.size || [500, 500])
+      .padding(_.padding || 1)
+      .spiral(_.spiral || 'archimedean')
+      .rotate(_.rotate || 0)
+      .font(_.font || 'sans-serif')
+      .fontStyle(_.fontStyle || 'normal')
+      .fontWeight(_.fontWeight || 'normal')
+      .fontSize(fontSize)
+      .random(random)
+      .layout();
+
+    const size = layout.size(),
+        dx = size[0] >> 1,
+        dy = size[1] >> 1,
+        n = words.length;
+
+    for (let i = 0, w, t; i<n; ++i) {
+      w = words[i];
+      t = w.datum;
+      t[as[0]] = w.x + dx;
+      t[as[1]] = w.y + dy;
+      t[as[2]] = w.font;
+      t[as[3]] = w.size;
+      t[as[4]] = w.style;
+      t[as[5]] = w.weight;
+      t[as[6]] = w.rotate;
+    }
+
+    return pulse.reflow(mod).modifies(as);
   }
-
-  function modp(param) {
-    var p = _[param];
-    return isFunction(p) && pulse.modified(p.fields);
-  }
-
-  var mod = _.modified();
-  if (!(mod || pulse.changed(pulse.ADD_REM) || Params.some(modp))) return;
-
-  var data = pulse.materialize(pulse.SOURCE).source,
-      layout = this.value,
-      as = _.as || Output,
-      fontSize = _.fontSize || 14,
-      range;
-
-  isFunction(fontSize)
-    ? (range = _.fontSizeRange)
-    : (fontSize = constant(fontSize));
-
-  // create font size scaling function as needed
-  if (range) {
-    var fsize = fontSize,
-        sizeScale = scale('sqrt')()
-          .domain(extent(data, fsize))
-          .range(range);
-    fontSize = function(x) { return sizeScale(fsize(x)); };
-  }
-
-  data.forEach(function(t) {
-    t[as[0]] = NaN;
-    t[as[1]] = NaN;
-    t[as[3]] = 0;
-  });
-
-  // configure layout
-  var words = layout
-    .words(data)
-    .text(_.text)
-    .size(_.size || [500, 500])
-    .padding(_.padding || 1)
-    .spiral(_.spiral || 'archimedean')
-    .rotate(_.rotate || 0)
-    .font(_.font || 'sans-serif')
-    .fontStyle(_.fontStyle || 'normal')
-    .fontWeight(_.fontWeight || 'normal')
-    .fontSize(fontSize)
-    .random(random)
-    .layout();
-
-  var size = layout.size(),
-      dx = size[0] >> 1,
-      dy = size[1] >> 1,
-      i = 0,
-      n = words.length,
-      w, t;
-
-  for (; i<n; ++i) {
-    w = words[i];
-    t = w.datum;
-    t[as[0]] = w.x + dx;
-    t[as[1]] = w.y + dy;
-    t[as[2]] = w.font;
-    t[as[3]] = w.size;
-    t[as[4]] = w.style;
-    t[as[5]] = w.weight;
-    t[as[6]] = w.rotate;
-  }
-
-  return pulse.reflow(mod).modifies(as);
-};
+});
