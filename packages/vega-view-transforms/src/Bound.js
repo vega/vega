b94ba2e49f77ab78718a6a5ace7cdcd9e9c1bebd
Jeffrey Heer <jheer@trifacta.com>
2020-05-31 14:40:45 +0200

    refactor(vega-view-transforms): Use leaner syntax.

--- packages/vega-view-transforms/src/Bound.js
+++ packages/vega-view-transforms/src/Bound.js
@@ -13,66 +13,67 @@ export default function Bound(params) {
   Transform.call(this, null, params);
 }
 
-var prototype = inherits(Bound, Transform);
+inherits(Bound, Transform, {
+  transform(_, pulse) {
+    const view = pulse.dataflow,
+          mark = _.mark,
+          type = mark.marktype,
+          entry = Marks[type],
+          bound = entry.bound;
 
-prototype.transform = function(_, pulse) {
-  var view = pulse.dataflow,
-      mark = _.mark,
-      type = mark.marktype,
-      entry = Marks[type],
-      bound = entry.bound,
-      markBounds = mark.bounds, rebound;
+    let markBounds = mark.bounds, rebound;
 
-  if (entry.nested) {
-    // multi-item marks have a single bounds instance
-    if (mark.items.length) view.dirty(mark.items[0]);
-    markBounds = boundItem(mark, bound);
-    mark.items.forEach(function(item) {
-      item.bounds.clear().union(markBounds);
-    });
-  }
-
-  else if (type === Group || _.modified()) {
-    // operator parameters modified -> re-bound all items
-    // updates group bounds in response to modified group content
-    pulse.visit(pulse.MOD, item => view.dirty(item));
-    markBounds.clear();
-    mark.items.forEach(item => markBounds.union(boundItem(item, bound)));
-
-    // force reflow for axes/legends/titles to propagate any layout changes
-    switch (mark.role) {
-      case AxisRole:
-      case LegendRole:
-      case TitleRole:
-        pulse.reflow();
+    if (entry.nested) {
+      // multi-item marks have a single bounds instance
+      if (mark.items.length) view.dirty(mark.items[0]);
+      markBounds = boundItem(mark, bound);
+      mark.items.forEach(function(item) {
+        item.bounds.clear().union(markBounds);
+      });
     }
-  }
 
-  else {
-    // incrementally update bounds, re-bound mark as needed
-    rebound = pulse.changed(pulse.REM);
-
-    pulse.visit(pulse.ADD, item => {
-      markBounds.union(boundItem(item, bound));
-    });
-
-    pulse.visit(pulse.MOD, item => {
-      rebound = rebound || markBounds.alignsWith(item.bounds);
-      view.dirty(item);
-      markBounds.union(boundItem(item, bound));
-    });
-
-    if (rebound) {
+    else if (type === Group || _.modified()) {
+      // operator parameters modified -> re-bound all items
+      // updates group bounds in response to modified group content
+      pulse.visit(pulse.MOD, item => view.dirty(item));
       markBounds.clear();
-      mark.items.forEach(item => markBounds.union(item.bounds));
+      mark.items.forEach(item => markBounds.union(boundItem(item, bound)));
+
+      // force reflow for axes/legends/titles to propagate any layout changes
+      switch (mark.role) {
+        case AxisRole:
+        case LegendRole:
+        case TitleRole:
+          pulse.reflow();
+      }
     }
+
+    else {
+      // incrementally update bounds, re-bound mark as needed
+      rebound = pulse.changed(pulse.REM);
+
+      pulse.visit(pulse.ADD, item => {
+        markBounds.union(boundItem(item, bound));
+      });
+
+      pulse.visit(pulse.MOD, item => {
+        rebound = rebound || markBounds.alignsWith(item.bounds);
+        view.dirty(item);
+        markBounds.union(boundItem(item, bound));
+      });
+
+      if (rebound) {
+        markBounds.clear();
+        mark.items.forEach(item => markBounds.union(item.bounds));
+      }
+    }
+
+    // ensure mark bounds do not exceed any clipping region
+    boundClip(mark);
+
+    return pulse.modifies('bounds');
   }
-
-  // ensure mark bounds do not exceed any clipping region
-  boundClip(mark);
-
-  return pulse.modifies('bounds');
-};
+});
 
 function boundItem(item, bound, opt) {
   return bound(item.bounds.clear(), item, opt);
