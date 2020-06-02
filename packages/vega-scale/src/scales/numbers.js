71fc6abe56f72d900dc1502b811e491a2c006356
Jeffrey Heer <jheer@trifacta.com>
2020-06-02 13:55:17 +0200

    refactor(vega-scale): Use leaner syntax.

--- packages/vega-scale/src/scales/numbers.js
+++ packages/vega-scale/src/scales/numbers.js
@@ -1,5 +1,7 @@
+import {toNumber} from 'vega-util';
+
 var map = Array.prototype.map;
 
 export function numbers(_) {
-  return map.call(_, function(x) { return +x; });
+  return map.call(_, toNumber);
 }
