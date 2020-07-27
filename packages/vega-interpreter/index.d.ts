d2b9bd904227add639e11133ebcba8f90fc93849
Dominik Moritz <domoritz@gmail.com>
2020-07-27 12:11:13 -0700

    feat: add types for vega interpreter (#2712)

---
+++ packages/vega-interpreter/index.d.ts
@@ -0,0 +1,7 @@
+export namespace expressionInterpreter {
+  export function operator(ctx: any, expr: any): any;
+  export function parameter(ctx: any, expr: any): any;
+  export function event(ctx: any, expr: any): any;
+  export function handler(ctx: any, expr: any): any;
+  export function encode(ctx: any, encode: any): any;
+}
