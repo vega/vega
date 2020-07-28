076400d46a2859487f9fef55495eee540a0109df
Zwyx <29386932+Zwyx@users.noreply.github.com>
2020-07-29 06:58:21 +0800

    feat: add types for Vega-Expression package (#2742)
    Co-authored-by: Zwyx <zwyx@users.noreply.github.com>

--- packages/vega-expression/index.d.ts
+++ packages/vega-expression/index.d.ts
@@ -1,21 +1,45 @@
-// TODO: add missing types
+/** Parse a JavaScript *expression* string and return the resulting abstract syntax tree in the ESTree format */
+export function parse(expression: string): object;
 
-export function parse(expression: string): any;
+interface CodegenOptions {
+  /** A hash of allowed top-level constant values */
+  constants?: { [cn: string]: string };
 
-export function codegen(params: {
-  constants?: object;
-  functions?: { [fn: string]: Function };
+  /** A function that is given an AST visitor instance as input and returns an object of allowed functions */
+  functions?: (astVisitor) => { [fn: string]: string | ((args) => string) };
+
+  /** An array of variable names that may not be referenced within the expression scope */
   blacklist?: string[];
+
+  /** An array of variable names that may be referenced within the expression scope */
   whitelist?: string[];
+
+  /** The name of the primary data input argument within the generated expression function */
   fieldvar?: string;
-  globalvar: string;
-}): (
-  ast: any
-) => {
-  /** The generated code as a string. */
+
+  /** The name of the variable upon which to lookup global variables */
+  globalvar: string | ((id: string) => string);
+}
+
+/** Create a new output code generator configured according to the provided options */
+export function codegen(options: CodegenOptions): (ast) => {
+  /** The generated code as a string */
   code: string;
-  /** A hash of all properties referenced within the fieldvar scope. */
+
+  /** A hash of all properties referenced within the _fieldvar_ scope */
   fields: string[];
+
   /** A hash of all properties referenced outside a provided whitelist */
   globals: string[];
 };
+
+/** An object defining default constant values for the Vega expression language */
+export const constants: { [cn: string]: string };
+
+/** Given a *codegen* instance as input, returns an object defining all valid function names for use within an expression */
+export function functions(codegen): {
+  [fn: string]: string | (() => string);
+};
+
+/** Constructor for a node in an expression abstract syntax tree (AST) */
+export function ASTNode(type: string): void;
