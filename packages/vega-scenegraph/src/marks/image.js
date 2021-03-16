b0366c9cf07a5e38a03c9758647dca49a71806f9
jheer <jeffrey.heer@gmail.com>
2021-03-16 15:49:28 +0100

    fix: Early exit on empty images.

--- packages/vega-scenegraph/src/marks/image.js
+++ packages/vega-scenegraph/src/marks/image.js
@@ -69,9 +69,12 @@ function draw(context, scene, bounds) {
     if (bounds && !bounds.intersects(item.bounds)) return; // bounds check
 
     const img = getImage(item, this);
-    let w = imageWidth(item, img),
-        h = imageHeight(item, img),
-        x = (item.x || 0) - imageXOffset(item.align, w),
+
+    let w = imageWidth(item, img);
+    let h = imageHeight(item, img);
+    if (w === 0 || h === 0) return; // early exit
+
+    let x = (item.x || 0) - imageXOffset(item.align, w),
         y = (item.y || 0) - imageYOffset(item.baseline, h),
         opacity, ar0, ar1, t;
 
