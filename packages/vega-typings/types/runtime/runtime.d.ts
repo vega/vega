d929be3307dddb715770452535c2b3723cb70c28
Saul Shanabrook <s.shanabrook@gmail.com>
2021-06-10 14:51:20 -0400

    fix tslint errors in runtime

--- packages/vega-typings/types/runtime/runtime.d.ts
+++ packages/vega-typings/types/runtime/runtime.d.ts
@@ -1,7 +1,7 @@
 import { AggregateOp, EventType, WindowEventType, Transforms } from '..';
 // All references to source code are from the vega-parser package
 
-export type Runtime = {
+export interface Runtime {
   description: string;
   operators: Entry[];
   streams: Stream[];
@@ -9,13 +9,13 @@ export type Runtime = {
   bindings: any;
   eventConfig: any;
   locale?: any;
-};
+}
 
 // These are called entries instead of operators because the JS class
 // is also called Entry, defined in util.js:entry
-type Entry = OperatorEntry | TransformEntry | DataTransformEntry;
+export type Entry = OperatorEntry | TransformEntry | DataTransformEntry;
 
-type OperatorEntry = EntryType<
+export type OperatorEntry = EntryType<
   'operator',
   | {
       // Adding root property in Scope.finish()
@@ -44,7 +44,7 @@ type OperatorEntry = EntryType<
 // The parameters for all builtin vega data transforms
 // from the parsers/transform.js function
 // Possible todo: create per transform type definitions based on transform definitions
-type DataTransformEntry = EntryType<
+export type DataTransformEntry = EntryType<
   // TODO: Do we allow external transforms as well?
   // The current vega spec definition does not.
   Transforms['type'],
@@ -59,7 +59,7 @@ type DataTransformEntry = EntryType<
 >;
 
 // // parsers.transform.js:parseParameter
-type TransformParam =
+export type TransformParam =
   // parseIndexParameter
   | Ref
   // parseSubParameters
@@ -69,7 +69,7 @@ type TransformParam =
   | unknown;
 
 // All entries defined in transforms.js
-type TransformEntry =
+export type TransformEntry =
   | AggregateEntry
   | AxisTicksEntry
   | BoundEntry
@@ -100,7 +100,7 @@ type TransformEntry =
   | ViewLayoutEntry
   | ValuesEntry;
 
-type AggregateEntry = EntryType<
+export type AggregateEntry = EntryType<
   'aggregate',
   {
     params: // DataScope.countsRef
@@ -134,36 +134,36 @@ type AggregateEntry = EntryType<
         };
   }
 >;
-type AxisTicksEntry = EntryType<'axisticks', { [k: string]: unknown }>;
-type BoundEntry = EntryType<'bound', { [k: string]: unknown }>;
-type CollectEntry = EntryType<'collect', { [k: string]: unknown }>;
-type CompareEntry = EntryType<'compare', { [k: string]: unknown }>;
-type DataJoinEntry = EntryType<'datajoin', { [k: string]: unknown }>;
-type EncodeEntry = EntryType<'encode', { [k: string]: unknown }>;
-type ExpressionEntry = EntryType<'expression', { [k: string]: unknown }>;
-type ExtentEntry = EntryType<'extent', { [k: string]: unknown }>;
-type FacetEntry = EntryType<'facet', { [k: string]: unknown }>;
-type FieldEntry = EntryType<'field', { [k: string]: unknown }>;
-type KeyEntry = EntryType<'key', { [k: string]: unknown }>;
-type LegendEntriesEntry = EntryType<'legendentries', { [k: string]: unknown }>;
-type LoadEntry = EntryType<'load', { [k: string]: unknown }>;
-type MarkEntry = EntryType<'mark', { [k: string]: unknown }>;
-type MultiextentEntry = EntryType<'multiextent', { [k: string]: unknown }>;
-type MultivaluesEntry = EntryType<'multivalues', { [k: string]: unknown }>;
-type OverlapEntry = EntryType<'overlap', { [k: string]: unknown }>;
-type ParamsEntry = EntryType<'params', { [k: string]: unknown }>;
-type PrefacetEntry = EntryType<'prefacet', { [k: string]: unknown }>;
-type ProjectionEntry = EntryType<'projection', { [k: string]: unknown }>;
-type ProxyEntry = EntryType<'proxy', { [k: string]: unknown }>;
-type RelayEntry = EntryType<'relay', { [k: string]: unknown }>;
-type RenderEntry = EntryType<'render', { [k: string]: unknown }>;
-type ScaleEntry = EntryType<'scale', { [k: string]: unknown }>;
-type SieveEntry = EntryType<'sieve', { [k: string]: unknown }>;
-type SortItemsEntry = EntryType<'sortitems', { [k: string]: unknown }>;
-type ViewLayoutEntry = EntryType<'viewlayout', { [k: string]: unknown }>;
-type ValuesEntry = EntryType<'values', { [k: string]: unknown }>;
+export type AxisTicksEntry = EntryType<'axisticks', { [k: string]: unknown }>;
+export type BoundEntry = EntryType<'bound', { [k: string]: unknown }>;
+export type CollectEntry = EntryType<'collect', { [k: string]: unknown }>;
+export type CompareEntry = EntryType<'compare', { [k: string]: unknown }>;
+export type DataJoinEntry = EntryType<'datajoin', { [k: string]: unknown }>;
+export type EncodeEntry = EntryType<'encode', { [k: string]: unknown }>;
+export type ExpressionEntry = EntryType<'expression', { [k: string]: unknown }>;
+export type ExtentEntry = EntryType<'extent', { [k: string]: unknown }>;
+export type FacetEntry = EntryType<'facet', { [k: string]: unknown }>;
+export type FieldEntry = EntryType<'field', { [k: string]: unknown }>;
+export type KeyEntry = EntryType<'key', { [k: string]: unknown }>;
+export type LegendEntriesEntry = EntryType<'legendentries', { [k: string]: unknown }>;
+export type LoadEntry = EntryType<'load', { [k: string]: unknown }>;
+export type MarkEntry = EntryType<'mark', { [k: string]: unknown }>;
+export type MultiextentEntry = EntryType<'multiextent', { [k: string]: unknown }>;
+export type MultivaluesEntry = EntryType<'multivalues', { [k: string]: unknown }>;
+export type OverlapEntry = EntryType<'overlap', { [k: string]: unknown }>;
+export type ParamsEntry = EntryType<'params', { [k: string]: unknown }>;
+export type PrefacetEntry = EntryType<'prefacet', { [k: string]: unknown }>;
+export type ProjectionEntry = EntryType<'projection', { [k: string]: unknown }>;
+export type ProxyEntry = EntryType<'proxy', { [k: string]: unknown }>;
+export type RelayEntry = EntryType<'relay', { [k: string]: unknown }>;
+export type RenderEntry = EntryType<'render', { [k: string]: unknown }>;
+export type ScaleEntry = EntryType<'scale', { [k: string]: unknown }>;
+export type SieveEntry = EntryType<'sieve', { [k: string]: unknown }>;
+export type SortItemsEntry = EntryType<'sortitems', { [k: string]: unknown }>;
+export type ViewLayoutEntry = EntryType<'viewlayout', { [k: string]: unknown }>;
+export type ValuesEntry = EntryType<'values', { [k: string]: unknown }>;
 
-type EntryType<NAME extends string, BODY extends { [k: string]: unknown }> = {
+export type EntryType<NAME extends string, BODY extends { [k: string]: unknown }> = {
   id: id;
   type: NAME;
   // Scope.add
@@ -172,7 +172,7 @@ type EntryType<NAME extends string, BODY extends { [k: string]: unknown }> = {
   data?: { [name: string]: ('input' | 'output' | 'values' | `index:${string}`)[] };
 } & BODY;
 
-type Stream = {
+export type Stream = {
   id: id;
 } & (
   | // from parsers/stream.js:eventStream -> scope.event
@@ -203,19 +203,19 @@ type Stream = {
     })
 );
 
-type Ref = {
+export interface Ref {
   $ref: id;
-};
+}
 
 // from `Scope.js:Scope:id`
 // String if sub id with `:` seperate parent from child id numbers
-type id = string | number;
+export type id = string | number;
 
 // from vega-functions:parser.js
-type Parse = {
+export interface Parse {
   // TODO: AST types
   // TODO: Make ast config as generic param?
   $expr: { code: string; ast?: unknown };
   $params: { [signalName: string]: Ref };
   $fields: string[];
-};
+}
