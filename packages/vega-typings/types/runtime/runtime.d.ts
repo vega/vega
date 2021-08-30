f5b308c1afa2b019cf8424f324c70ab5c0a719b6
Saul Shanabrook <s.shanabrook@gmail.com>
2021-08-30 11:02:07 -0400

    Move comments to blocks

--- packages/vega-typings/types/runtime/runtime.d.ts
+++ packages/vega-typings/types/runtime/runtime.d.ts
@@ -5,13 +5,19 @@ import { EventType, WindowEventType, Transforms, Binding as SpecBinding } from '
 export interface Runtime {
   description: string;
   operators: Operator[];
-  // Event streams that Vega is listening to. Often DOM events.
+  /**
+   * Event streams vega is listening to, often DOM events.
+   */
   streams: Stream[];
-  // Triggers updates of operations based on other operations
+  /**
+   * Triggers updates of streams/operations based on other nodes.
+   */
   updates: Update[];
-  // Contains a list of bindings which map singal names to DOM input elements.
-  // When the dom elements are updated, it updates the operator with the signal name
-  // the same as the binding
+  /**
+   * Contains a list of bindings which map signal names to DOM input elements.
+   * When the dom elements are updated, it updates the operator with the signal name
+   * the same as the binding
+   */
   bindings: Binding[];
   eventConfig?: Config['events'];
   locale?: Config['locale'];
@@ -20,41 +26,62 @@ export interface Runtime {
 export interface BaseOperator {
   id: ID;
   type: string;
-  // Parameters that are passed into the transform when it is updated.
-  // They can either be static values or references to other operators
+  /**
+   * Parameters that are passed into the transform when it is updated.
+   * They can either be static values or references to other operators
+   */
   params?: Parameters;
-  // The initial value
+  /**
+   * The initial value
+   */
   value?: unknown;
   root?: boolean;
 
-  // Shows up from Scope.add but isn't used in the runtime
+  /**
+   * Shows up from Scope.add but isn't used in the runtime
+   */
   refs?: null;
 
-  // The name of the signal to bind this to, when the signal is updated, this operator's
-  // value is set to the new value
+  /**
+   * The name of the signal to bind this to, when the signal is updated, this operator's
+   * value is set to the new value
+   */
   signal?: string;
-  // Scope.finish.annotate
+  /**
+   * From Scope.finish.annotate
+   */
   data?: { [name: string]: ('input' | 'output' | 'values' | `index:${string}`)[] };
-  // A parent reference to watch for changes
+  /**
+   * A parent reference to watch for changes
+   */
   parent?: OperatorParam;
   scale?: string;
   metadata?: { [k: string]: unknown };
-  // A flag indicating if this operator should
-  // automatically update (react) when parameter values change. In other words,
-  // this flag determines if the operator registers itself as a listener on
-  // any upstream operators included in the parameters.
-  // Default is true
+
+  /**
+   * A flag indicating if this operator should
+   * automatically update (react) when parameter values change. In other words,
+   * this flag determines if the operator registers itself as a listener on
+   * any upstream operators included in the parameters.
+   * Default is true
+   */
   react?: boolean;
-  // A flag indicating if this operator
-  // should calculate an update only upon its initiatal evaluation, then
-  // deregister dependencies and suppress all future update invocations.
-  // Default is false
+
+  /**
+   * A flag indicating if this operator
+   * should calculate an update only upon its initial evaluation, then
+   * deregister dependencies and suppress all future update invocations.
+   * Default is false
+   */
   initonly?: boolean;
 }
 export type Operator = DefinedOperator | OtherOperator;
 
-// We have added special typings for a few operators. Ideally, all built in operators would have
-// precise typings, to correspond to their parameters, but we are typing them gradually.
+/**
+ * We have added special typings for a few operators. Ideally, all built in
+ * operators would have precise typings, to correspond to their parameters, but we
+ * are typing them gradually.
+ */
 export type DefinedOperator = OperatorOperator | CollectOperator | AggregateOperator;
 
 export interface OperatorOperator extends BaseOperator {
@@ -64,18 +91,26 @@ export interface OperatorOperator extends BaseOperator {
 
 export interface CollectOperator extends BaseOperator {
   type: 'collect';
-  // Either the data literals, or a reference to data to parse and load
+  /**
+   * Either the data literals, or a reference to data to parse and load
+   */
   value?: ObjectOrAny<
     {
-      // format of data
+      /**
+       * format of data
+       */
       $format?: Format;
     } & (
       | {
-          // URL to data
+          /**
+           * URL to data
+           */
           $request: string;
         }
       | {
-          // data as string for CSV or TSV, or object, for JSON
+          /**
+           * data as string for CSV or TSV, or object, for JSON
+           */
           $ingest: unknown;
         }
     )
@@ -89,32 +124,48 @@ export interface AggregateOperator extends BaseOperator {
   type: 'aggregate';
   params: {
     pulse: OrArray<OperatorParam>;
-    // An array of accessors to groupby.
+    /**
+     * An array of accessors to groupby.
+     */
     groupby?: OrArray<AccessorParameters>;
-    // An accessor that should return a string to key by
-    // Defaults to concatonating the groupby accessors
+    /**
+     * An accessor that should return a string to key by
+     * Defaults to concatenating the groupby accessors
+     */
     key?: AccessorParameters;
-    // An array of accessors to aggregate
-    // Can only be null if the op is count
+    /**
+     * An array of accessors to aggregate
+     * Can only be null if the op is count
+     */
     fields?: OrArray<AccessorParameters | null>;
-    // An array of strings indicating aggregation operations.
+    /**
+     * An array of strings indicating aggregation operations.
+     */
     ops?: OrArray<AggregateOps | OperatorParam>;
-    //  An array of output field names for aggregated values.
+    /**
+     * An array of output field names for aggregated values.
+     */
     as?: OrArray<string | OperatorParam>;
-    // A flag indicating that the full
-    // cross-product of groupby values should be generated, including empty cells.
-    // If true, the drop parameter is ignored and empty cells are retained.
-    // Defaults to false
+    /**
+     * A flag indicating that the full cross-product of groupby values should be
+     * generated, including empty cells.
+     * If true, the drop parameter is ignored and empty cells are retained.
+     * Defaults to false
+     */
     cross?: boolean | OperatorParam;
-    // A flag indicating if empty cells should be removed.
-    // Defaults to true
+    /**
+     * A flag indicating if empty cells should be removed.
+     * Defaults to true
+     */
     drop?: boolean | OperatorParam;
   };
 }
 
 export type OrArray<T> = T | T[];
 
-// All valid aggregate operators, from vega-transforms utils/AggregateOps.js
+/**
+ * All valid aggregate operators, from vega-transforms utils/AggregateOps.js
+ */
 export type AggregateOps =
   | 'values'
   | 'count'
@@ -140,10 +191,14 @@ export type AggregateOps =
   | 'max'
   | 'argmin'
   | 'argmax';
-// Support other operators which we haven't added as granular types for yet.
+/**
+ * Support other operators which we haven't added as granular types for yet.
+ */
 export interface OtherOperator extends BaseOperator {
   type: Exclude<
-    // All operator types defined in vega-parser transforms.js
+    /**
+     * All operator types defined in vega-parser transforms.js
+     */
     | 'axisticks'
     | 'bound'
     | 'compare'
@@ -177,13 +232,19 @@ export interface OtherOperator extends BaseOperator {
   >;
 }
 
-// From vega-runtime/parameters.js
+/**
+ * From vega-runtime/parameters.js
+ */
 export interface Parameters {
-  // If pulse is a param, it must be a ref
+  /**
+   * If pulse is a param, it must be a ref
+   */
   pulse?: OperatorParam | OperatorParam[];
   [name: string]: Parameter;
 }
-// A parameter is either builtin, with the proper keys, or some primitive value or other object
+/**
+ * A parameter is either builtin, with the proper keys, or some primitive value or other object
+ */
 export type Parameter = ObjectOrListObjectOrAny<BuiltinParameter>;
 
 /**
@@ -220,7 +281,9 @@ export interface KeyParam {
  * Resolve an expression reference.
  */
 export interface ExpressionParam {
-  // TODO: AST types
+  /**
+   * TODO: add support for AST types
+   */
   $expr: { code: string; ast?: unknown };
   $params?: Record<string, OperatorParam>;
   $fields?: string[];
@@ -246,7 +309,10 @@ export interface EncodeParam {
 export interface EncodeValue {
   $fields: string[];
   $output: string[];
-  // The keys of the channels is the same as the $output
+
+  /**
+   * The keys of the channels are the same as the $output
+   */
   $expr: { marktype: string; channels: Record<string, expr> };
 }
 
@@ -254,7 +320,9 @@ export interface EncodeValue {
  * Resolve a comparator function reference.
  */
 export interface CompareParam {
-  // Fields to compare on
+  /**
+   * Fields to compare on
+   */
   $compare: OrArray<string>;
   $order: OrArray<Order>;
 }
@@ -283,20 +351,28 @@ export type Subflow = Pick<Runtime, 'operators' | 'streams' | 'updates'>;
  */
 export type Stream = {
   id: ID;
-  // from parsers/stream.js:streamParameters
-  // Currently, only merged or streams that reference another stram
-  // use these parameters, but in the vega runtime any stream can have them
+  /**
+   * from parsers/stream.js:streamParameters
+   * Currently, only merged or streams that reference another stream use these
+   * parameters, but in the vega runtime any stream can have them
+   */
 
-  // Filter this stream for events that happen after an event from the first stream
-  // and before an event in the second.
+  /**
+   * Filter this stream for events that happen after an event from the first
+   * stream and before an event in the second.
+   */
   between?: [ID, ID];
   filter?: expr;
   throttle?: number;
   debounce?: number;
-  // Whether to stop native event propogation
+  /**
+   * Whether to stop native event propagation
+   */
   consume?: true;
 } & (
-  | // from parsers/stream.js:eventStream -> scope.event
+  | /**
+   * from parsers/stream.js:eventStream -> scope.event
+   */
   {
       source: 'timer';
       type: number;
@@ -309,28 +385,41 @@ export type Stream = {
       source: 'window';
       type: WindowEventType;
     }
-  // from parsers/stream.js:eventStream & nestedStream -> streamParameters
+  /**
+   * from parsers/stream.js:eventStream & nestedStream -> streamParameters
+   */
   | { stream: ID }
-  // from parsers/stream.js:mergeStream -> streamParameters
+  /**
+   * from parsers/stream.js:mergeStream -> streamParameters
+   */
   | { merge: ID[] }
 );
 
-// Updates are added in parsers/update.js -> scope.addUpdate
-// which is called from parsers/signal-updates.js for each "on" on each signal
+/**
+ * Updates are added in parsers/update.js -> scope.addUpdate
+ * which is called from parsers/signal-updates.js for each "on" on each signal
+ */
 export interface Update {
-  // The target signal is set to the new value
-  // Using an expression as a target is supported in the vega runtime
-  // but not used currently in any examples, so we don't include it in the typings
+  /**
+   * The target signal is set to the new value.
+   * Using an expression as a target is supported in the vega runtime
+   * but not used currently in any examples, so we don't include it in the typings.
+   */
   target: ID;
-  // Whenever the source signal fires, the update is triggered
+  /**
+   * Whenever the source signal fires, the update is triggered
+   */
   source: ID | OperatorParam;
-  // The update either a static value or a parse expxression that
-  // is re-evaluted whenever the source fires, and returns the value
-  // fpr the target
-  // The update is either a static value or a Parse expression.
+  /**
+   * The update is  either a static value or a parse expression that is
+   * re-evaluated whenever the source fires, and returns the value for the
+   * target
+   */
   update: ObjectOrAny<ExpressionParam>;
-  // If force is true, then it will always update the target,
-  // even if the result is the same
+  /**
+   * If force is true, then it will always update the target,
+   * even if the result is the same
+   */
   options?: { force?: boolean };
 }
 
@@ -365,6 +454,8 @@ export type KeysOfUnion<T> = T extends T ? keyof T : never;
 
 export type Primitive = number | string | bigint | boolean | symbol | null | undefined;
 
-// from `Scope.js:Scope:id`
-// String if sub id with `:` seperate parent from child id numbers
+/**
+ * from `Scope.js:Scope:id`
+ * String if sub id with `:` separate parent from child id numbers
+ **/
 export type ID = string | number;
