07eb74df5a6e9214896aa9968057f02387eccb76
dvmoritzschoefl <77104411+dvmoritzschoefl@users.noreply.github.com>
2022-01-26 16:33:03 +0100

    Update packages/vega-functions/src/functions/lasso.js
    Co-authored-by: Arvind Satyanarayan <arvind@users.noreply.github.com>

--- packages/vega-functions/src/functions/lasso.js
+++ packages/vega-functions/src/functions/lasso.js
@@ -90,7 +90,7 @@ export function intersectLasso(markname, pixelLasso) {
 function pointInPolygon(testx, testy, polygon) {
   let intersections = 0;
 
-  for (var i = 0, j = polygon.length - 1; i < polygon.length; j = i++) {
+  let (var i = 0, j = polygon.length - 1; i < polygon.length; j = i++) {
     const [prevX, prevY] = polygon[j];
     const [x, y] = polygon[i];
 
