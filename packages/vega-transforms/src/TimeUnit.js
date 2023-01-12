f60736127a4eec0a65e00d4abd9f30fadd322e1a
Paul Rosenzweig <paul.a.rosenzweig@gmail.com>
2023-01-12 12:29:55 -0500

    add "auto" unit to timeunit transform

--- packages/vega-transforms/src/TimeUnit.js
+++ packages/vega-transforms/src/TimeUnit.js
@@ -1,9 +1,9 @@
-import {Transform} from 'vega-dataflow';
+import { Transform } from 'vega-dataflow';
 import {
-  TIME_UNITS, timeBin, timeFloor, timeInterval, timeUnits,
-  utcFloor, utcInterval
+  TIME_UNITS, detectTimeUnits, timeBin, timeFloor, timeInterval,
+  timeUnits, utcFloor, utcInterval
 } from 'vega-time';
-import {accessorFields, extent, inherits, peek} from 'vega-util';
+import { accessorFields, extent, inherits, peek } from 'vega-util';
 
 /**
  * Discretize dates to specific time units.
@@ -19,14 +19,14 @@ const OUTPUT = ['unit0', 'unit1'];
 
 TimeUnit.Definition = {
   'type': 'TimeUnit',
-  'metadata': {'modifies': true},
+  'metadata': { 'modifies': true },
   'params': [
     { 'name': 'field', 'type': 'field', 'required': true },
     { 'name': 'interval', 'type': 'boolean', 'default': true },
-    { 'name': 'units', 'type': 'enum', 'values': TIME_UNITS, 'array': true },
+    { 'name': 'units', 'type': 'enum', 'values': [...TIME_UNITS, 'auto'], 'array': true },
     { 'name': 'step', 'type': 'number', 'default': 1 },
     { 'name': 'maxbins', 'type': 'number', 'default': 40 },
-    { 'name': 'extent', 'type': 'date', 'array': true},
+    { 'name': 'extent', 'type': 'date', 'array': true },
     { 'name': 'timezone', 'type': 'enum', 'default': 'local', 'values': ['local', 'utc'] },
     { 'name': 'as', 'type': 'string', 'array': true, 'length': 2, 'default': OUTPUT }
   ]
@@ -35,18 +35,18 @@ TimeUnit.Definition = {
 inherits(TimeUnit, Transform, {
   transform(_, pulse) {
     const field = _.field,
-          band = _.interval !== false,
-          utc = _.timezone === 'utc',
-          floor = this._floor(_, pulse),
-          offset = (utc ? utcInterval : timeInterval)(floor.unit).offset,
-          as = _.as || OUTPUT,
-          u0 = as[0],
-          u1 = as[1],
-          step = floor.step;
+      band = _.interval !== false,
+      utc = _.timezone === 'utc',
+      floor = this._floor(_, pulse),
+      offset = (utc ? utcInterval : timeInterval)(floor.unit).offset,
+      as = _.as || OUTPUT,
+      u0 = as[0],
+      u1 = as[1],
+      step = floor.step;
 
     let min = floor.start || Infinity,
-        max = floor.stop || -Infinity,
-        flag = pulse.ADD;
+      max = floor.stop || -Infinity,
+      flag = pulse.ADD;
 
     if (
       _.modified() ||
@@ -83,17 +83,19 @@ inherits(TimeUnit, Transform, {
     const utc = _.timezone === 'utc';
 
     // get parameters
-    const {units, step} = _.units
-      ? {units: _.units, step: _.step || 1}
-      : timeBin({
-        extent:  _.extent || extent(pulse.materialize(pulse.SOURCE).source, _.field),
-        maxbins: _.maxbins
-      });
+    const { units, step } = _.units === 'auto'
+      ? detectTimeUnits(pulse.materialize(pulse.SOURCE).source, _.field)
+      : _.units
+        ? { units: _.units, step: _.step || 1 }
+        : timeBin({
+          extent: _.extent || extent(pulse.materialize(pulse.SOURCE).source, _.field),
+          maxbins: _.maxbins
+        });
 
     // check / standardize time units
     const tunits = timeUnits(units),
-          prev = this.value || {},
-          floor = (utc ? utcFloor : timeFloor)(tunits, step);
+      prev = this.value || {},
+      floor = (utc ? utcFloor : timeFloor)(tunits, step);
 
     floor.unit = peek(tunits);
     floor.units = tunits;
