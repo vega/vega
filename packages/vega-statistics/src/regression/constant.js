490d29bad363b0662cf25d90ebce9d31de631495
Dominik Moritz <domoritz@gmail.com>
2023-04-06 14:55:18 -0400

    Simplify

--- packages/vega-statistics/src/regression/constant.js
+++ packages/vega-statistics/src/regression/constant.js
@@ -1,15 +1,15 @@
 export default function (data, x, y) {
   let mean = 0, n = 0;
+
   for (const d of data) {
     const val = y(d)
     if (x(d) == null || val == null || isNaN(val)) continue;
     mean += (val - mean) / ++n;
   }
-  const predict = () => mean;
 
   return {
     coef: [mean],
-    predict: predict,
+    predict: () => mean,
     rSquared: 0
   };
 }
