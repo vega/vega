e02afa25127b10a0d83146cfd0f1b1514c6d240c
Dominik Moritz <domoritz@gmail.com>
2023-05-10 19:39:32 +0200

    chore: ignore eslint warnigns

--- .eslintrc.js
+++ .eslintrc.js
@@ -37,7 +37,9 @@ module.exports = {
       rules: {
         ...rules,
         '@typescript-eslint/ban-types': 'off',
-        '@typescript-eslint/no-explicit-any': 'off'
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/ban-ts-comment': 'off',
+        '@typescript-eslint/no-unused-vars': 'off'
       }
     }
   ]
