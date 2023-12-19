5f38e1b5530fbc34a25b3418aa1ecc4eec9d197a
Nicolas Kruchten <nkruchten@hex.tech>
2023-12-19 15:35:00 -0500

    sum([null]) -> null

--- packages/vega-transforms/src/util/AggregateOps.js
+++ packages/vega-transforms/src/util/AggregateOps.js
@@ -33,7 +33,7 @@ export const AggregateOps = {
   },
   sum: {
     init:  m => m.sum = 0,
-    value: m => m.sum,
+    value: m => m.valid ? m.sum : undefined,
     add:  (m, v) => m.sum += +v,
     rem:  (m, v) => m.sum -= v
   },
