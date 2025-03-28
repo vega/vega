d98c6c77ec2b4a13c76020a6e2418617cce61d97
Dominik Moritz <domoritz@gmail.com>
2025-03-28 16:02:05 -0400

    fix: update version in specs (#4045)

--- packages/vega-typings/tests/spec/valid/falkensee.ts
+++ packages/vega-typings/tests/spec/valid/falkensee.ts
@@ -1,7 +1,7 @@
 import { Spec } from 'vega';
 
 export const spec: Spec = {
-  $schema: 'https://vega.github.io/schema/vega/v5.json',
+  $schema: 'https://vega.github.io/schema/vega/v6.json',
   width: 500,
   height: 250,
   padding: 5,
