8f3527fb483b25983f5b9d8c5ab7f4d1d521d7d6
Dominik Moritz <domoritz@gmail.com>
2025-03-30 13:06:12 -0400

    chore: delete map files and old compatibility libraries

--- docs/assets/symbol.min.js
+++
@@ -1,1 +0,0 @@
-!function(e){if(typeof Symbol==='undefined'){e.Symbol=function(n){n='@@'+n;Object.defineProperty(Object.prototype,n,{get:function(){},set:function(v){Object.defineProperty(this,n,{configurable:true,writable:true,value:v});}});return n;}}}(this);
