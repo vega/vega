8ef645bc4bce92c2c93160282d9af5c37aac320c
Lukas Hermann <1734032+lsh@users.noreply.github.com>
2025-09-03 22:49:44 -0700

    bump package versions
    Signed-off-by: Lukas Hermann <1734032+lsh@users.noreply.github.com>

--- docs/vega.js
+++ docs/vega.js
@@ -3361,22 +3361,20 @@
   /**
    * Factory for a loader constructor that provides methods for requesting
    * files from either the network or disk, and for sanitizing request URIs.
-   * @param {function} fetch - The Fetch API for HTTP network requests.
-   *   If null or undefined, HTTP loading will be disabled.
    * @param {object} fs - The file system interface for file loading.
    *   If null or undefined, local file loading will be disabled.
    * @return {function} A loader constructor with the following signature:
    *   param {object} [options] - Optional default loading options to use.
    *   return {object} - A new loader instance.
    */
-  function loaderFactory(fetch, fs) {
+  function loaderFactory(fs) {
     return options => ({
       options: options || {},
       sanitize: sanitize,
       load: load$1,
       fileAccess: false,
       file: fileLoader(),
-      http: httpLoader(fetch)
+      http: httpLoader
     });
   }
 
@@ -3393,7 +3391,7 @@
   async function load$1(uri, options) {
     const opt = await this.sanitize(uri, options),
       url = opt.href;
-    return opt.localFile ? this.file(url) : this.http(url, options);
+    return opt.localFile ? this.file(url) : this.http(url, options?.http);
   }
 
   /**
@@ -3492,27 +3490,16 @@
   }
 
   /**
-   * HTTP request handler factory.
-   * @param {function} fetch - The Fetch API method.
-   * @return {function} - An http loader with the following signature:
-   *   param {string} url - The url to request.
-   *   param {object} options - An options hash.
-   *   return {Promise} - A promise that resolves to the file contents.
+   * An http loader.
+   * @param {string} url - The url to request.
+   * @param {Partial<RequestInit>} options - An options hash.
+   * @return {Promise} - A promise that resolves to the file contents.
    */
-  function httpLoader(fetch) {
-    return fetch ? async function (url, options) {
-      const opt = extend$1({}, this.options.http, options),
-        type = options && options.response,
-        response = await fetch(url, opt);
-      return !response.ok ? error(response.status + '' + response.statusText) : isFunction(response[type]) ? response[type]() : response.text();
-    } : httpReject;
-  }
-
-  /**
-   * Default http request handler that simply rejects.
-   */
-  async function httpReject() {
-    error('No HTTP fetch method available.');
+  async function httpLoader(url, options) {
+    const opt = extend$1({}, this.options.http, options),
+      type = options && options.response,
+      response = await fetch(url, opt);
+    return !response.ok ? error(response.status + '' + response.statusText) : isFunction(response[type]) ? response[type]() : response.text();
   }
   const isValid = _ => _ != null && _ === _;
   const isBoolean = _ => _ === 'true' || _ === 'false' || _ === true || _ === false;
@@ -3672,7 +3659,7 @@
       }
     }
   }
-  const loader = loaderFactory(typeof fetch !== 'undefined' && fetch);
+  const loader = loaderFactory();
 
   function UniqueList(idFunc) {
     const $ = idFunc || identity$6,
@@ -33052,7 +33039,7 @@
     resolvefilter: ResolveFilter
   });
 
-  var version$1 = "6.1.0";
+  var version$1 = "6.2.0";
 
   const RawCode = 'RawCode';
   const Literal = 'Literal';
@@ -34558,6 +34545,8 @@
       globalvar = opt.globalvar,
       fieldvar = opt.fieldvar,
       outputGlobal = isFunction(globalvar) ? globalvar : id => `${globalvar}["${id}"]`;
+    // JSON authors are not allowed to set properties with these names, as these are built-in to the JS Object Prototype.
+    new Set([...Object.getOwnPropertyNames(Object.prototype).filter(name => typeof Object.prototype[name] === 'function'), '__proto__']);
     let globals = {},
       fields = {},
       memberDepth = 0;
@@ -34611,7 +34600,16 @@
       UnaryExpression: n => '(' + n.operator + visit(n.argument) + ')',
       ConditionalExpression: n => '(' + visit(n.test) + '?' + visit(n.consequent) + ':' + visit(n.alternate) + ')',
       LogicalExpression: n => '(' + visit(n.left) + n.operator + visit(n.right) + ')',
-      ObjectExpression: n => '{' + n.properties.map(visit).join(',') + '}',
+      ObjectExpression: n => {
+        // If any keys would override Object prototype methods, throw error
+        for (const prop of n.properties) {
+          const keyName = prop.key.name;
+          if (blockedPropertyNames.has(keyName)) {
+            error('Illegal property: ' + keyName);
+          }
+        }
+        return '{' + n.properties.map(visit).join(',') + '}';
+      },
       Property: n => {
         memberDepth += 1;
         const k = visit(n.key);
@@ -41729,4 +41727,3 @@
   exports.zoomSymlog = zoomSymlog;
 
 }));
-//# sourceMappingURL=vega.js.map
