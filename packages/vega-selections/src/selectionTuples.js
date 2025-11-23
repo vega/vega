e07f05e825dd3825f5755b3fa6375cd55140a659
Cameron Yick <cameron.yick@datadoghq.com>
2025-11-23 03:13:45 -0500

    fix(vega-selections): fix misleading docstring / restore brushing behavior

--- packages/vega-selections/src/selectionTuples.js
+++ packages/vega-selections/src/selectionTuples.js
@@ -1,11 +1,11 @@
 import {extend} from 'vega-util';
 import {$selectionId, SelectionId, getter} from './util.js';
-import { error, isArray, isString } from 'vega-util';
+import { error, isArray, isObject } from 'vega-util';
 
 /**
  * Maps an array of scene graph items to an array of selection tuples.
- * @param {string} name  - The name of the dataset representing the selection.
- * @param {string} base  - The base object that generated tuples extend.
+ * @param {array} array - Input scene graph items
+ * @param {object} base - The base object that generated tuples extend.
  *
  * @returns {array} An array of selection entries for the given unit.
  */
@@ -14,8 +14,8 @@ export function selectionTuples(array, base) {
   if (!isArray(array)) {
     error('First argument to selectionTuples must be an array.');
   }
-  if (!isString(base)) {
-    error('Second argument to selectionTuples must be a string.');
+  if (!isObject(base)) {
+    error('Second argument to selectionTuples must be an object.');
   }
 
   return array.map(x => extend(
