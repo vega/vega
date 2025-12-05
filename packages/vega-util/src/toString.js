9ee526f6b2c49e0a2e77f6bdcb9a14618463c22b
Cameron Yick <hydrosquall@users.noreply.github.com>
2025-12-05 00:45:16 -0500

    feat(vega-util): add signatures for all remaining type coercion to* utils (#4188)
    ## Motivation
    
    - Increase guardrails of codebase to make it possible to add new
    contributors / propose more ambitious features
    - Check the cost of adding types to vega without migrating the whole
    codebase at once
    - Adding JSDoc param types allows us to ge type safety without having to
    switch to the typescript compiler (.ts files) yet
    - Builds on https://github.com/vega/vega/pull/4185 /
    https://github.com/vega/vega/pull/4187 /
    https://github.com/vega/vega/issues/3971
    
    ## Changes
    
    - Types/documents every `to*` helper in `packages/vega-util/src`
    - Changes _some_ runtime behaviors + documents some surprises (i.e.
    toBoolean does not always return a Boolean)
    
    ## Testing
    
    - Verified `npm run generate-types` and `npm run typecheck`; the
    rewritten helpers now use the same pipeline as the string utilities.

--- packages/vega-util/src/toString.js
+++ packages/vega-util/src/toString.js
@@ -1,3 +1,7 @@
+/** Coerces a value to a string, returning `null` for empty inputs.
+ * @param {unknown} _ - Input value to coerce.
+ * @returns {string | null}
+ */
 export default function(_) {
   return _ == null || _ === '' ? null : _ + '';
 }
