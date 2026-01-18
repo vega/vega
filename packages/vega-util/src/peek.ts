50dc5f56f18a435529a8154d41e2db2408650c21
Cameron Yick <cameron.yick@datadoghq.com>
2026-01-18 12:41:15 -0500

    refactor(vega-util): make transform utilities typesafe

--- packages/vega-util/src/peek.ts
+++ packages/vega-util/src/peek.ts
@@ -1,6 +1,8 @@
 /**
  * Returns the last element of an array.
  */
+export default function peek<T>(array: readonly [T, ...T[]]): T;
+export default function peek<T>(array: readonly T[]): T | undefined;
 export default function peek<T>(array: readonly T[]): T | undefined {
   return array[array.length - 1];
 }
